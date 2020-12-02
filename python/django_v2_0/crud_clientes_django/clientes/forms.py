4fcc221bfae9ae456b403fff0d766634748b0331
William Goulart Pacheco <gtaskwill@gmail.com>
2020-12-02 13:25:54 -0300

    #

--- python/django_v2_0/crud_clientes_django/clientes/forms.py
+++ python/django_v2_0/crud_clientes_django/clientes/forms.py
@@ -6,3 +6,8 @@ class ClienteForm(forms.ModelForm):
     class Meta:
         model = Cliente
         fields = ['nome', 'email', 'cpf']
+        widgets = {
+            'nome' : forms.TextInput(attrs={'placeholder' : 'Informe o nome do cliente'}),
+            'email': forms.TextInput(attrs={'placeholder': 'Informe o email do cliente'}),
+            'cpf': forms.TextInput(attrs={'placeholder': 'Informe o cpf do cliente'}),
+        }
