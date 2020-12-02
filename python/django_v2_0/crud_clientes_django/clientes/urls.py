5b3bea0665bc0f21b05d26dd900e088e76b9164a
William Goulart Pacheco <gtaskwill@gmail.com>
2020-12-02 13:19:58 -0300

    #

--- python/django_v2_0/crud_clientes_django/clientes/urls.py
+++ python/django_v2_0/crud_clientes_django/clientes/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import lista_De_clientes
+from .views import lista_De_clientes,adicionar_cliente
 
 urlpatterns = [
     path('', lista_De_clientes),
+    path('adicionar_cliente/', adicionar_cliente),
 ]
--- python/django_v2_0/crud_clientes_django/clientes/views.py
+++ python/django_v2_0/crud_clientes_django/clientes/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from .models import Cliente
+from .forms import ClienteForm
 
 
 # Create your views here.
@@ -7,4 +8,18 @@ def lista_De_clientes(request):
     clientes = Cliente.objects.all()
     v_template="clientes/lista_de_clientes.html"
     v_context_parms = {"clientes":clientes}
+    return render(request,v_template, v_context_parms)
+
+
+def adicionar_cliente(request):
+    form = ClienteForm(request.POST)
+
+    if form.is_valid() :
+        obj = form.save
+        obj.save()
+    else:
+        form = ClienteForm()
+
+    v_template="clientes/adicionar_cliente.html"
+    v_context_parms = {"form":form}
     return render(request,v_template, v_context_parms)
