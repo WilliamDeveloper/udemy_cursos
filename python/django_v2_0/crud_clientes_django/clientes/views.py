0daa591041a42b84345d2c103cf39c9084396fe6
William Goulart Pacheco <gtaskwill@gmail.com>
2020-12-02 12:35:50 -0300

    #

--- python/django_v2_0/crud_clientes_django/clientes/views.py
+++ python/django_v2_0/crud_clientes_django/clientes/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from .models import Cliente
+
+
 # Create your views here.
 def lista_De_clientes(request):
-    return  HttpResponse('lista de clientes')
+    clientes = Cliente.objects.all()
+    v_template="clientes/lista_de_clientes.html"
+    v_context_parms = {"clientes":clientes}
+    return render(request,v_template, v_context_parms)
