0af3e894183dc7176a32e155f89c3b3c91b37740
William Goulart Pacheco <gtaskwill@gmail.com>
2020-12-02 10:24:55 -0300

    #

--- python/django_v2_0/produtos/produtos/settings.py
+++ python/django_v2_0/produtos/produtos/settings.py
@@ -57,7 +57,7 @@ ROOT_URLCONF = 'produtos.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [TEMPLATE_DIR,],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -121,3 +121,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIR = (
+    STATIC_DIR,
+)
