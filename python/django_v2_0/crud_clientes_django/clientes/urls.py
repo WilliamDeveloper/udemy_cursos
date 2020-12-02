957256d097dea270884d3a9f11fbc8e9925a5147
William Goulart Pacheco <gtaskwill@gmail.com>
2020-12-02 13:55:14 -0300

    #

--- python/django_v2_0/crud_clientes_django/clientes/urls.py
+++ python/django_v2_0/crud_clientes_django/clientes/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import lista_De_clientes,adicionar_cliente
+from .views import lista_De_clientes,adicionar_cliente,editar_cliente
 
 urlpatterns = [
     path('', lista_De_clientes,name='lista_De_clientes'),
     path('adicionar_cliente/', adicionar_cliente),
+    path('editar_cliente/<int:id>', editar_cliente),
 ]
--- python/django_v2_0/crud_clientes_django/clientes/views.py
+++ python/django_v2_0/crud_clientes_django/clientes/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect,get_object_or_404
 from .models import Cliente
 from .forms import ClienteForm
 
@@ -24,3 +24,19 @@ def adicionar_cliente(request):
     v_template="clientes/adicionar_cliente.html"
     v_context_parms = {"form":form}
     return render(request,v_template, v_context_parms)
+
+
+def editar_cliente(request, id=None):
+    cliente = get_object_or_404(Cliente, id=id)
+
+    form = ClienteForm(request.POST or None)
+    if form.is_valid():
+        obj = form.save()
+        obj.save()
+        form = ClienteForm()
+        v_to = 'lista_De_clientes'
+        return redirect(v_to)
+
+    v_template = "clientes/editar_cliente.html"
+    v_context_parms = {"form": form}
+    return render(request, v_template, v_context_parms)
