47dc665e17cecc3beee07e29aa9f11609c14edb5
William Goulart Pacheco <gtaskwill@gmail.com>
2020-12-02 10:08:16 -0300

    #

--- python/django_v2_0/produtos/items/views.py
+++ python/django_v2_0/produtos/items/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+def home(request):
+    return HttpResponse('<h1>àrea de home</h1>')
+
+
+def produtos(requests):
+    return HttpResponse('<h1>àrea de produtos</h1>')
+
+
+
+def clientes(requests):
+    return HttpResponse('<h1>àrea de clientes</h1>')
