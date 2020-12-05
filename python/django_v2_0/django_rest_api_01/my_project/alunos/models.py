5cd6a47dc0de9d81a9a6b011e10a0d99ac98e359
William Goulart Pacheco <gtaskwill@gmail.com>
2020-12-04 23:12:08 -0300

    #

--- python/django_v2_0/django_rest_api_01/my_project/alunos/models.py
+++ python/django_v2_0/django_rest_api_01/my_project/alunos/models.py
@@ -1,10 +1,11 @@
 from django.db import models
+from django.core.validators import MinValueValidator,MaxValueValidator
 
 # Create your models here.
 class Aluno(models.Model):
     nome = models.CharField(max_length=200)
     profissao = models.CharField(max_length=200)
-    ano_nasc =  models.PositiveSmallIntegerField()
+    ano_nasc =  models.PositiveSmallIntegerField(validators=[MinValueValidator(1970)])
     ativo = models.BooleanField(default=True)
 
     def __str__(self):
