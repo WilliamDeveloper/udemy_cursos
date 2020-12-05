43af645dca0d8eb6470c47b3c11dcc2a272e7bd0
William Goulart Pacheco <gtaskwill@gmail.com>
2020-12-05 15:07:09 -0300

    #

--- python/django_v2_0/django_rest_api_02/my_project/artigos_app/api/serializers.py
+++ python/django_v2_0/django_rest_api_02/my_project/artigos_app/api/serializers.py
@@ -4,7 +4,7 @@ from artigos_app.models import Autor, Artigo
 class AutorSerializer(serializers.ModelSerializer):
     class Meta:
         model = Autor
-        fields = ('nome','endereco','site','email')
+        fields = ('id','nome','endereco','site','email')
 
 class ArtigoSerializer(serializers.ModelSerializer):
     autor = AutorSerializer()
