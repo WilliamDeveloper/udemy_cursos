0feae7d85a192b636081e9001ed47f26c713f1bf
William Goulart Pacheco <gtaskwill@gmail.com>
2020-12-05 15:13:02 -0300

    #

--- python/django_v2_0/django_rest_api_02/my_project/my_project/settings.py
+++ python/django_v2_0/django_rest_api_02/my_project/my_project/settings.py
@@ -15,8 +15,8 @@ import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-TEMPLATE_DIR = os.path.join('templates')
-STATIC_DIR = os.path.join('static')
+TEMPLATE_DIR = os.path.join(BASE_DIR,'templates')
+STATIC_DIR = os.path.join(BASE_DIR,'static')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
