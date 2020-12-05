7aa625bde463d181a01c2669213bec0aacf248f9
William Goulart Pacheco <gtaskwill@gmail.com>
2020-12-04 23:08:45 -0300

    #

--- python/django_v2_0/django_rest_api_01/my_project/alunos/models.py
+++ python/django_v2_0/django_rest_api_01/my_project/alunos/models.py
@@ -6,3 +6,6 @@ class Aluno(models.Model):
     profissao = models.CharField(max_length=200)
     ano_nasc =  models.PositiveSmallIntegerField()
     ativo = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.nome
