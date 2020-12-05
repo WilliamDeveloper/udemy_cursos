c93da1b54ae608b7a97b3dbd0f58d0d0737bd74b
William Goulart Pacheco <gtaskwill@gmail.com>
2020-12-05 14:39:26 -0300

    #

--- python/django_v2_0/django_rest_api_02/my_project/artigos_app/api/views.py
+++ python/django_v2_0/django_rest_api_02/my_project/artigos_app/api/views.py
@@ -1,9 +1,10 @@
 from rest_framework import viewsets
 from artigos_app.models import Autor
 from .serializers import AutorSerializer
-from rest_framework.permissions import IsAuthenticated
+from rest_framework.permissions import IsAuthenticated, AllowAny
 
 class AutorViewSet(viewsets.ModelViewSet):
-    permission_classes = (IsAuthenticated,)
+    # permission_classes = (IsAuthenticated,)
+    permission_classes = (AllowAny,)
     queryset = Autor.objects.all()
     serializer_class = AutorSerializer
