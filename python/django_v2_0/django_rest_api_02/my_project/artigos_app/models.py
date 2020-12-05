e52d7968499887fd292fac9eba36454fec8beaaa
William Goulart Pacheco <gtaskwill@gmail.com>
2020-12-05 13:23:13 -0300

    #

--- python/django_v2_0/django_rest_api_02/my_project/artigos_app/models.py
+++ python/django_v2_0/django_rest_api_02/my_project/artigos_app/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Autor(models.Model):
+    nome = models.CharField(max_length=255)
+    endereco = models.CharField(max_length=255)
+    site = models.URLField(blank=True,null=True)
+    email = models.EmailField()
