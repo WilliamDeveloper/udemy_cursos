5b2226d7c0f15b1fd1214c8d98f5c956bff25301
William Goulart Pacheco <gtaskwill@gmail.com>
2020-12-04 23:40:11 -0300

    #

--- python/django_v2_0/django_rest_api_01/my_project/alunos/api/serializers.py
+++ python/django_v2_0/django_rest_api_01/my_project/alunos/api/serializers.py
@@ -1,2 +1,8 @@
 #pip3 install djangorestframework
 from rest_framework import serializers
+from alunos.models import Aluno
+
+class AlunoSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Aluno
+        fields = ('__all__')
