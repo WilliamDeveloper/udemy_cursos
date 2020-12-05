19d9fda7b11581635eef553337a55c10b8744de2
William Goulart Pacheco <gtaskwill@gmail.com>
2020-12-05 13:43:54 -0300

    #

--- python/django_v2_0/django_rest_api_02/my_project/artigos_app/admin.py
+++ python/django_v2_0/django_rest_api_02/my_project/artigos_app/admin.py
@@ -1,6 +1,12 @@
 from django.contrib import admin
-from .models import Autor
+from .models import Autor,Artigo
 
 # Register your models here.
 
 admin.site.register(Autor)
+
+class ArtigoAdmin(admin.ModelAdmin):
+    list_display = ('autor','titulo','publicado_em','atualizado_em','texto')
+
+
+admin.site.register(Artigo)
