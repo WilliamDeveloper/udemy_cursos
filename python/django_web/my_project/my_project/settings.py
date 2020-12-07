bb8248ca5d9415fee66f119b56a8912b78d30f8f
William Goulart Pacheco <gtaskwill@gmail.com>
2020-12-07 12:51:03 -0300

    #

--- python/django_web/my_project/my_project/settings.py
+++ python/django_web/my_project/my_project/settings.py
@@ -125,3 +125,13 @@ STATIC_URL = '/static/'
 # habilitando upload de arquivos estaticos
 MEDIA_ROOT= os.path.join(BASE_DIR, 'my_project', 'media' )
 MEDIA_URL='/media/'
+
+# emails
+# EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+DEFAULT_FROM_EMAIL='NOME <email@gmail.com>'
+EMAIL_USE_TLS = True
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = 'smtp.gmail.com'
+EMAIL_HOST_PASSWORD = 'senha'
+
