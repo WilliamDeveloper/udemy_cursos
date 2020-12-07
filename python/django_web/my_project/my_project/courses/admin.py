4c0b5cae1a55169363c7d93dc654caeab243b4f7
William Goulart Pacheco <gtaskwill@gmail.com>
2020-12-07 08:06:14 -0300

    #

--- python/django_web/my_project/my_project/courses/admin.py
+++ python/django_web/my_project/my_project/courses/admin.py
@@ -5,6 +5,9 @@ from .models import Course
 class CourseAdmin(admin.ModelAdmin):
     list_display = ['name','slug','start_date','created_at']
     search_fields = ['name','slug']
+    prepopulated_fields = {
+        'slug': ('name',) ,
+    }
 
 
 # Register your models here.
