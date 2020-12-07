f8efce20fff957944e6f8fe9bb033eea456931a6
William Goulart Pacheco <gtaskwill@gmail.com>
2020-12-07 10:06:23 -0300

    #

--- python/django_web/my_project/my_project/courses/urls.py
+++ python/django_web/my_project/my_project/courses/urls.py
@@ -1,10 +1,11 @@
 from django.contrib import admin
 from django.urls import path,include
-from .views import index
+from .views import index,details
 
 app_name = 'my_project.courses'
 
 urlpatterns = [
     path('', index, name='index'),
+    path('<int:id>/', details, name='details'),
 
 ]
--- python/django_web/my_project/my_project/courses/views.py
+++ python/django_web/my_project/my_project/courses/views.py
@@ -5,8 +5,18 @@ from .models import Course
 # Create your views here.
 def index(request):
     courses = Course.objects.all()
-    template_name = 'courses/index.html'
+
     v_params_contexto = {
         'courses' : courses
     }
+    template_name = 'courses/index.html'
+    return render(request,template_name,v_params_contexto)
+
+def details(request,id):
+    course = Course.objects.get(pk=id)
+
+    v_params_contexto = {
+        'course' : course
+    }
+    template_name = 'courses/details.html'
     return render(request,template_name,v_params_contexto)
