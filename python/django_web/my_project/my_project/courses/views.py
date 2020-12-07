473304cd51b84d52d14e1a08c5d5e204db5b727f
William Goulart Pacheco <gtaskwill@gmail.com>
2020-12-07 11:29:49 -0300

    #

--- python/django_web/my_project/my_project/courses/views.py
+++ python/django_web/my_project/my_project/courses/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render,get_object_or_404
 
 from .models import Course
-
+from .forms import ContactCourse
 # Create your views here.
 def index(request):
     courses = Course.objects.all()
@@ -27,7 +27,8 @@ def details(request,slug):
     course = get_object_or_404(Course,slug=slug)
 
     v_params_contexto = {
-        'course' : course
+        'course' : course,
+        'form' : ContactCourse()
     }
     template_name = 'courses/details.html'
     return render(request,template_name,v_params_contexto)
