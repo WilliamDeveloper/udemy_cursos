5c86cdd58c4f311cef66ca66873e52772a48b6a5
William Goulart Pacheco <gtaskwill@gmail.com>
2020-12-07 18:00:22 -0300

    #

--- python/django_web/my_project/my_project/accounts/urls.py
+++ python/django_web/my_project/my_project/accounts/urls.py
@@ -7,5 +7,5 @@ from django.contrib.auth.views import LoginView
 app_name = 'my_project.accounts'
 
 urlpatterns = [
-    path('entrar/', LoginView, {'template_name':'accounts/login.html'},name='login'),
+    path('entrar/', LoginView.as_view(), {'template_name':'accounts/login.html'},name='login'),
 ]
