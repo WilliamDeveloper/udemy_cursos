7397368b1f6883962bc4916b2fd6e5936681db90
William Goulart Pacheco <gtaskwill@gmail.com>
2020-12-07 18:09:50 -0300

    #

--- python/django_web/my_project/my_project/accounts/urls.py
+++ python/django_web/my_project/my_project/accounts/urls.py
@@ -8,4 +8,5 @@ app_name = 'my_project.accounts'
 
 urlpatterns = [
     path('entrar/', LoginView.as_view(), {'template_name':'accounts/login.html'},name='login'),
+    # path('entrar/', LoginView.as_view(), {'template_name':'accounts/register.html'},name='register'),
 ]
