897da0243b9bcfd3df2a45abb774890e133cedc0
William Goulart Pacheco <gtaskwill@gmail.com>
2020-12-07 07:55:52 -0300

    #

--- python/django_web/my_project/my_project/courses/admin.py
+++ python/django_web/my_project/my_project/courses/admin.py
@@ -1,5 +1,11 @@
 from django.contrib import admin
 from .models import Course
 
+
+class CourseAdmin(admin.ModelAdmin):
+    list_display = ['name','slug','start_date','created_at']
+    search_fields = ['name','slug']
+
+
 # Register your models here.
-admin.site.register(Course)
+admin.site.register(Course,CourseAdmin)
