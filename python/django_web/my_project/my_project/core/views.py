bcf0edafd4c58d857bd0960b248b0ca88e6a706a
William Goulart Pacheco <gtaskwill@gmail.com>
2020-12-06 22:29:15 -0300

    #

--- python/django_web/my_project/my_project/core/views.py
+++ python/django_web/my_project/my_project/core/views.py
@@ -4,4 +4,7 @@ from django.http import HttpResponse
 # Create your views here.
 
 def home(request):
-    return HttpResponse('Hello World!')
+    v_params_context = {
+        'usuario': 'William'
+    }
+    return render(request,'home.html',v_params_context)
