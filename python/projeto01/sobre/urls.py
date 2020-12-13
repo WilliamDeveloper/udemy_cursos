7bbacbeda5074f4444d3bbc154474ee700aa731a
William Goulart Pacheco <gtaskwill@gmail.com>
2020-12-13 14:24:47 -0300

    #

--- python/projeto01/sobre/urls.py
+++ python/projeto01/sobre/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns =[
-    path('', views.index) # /sobre
+    path('', views.index), # /sobre
+    path('teste/', views.teste), # /sobre/teste
 ]
--- python/projeto01/sobre/views.py
+++ python/projeto01/sobre/views.py
@@ -4,3 +4,7 @@ from django.http import HttpResponse
 # Create your views here.
 def index(request):
     return render(request,'sobre/index.html')
+
+
+def teste(request):
+    return render(request,'sobre/teste.html')
