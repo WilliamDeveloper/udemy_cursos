3968509b934c69f77e5542a107feb8c060c8bd27
William Goulart Pacheco <gtaskwill@gmail.com>
2020-12-14 08:37:32 -0300

    #

--- python/projeto03/paginas/views.py
+++ python/projeto03/paginas/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-    pass
+    return render(request,'paginas/index.html')
 
 def sobre(request):
-    pass
+    return render(request,'paginas/sobre.html')
