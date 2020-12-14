2da6bd6568340602d0776e55ecd72d972261b9c8
William Goulart Pacheco <gtaskwill@gmail.com>
2020-12-14 14:04:48 -0300

    #

--- python/projeto05/contatos/admin.py
+++ python/projeto05/contatos/admin.py
@@ -3,6 +3,9 @@ from .models import Categoria,Contato
 
 class ContatoAdmin(admin.ModelAdmin):
     list_display = ('id','nome','sobrenome','telefone' ,'email','data_criacao', 'categoria')
+    list_display_links = ('id','nome','sobrenome','telefone' ,'email','data_criacao', 'categoria')
+    list_filter = ('nome','sobrenome')
+    list_per_page = 10
 
 # Register your models here.
 admin.site.register(Categoria)
