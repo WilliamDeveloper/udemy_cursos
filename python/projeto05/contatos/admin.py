5df53719673c965d506da8506cd9d6a9e2436182
William Goulart Pacheco <gtaskwill@gmail.com>
2020-12-15 13:03:37 -0300

    #

--- python/projeto05/contatos/admin.py
+++ python/projeto05/contatos/admin.py
@@ -2,9 +2,10 @@ from django.contrib import admin
 from .models import Categoria,Contato
 
 class ContatoAdmin(admin.ModelAdmin):
-    list_display = ('id','nome','sobrenome','telefone' ,'email','data_criacao', 'categoria')
-    list_display_links = ('id','nome','sobrenome','telefone' ,'email','data_criacao', 'categoria')
+    list_display = ('id','nome','sobrenome','telefone' ,'email','data_criacao', 'categoria', 'mostrar')
+    list_display_links = ('id','nome','sobrenome','email','data_criacao', 'categoria')
     list_filter = ('nome','sobrenome')
+    list_editable = ('mostrar','telefone',)
     list_per_page = 10
     search_fields = ('nome','sobrenome','telefone')
 
