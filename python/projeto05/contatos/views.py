98ecd21ac721e673a214cc4b5d055d031c42dd3b
William Goulart Pacheco <gtaskwill@gmail.com>
2020-12-15 10:01:32 -0300

    #

--- python/projeto05/contatos/views.py
+++ python/projeto05/contatos/views.py
@@ -1,5 +1,11 @@
 from django.shortcuts import render
 
+from .models import Contato
+
 # Create your views here.
 def index(request):
-    return render(request,'contatos/index.html')
+    contatos = Contato.objects.all()
+    v_parametros = {
+        'contatos':contatos,
+    }
+    return render(request,'contatos/index.html', v_parametros)
