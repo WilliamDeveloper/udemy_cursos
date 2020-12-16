ead642d77b3d43c009fecf81ce46f027897568a4
William Goulart Pacheco <gtaskwill@gmail.com>
2020-12-16 18:17:42 -0300

    #

--- python/projeto05/accounts/views.py
+++ python/projeto05/accounts/views.py
@@ -10,8 +10,35 @@ def logout(request) :
     return render(request, 'accounts/logout.html')
 
 def cadastro(request) :
-    v_msg= 'olá mundo'
-    messages.success(request,v_msg)
+    print('request: ', request)
+    print('request.post: ',request.POST)
+    print('request.get: ',request.GET)
+
+    if(request.method != 'POST'):
+        return render(request, 'accounts/cadastro.html')
+
+    nome = request.POST.get('nome')
+    sobrenome = request.POST.get('sobrenome')
+    email = request.POST.get('email')
+    usuario = request.POST.get('usuario')
+    senha = request.POST.get('senha')
+    senha2 = request.POST.get('senha2')
+
+    if not nome \
+        or not sobrenome \
+        or not email \
+        or not usuario \
+        or not senha \
+        or not senha2:
+
+        v_msg= 'Nunhum campo pode estar vazio'
+        messages.error(request,v_msg)
+
+
+
+
+
+
     return render(request, 'accounts/cadastro.html')
 
 def dashboard(request) :
