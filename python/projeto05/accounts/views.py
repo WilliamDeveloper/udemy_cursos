17c1159cd247c04b66b7b59b30f1ad409c1d7be7
William Goulart Pacheco <gtaskwill@gmail.com>
2020-12-16 18:59:32 -0300

    #

--- python/projeto05/accounts/views.py
+++ python/projeto05/accounts/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 from django.contrib import messages, auth
 from django.core.validators import validate_email
 from django.contrib.auth.models import User
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 
@@ -104,5 +105,6 @@ def cadastro(request) :
 
     return redirect('login')
 
+@login_required(redirect_field_name='login')
 def dashboard(request) :
     return render(request, 'accounts/dashboard.html')
