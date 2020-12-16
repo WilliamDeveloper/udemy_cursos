4f44ceb35d60ac31e9de606c5ea6a50342ec199c
William Goulart Pacheco <gtaskwill@gmail.com>
2020-12-16 07:52:39 -0300

    #

--- python/projeto05/contatos/urls.py
+++ python/projeto05/contatos/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('<int:contato_id>', views.contato_detalhe, name='contato_detalhe'),
+    path('busca/', views.index, name='busca'),
 ]
