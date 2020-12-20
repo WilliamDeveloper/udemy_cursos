0c12fb741b43d2ada3f6511f6839cf6ecae0079b
William Goulart Pacheco <gtaskwill@gmail.com>
2020-12-20 17:37:40 -0300

    #

---
+++ python/projeto06/blog/modo_prod.py
@@ -0,0 +1,74 @@
+import os
+from .settings import *
+
+##########################################################
+# configs custom -  MAQUINA_PRODUCAO
+##########################################################
+
+
+DEBUG = False
+
+ALLOWED_HOSTS = []
+
+LANGUAGE_CODE = 'pt-BR'
+TIME_ZONE = 'America/Sao_Paulo'
+
+FOLDER_GLOBAL_TEMPLATES= os.path.join(BASE_DIR,'templates')
+FOLDER_GLOBAL_STATIC= os.path.join(BASE_DIR,'templates','static')
+
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [FOLDER_GLOBAL_TEMPLATES,],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
+]
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/2.2/howto/static-files/
+
+STATICFILES_DIRS =[
+    FOLDER_GLOBAL_STATIC,
+]
+
+# juntara todos arquivos coletados na pasta static na raiz do projeto
+# python manage.py collectstatic
+STATIC_ROOT = os.path.join('static')
+
+MEDIA_ROOT=os.path.join(BASE_DIR,'media')
+MEDIA_URL="media/"
+
+
+#usando messages do bootstrap
+#sobrescrevendo as tag de alert do django para as tag do boostrap
+from django.contrib.messages import constants
+
+MESSAGE_TAGS ={
+    constants.ERROR : 'alert-danger',
+    constants.WARNING : 'alert-warning',
+    constants.DEBUG : 'alert-info',
+    constants.SUCCESS : 'alert-success',
+    constants.INFO : 'alert-info',
+}
+
+# sumernote
+INSTALLED_APPS += ('django_summernote',)
+X_FRAME_OPTIONS = 'SAMEORIGIN'
+
+# humanize
+INSTALLED_APPS += ('django.contrib.humanize',)
+
+
+# config ssl off
+SECURE_PROXY_SSL_HEADER = None
+SECURE_SSL_REDIRECT = False
+SESSION_COOKIE_SECURE = False
+CSRF_COOKIE_SECURE = False
