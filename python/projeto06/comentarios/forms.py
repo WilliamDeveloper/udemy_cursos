c4319727b4f175630fd67cfd248fcf226249b44d
William Goulart Pacheco <gtaskwill@gmail.com>
2020-12-20 15:48:38 -0300

    #

--- python/projeto06/comentarios/forms.py
+++ python/projeto06/comentarios/forms.py
@@ -5,8 +5,19 @@ class FormComentario(ModelForm):
 
     def clean(self):
         data = self.cleaned_data
-
         print(data)
+
+        nome = self.data.get('nome_comentario')
+        email = self.data.get('email_comentario')
+        comentario = self.data.get('comentario')
+
+        if len(nome) < 5:
+            self.add_error(
+                'nome_comentario',
+                'Nome precisa ter mais que 5 caracteres.'
+            )
+
+
 
     class Meta:
         model = Comentario
