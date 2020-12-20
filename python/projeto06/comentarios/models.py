d32915f80e4bc1d416a8c80ab3491f4e0d899c60
William Goulart Pacheco <gtaskwill@gmail.com>
2020-12-20 15:43:11 -0300

    #

--- python/projeto06/comentarios/models.py
+++ python/projeto06/comentarios/models.py
@@ -6,9 +6,9 @@ from django.utils import timezone
 
 # Create your models here.
 class Comentario(models.Model):
-    nome_comentario = models.CharField(max_length=150)
-    email_comentario = models.EmailField()
-    comentario = models.TextField()
+    nome_comentario = models.CharField(max_length=150, verbose_name='Nome')
+    email_comentario = models.EmailField(verbose_name='Email')
+    comentario = models.TextField(verbose_name='Comentário')
     post_comentario = models.ForeignKey(Post, on_delete=models.CASCADE)
     usuario_comentario = models.ForeignKey(User, on_delete=models.DO_NOTHING)
     data_comentario = models.DateTimeField(default=timezone.now)
