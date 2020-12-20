2a7ff1ccd751ef471ca5401f94743a062f21dcac
William Goulart Pacheco <gtaskwill@gmail.com>
2020-12-20 15:45:20 -0300

    #

--- python/projeto06/comentarios/forms.py
+++ python/projeto06/comentarios/forms.py
@@ -2,6 +2,12 @@ from django.forms import ModelForm
 from .models import Comentario
 
 class FormComentario(ModelForm):
+
+    def clean(self):
+        data = self.cleaned_data
+
+        print(data)
+
     class Meta:
         model = Comentario
         fields = (
