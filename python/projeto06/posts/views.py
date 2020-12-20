7a316f496255daf083720707d99c30904e72fde0
William Goulart Pacheco <gtaskwill@gmail.com>
2020-12-20 14:26:11 -0300

    #

--- python/projeto06/posts/views.py
+++ python/projeto06/posts/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.views.generic.list import ListView
 from django.views.generic.edit import UpdateView
 from .models import Post
+from django.db.models import Q, Count, Case, When
 
 # Create your views here.
 
@@ -10,6 +11,21 @@ class PostIndex(ListView) :
     template_name = 'posts/index.html'
     paginate_by = 2
     context_object_name = 'posts'
+
+    def get_queryset(self):
+        qs = super().get_queryset()
+        qs = qs.order_by('-id')
+
+        qs = qs.annotate(
+            numero_comentarios=Count(
+                Case(
+                    When(comentario__publicado_comentario=True, then=1)
+                )
+            )
+        )
+
+        return qs
+
 
 class PostBusca(PostIndex) :
     pass
