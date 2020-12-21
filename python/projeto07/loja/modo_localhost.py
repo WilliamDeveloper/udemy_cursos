f20884cf4aa4c936ed182dc73c5251f04ae8a0cb
William Goulart Pacheco <gtaskwill@gmail.com>
2020-12-21 09:22:12 -0300

    #

--- python/projeto07/loja/modo_localhost.py
+++ python/projeto07/loja/modo_localhost.py
@@ -55,8 +55,8 @@ MESSAGE_TAGS ={
 }
 
 # sumernote
-INSTALLED_APPS += ('django_summernote',)
-X_FRAME_OPTIONS = 'SAMEORIGIN'
+# INSTALLED_APPS += ('django_summernote',)
+# X_FRAME_OPTIONS = 'SAMEORIGIN'
 
 # humanize
 INSTALLED_APPS += ('django.contrib.humanize',)
