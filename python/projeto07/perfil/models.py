2ce2fdf4bdf693602c8683e00776c50c30f6d1e2
William Goulart Pacheco <gtaskwill@gmail.com>
2020-12-23 15:39:38 -0300

    #

--- python/projeto07/perfil/models.py
+++ python/projeto07/perfil/models.py
@@ -36,5 +36,13 @@ class Perfil(models.Model):
             ('PE', 'Pernambuco'),
             ('PI', 'Piauí'),
             ('RJ', 'Rio de Janeiro'),
+            ('RN', 'Rio Grande do Norte'),
+            ('RS', 'Rio Grande do Sul'),
+            ('RO', 'Rondônia'),
+            ('RR', 'Roraima'),
+            ('SC', 'Santa Catarina'),
+            ('SP', 'São Paulo'),
+            ('SE', 'Sergipe'),
+            ('TO', 'Tocantins'),
         )
     )
