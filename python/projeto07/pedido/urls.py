dab72fd9d6cdf20d1d795fa47b2af7bdb6ef7f45
William Goulart Pacheco <gtaskwill@gmail.com>
2020-12-24 14:14:22 -0300

    #

--- python/projeto07/pedido/urls.py
+++ python/projeto07/pedido/urls.py
@@ -1,5 +1,8 @@
 from django.urls import path, include
 
+# para sevir de namespace
+app_name='pedido'
+
 urlpatterns = [
 
 ]
--- python/projeto07/perfil/urls.py
+++ python/projeto07/perfil/urls.py
@@ -1,5 +1,9 @@
 from django.urls import path, include
+from . import views
+
+# para sevir de namespace
+app_name='perfil'
 
 urlpatterns = [
-
+    path('',views.Criar.as_view(), name='')
 ]
