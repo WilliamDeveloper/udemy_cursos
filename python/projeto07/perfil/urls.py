423bfcf1fa159794b0fc7e72176ae87e3d4dea2b
William Goulart Pacheco <gtaskwill@gmail.com>
2020-12-24 14:17:09 -0300

    #

--- python/projeto07/perfil/urls.py
+++ python/projeto07/perfil/urls.py
@@ -5,5 +5,8 @@ from . import views
 app_name='perfil'
 
 urlpatterns = [
-    path('',views.Criar.as_view(), name='')
+    path('',views.Criar.as_view(), name='criar'),
+    path('',views.Atualizar.as_view(), name='atualizar'),
+    path('',views.Login.as_view(), name='login'),
+    path('',views.Logout.as_view(), name='logout'),
 ]
