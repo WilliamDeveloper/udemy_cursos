d36bfbf557ab6dfaf538c8f247354b98ae25a26e
William Goulart Pacheco <gtaskwill@gmail.com>
2020-12-24 15:01:24 -0300

    #

--- python/projeto07/produto/views.py
+++ python/projeto07/produto/views.py
@@ -2,11 +2,14 @@ from django.shortcuts import render
 from django.views.generic.list import ListView
 from django.views import View
 from django.http import HttpResponse
+from . import models
 
 # Create your views here.
-class ProdutosLista(View):
-    def get(self,*args,**kwargs):
-        return HttpResponse('ProdutosLista')
+class ProdutosLista(ListView):
+    model = models.Produto
+    template_name = 'produto/lista.html'
+    # def get(self,*args,**kwargs):
+    #     return HttpResponse('ProdutosLista')
 
 class ProdutoDetalhe(View):
     def get(self,*args,**kwargs):
