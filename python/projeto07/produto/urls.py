d9190a0968855896b9074fa94382652822d87a07
William Goulart Pacheco <gtaskwill@gmail.com>
2020-12-24 14:11:34 -0300

    #

--- python/projeto07/produto/urls.py
+++ python/projeto07/produto/urls.py
@@ -4,5 +4,10 @@ from . import views
 app_name = 'produto'
 
 urlpatterns = [
-    path('',views.ListaProdutos.as_view(), name='lista'),
+    path('',views.ProdutosLista.as_view(), name='lista'),
+    path('<slug>',views.ProdutoDetalhe.as_view(), name='detalhe'),
+    path('carrinho_adicionar/',views.CarrinhoAdicionar.as_view(), name='carrinho_adicionar'),
+    path('carrinho_remover/',views.CarrinhoRemover.as_view(), name='carrinho_remover'),
+    path('carrinho/',views.Carrinho.as_view(), name='carrinho'),
+    path('carrinho_finalizar/',views.CarrinhoFinalizar.as_view(), name='carrinho_finalizar'),
 ]
--- python/projeto07/produto/views.py
+++ python/projeto07/produto/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
+from django.views.generic.list import ListView
+from django.views import View
 
 # Create your views here.
+class ProdutosLista(ListView):
+    pass
+
+class ProdutoDetalhe(View):
+    pass
+
+class CarrinhoAdicionar(View):
+    pass
+
+class CarrinhoRemover(View):
+    pass
+
+class Carrinho(View):
+    pass
+
+class CarrinhoFinalizar(View):
+    pass
