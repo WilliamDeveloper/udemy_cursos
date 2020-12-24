ff9108d8922c18824aa43b196fec8597fc152632
William Goulart Pacheco <gtaskwill@gmail.com>
2020-12-24 14:22:58 -0300

    #

--- python/projeto07/pedido/urls.py
+++ python/projeto07/pedido/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path, include
+from . import views
 
 # para sevir de namespace
 app_name='pedido'
 
 urlpatterns = [
-
+    path('', views.Pagar.as_view(), name='pagar'),
+    path('fechar_pedido/', views.FecharPedido.as_view(), name='fechar_pedido'),
+    path('detalhe/', views.Detalhe.as_view(), name='detalhe'),
 ]
