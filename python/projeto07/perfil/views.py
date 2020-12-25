eee3a53cda058b6b6731b31144fc6ea6d3db3cff
William Goulart Pacheco <gtaskwill@gmail.com>
2020-12-24 21:31:06 -0300

    #

--- python/projeto07/perfil/views.py
+++ python/projeto07/perfil/views.py
@@ -3,12 +3,35 @@ from django.views.generic import ListView
 from django.views import View
 from django.http import HttpResponse
 
-# Create your views here.
-class Criar(View):
-    def get(self,*args,**kwargs):
-        return HttpResponse('Criar')
+from . import models
+from . import forms
 
-class Atualizar(View):
+# Create your views here.
+class BasePerfil(View):
+    template_name = 'perfil/criar.html'
+
+    def setup(self, *args, **kwargs):
+        super().setup(*args, **kwargs)
+
+        self.contexto={
+            'userForm': forms.UserForm(
+                data=self.request.POST or None,
+            ),
+            'perfilForm': forms.PerfilForm(
+                data=self.request.POST or None,
+            )
+        }
+        self.renderizar = render(self.request, self.template_name, self.contexto)
+
+
+    def get(self, *args, **kwargs):
+        return self.renderizar
+
+
+class Criar(BasePerfil):
+    pass
+
+class Atualizar(BasePerfil):
     def get(self,*args,**kwargs):
         return HttpResponse('Atualizar')
 
