7d1758f63553023d28615428f6227d4085b7bd47
William Goulart Pacheco <gtaskwill@gmail.com>
2020-12-26 13:18:48 -0300

    #

--- python/projeto07/pedido/urls.py
+++ python/projeto07/pedido/urls.py
@@ -9,5 +9,5 @@ urlpatterns = [
     path('pagar/<int:pk>', views.Pagar.as_view(), name='pagar'),
     path('salvar_pedido/', views.SalvarPedido.as_view(), name='salvar_pedido'),
     path('lista/', views.Lista.as_view(), name='lista'),
-    path('detalhe/', views.Detalhe.as_view(), name='detalhe'),
+    path('detalhe/<int:pk>', views.Detalhe.as_view(), name='detalhe'),
 ]
