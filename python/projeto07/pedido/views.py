5dff26ceb8185732ae849fa88db87f4d8c2a23cf
William Goulart Pacheco <gtaskwill@gmail.com>
2020-12-25 22:06:10 -0300

    #

--- python/projeto07/pedido/views.py
+++ python/projeto07/pedido/views.py
@@ -1,12 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views.generic import ListView
 from django.views import View
 from django.http import HttpResponse
 
+from django.contrib import messages
+
 # Create your views here.
 class Pagar(View) :
+    template_name = 'pedido/pagar.html'
+
     def get(self,*args,**kwargs):
-        return HttpResponse('Pagar')
+        contexto={
+
+        }
+        return render(self.request, self.template_name, contexto)
 
 
 class SalvarPedido(View) :
