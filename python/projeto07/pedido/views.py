9107dfa5e53282e268bc54249a540a5250583f2d
William Goulart Pacheco <gtaskwill@gmail.com>
2020-12-26 10:18:02 -0300

    #

--- python/projeto07/pedido/views.py
+++ python/projeto07/pedido/views.py
@@ -36,7 +36,39 @@ class Pagar(View) :
         bd_variacoes = Variacao.objects.select_related('produto').filter(id__in= carrinho_variacao_ids)
         bd_variacoes = list(bd_variacoes)
 
+
+
         print(bd_variacoes)
+
+        for variacao in bd_variacoes:
+            vid = str(variacao.id)
+            estoque = variacao.estoque
+            qtd_carrinho =carrinho[vid]['quantidade']
+
+            preco_unitario = carrinho[vid]['preco_unitario']
+            preco_unitario_promocional = carrinho[vid]['preco_unitario_promocional']
+
+            error_msg_estoque=''
+
+            if estoque < qtd_carrinho:
+                carrinho[vid]['quantidade'] = estoque
+                carrinho[vid]['preco_quantitativo'] = estoque * preco_unitario
+                carrinho[vid]['preco_unitario_promocional'] = estoque * preco_unitario_promocional
+
+                error_msg_estoque = """
+                            Estoque insuficiente para alguns produtos do seu carrinho.
+                            Reduzimos a quantidade dessses produtos.
+                            Por favor revise os produtos no carrinho.
+                        """
+
+                if error_msg_estoque:
+                    messages.error(
+                        self.request,
+                        error_msg_estoque
+                    )
+                    self.request.session.save()
+
+                    return redirect('produto:carrinho')
 
         contexto={
 
