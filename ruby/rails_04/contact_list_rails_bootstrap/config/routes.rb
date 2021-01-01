c0a698129f92b7d72401aaefaf210b5dcc7e8084
William Goulart Pacheco <gtaskwill@gmail.com>
2021-01-01 19:50:11 -0300

    #

--- ruby/rails_04/contact_list_rails_bootstrap/config/routes.rb
+++ ruby/rails_04/contact_list_rails_bootstrap/config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :contacts
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   #nome_do_controller + nome_da_action(funcao no controller)
