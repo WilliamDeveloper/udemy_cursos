c9312f0f9407ff50284ae0ee7b2336973e143174
William Goulart Pacheco <gtaskwill@gmail.com>
2021-01-02 12:18:59 -0300

    #

--- ruby/rails_04/contact_list_rails_bootstrap/app/controllers/users_controller.rb
+++ ruby/rails_04/contact_list_rails_bootstrap/app/controllers/users_controller.rb
@@ -3,8 +3,12 @@ class UsersController < ApplicationController
   before_action :require_logged_in_user, only: [:show, :edit, :update]
 
   def new
-    redirect_to current_user if user_signed_in?
+
     @user = User.new
+  end
+
+  def show
+    redirect_to root_url
   end
 
   def create
