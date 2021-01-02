be968394067aba29cee09821bcab78746b22fd9c
William Goulart Pacheco <gtaskwill@gmail.com>
2021-01-02 12:07:54 -0300

    #

--- ruby/rails_04/contact_list_rails_bootstrap/app/controllers/users_controller.rb
+++ ruby/rails_04/contact_list_rails_bootstrap/app/controllers/users_controller.rb
@@ -1,6 +1,9 @@
 class UsersController < ApplicationController
 
+  before_action :require_logged_in_user, only: [:show, :edit, :update]
+
   def new
+    redirect_to current_user if user_signed_in?
     @user = User.new
   end
 
@@ -11,6 +14,18 @@ class UsersController < ApplicationController
       redirect_to root_url
     else
       render 'new'
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if current_user.update(user_params)
+      flash[:success] = 'Dados atualizados com sucesso'
+      redirect_to user_contacts_url(current_user)
+    else
+      render 'edit'
     end
   end
 
--- ruby/rails_04/contact_list_rails_bootstrap/config/routes.rb
+++ ruby/rails_04/contact_list_rails_bootstrap/config/routes.rb
@@ -4,16 +4,19 @@ Rails.application.routes.draw do
 
   #nome_do_controller + nome_da_action(funcao no controller)
   root to: "static_pages#index"
-
   get 'sobre', to: "static_pages#sobre"
-
   get 'contato', to: "static_pages#contato"
 
+  get 'cadastro', to: "users#new"
   get 'entrar', to: "sessions#new"
   post 'entrar', to: "sessions#create"
+  delete 'sair', to:'sessions#destroy'
 
 
-  resources :contacts
-  resources :users, only: [:new, :create, :show]
+  resources :users, only: [:show, :new, :create, :edit, :update] do
+    resources :contacts, only:[:index, :new, :create, :edit, :update, :destroy]
+  end
+
+  resources  :sessions, only: [:new, :create, :destroy]
 
 end
