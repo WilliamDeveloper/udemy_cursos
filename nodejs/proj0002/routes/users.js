fca071b8c4cb8609adcee55b81481e3ee26e5b77
William Goulart Pacheco <gtaskwill@gmail.com>
2021-01-30 18:52:47 -0300

    #

--- nodejs/proj0002/routes/users.js
+++ nodejs/proj0002/routes/users.js
@@ -15,4 +15,13 @@ router.post('/', (req,res) => {
     return res.send({message: 'Tudo ok com o método POST da rota de usuarios!'})
 });
 
+router.get('/create', (req,res) => {
+    let params = req.query;
+    let respostaJson ={
+        message: 'Tudo ok com o método GET da rota de usuarios/create!',
+        params : params
+    };
+    return res.send(respostaJson)
+});
+
 module.exports = router;
