4f54cda8c0cfc383554e714efd69f2cb25fd30cb
William Goulart Pacheco <gtaskwill@gmail.com>
2021-01-30 18:18:44 -0300

    #

--- nodejs/proj0001/index.js
+++ nodejs/proj0001/index.js
@@ -5,11 +5,11 @@ const porta = 3000;
 
 app.get('/', (req,res) => {
     return res.send({message: 'Tudo ok com o método GET!'})
-})
+});
 
 app.get('/', (req,res) => {
     return res.send({message: 'Tudo ok com o método POST!'})
-})
+});
 
 app.listen(porta);
 
