4ea1e435f1dce73ec4d3aa4322c518c8be3e99f6
William Goulart Pacheco <gtaskwill@gmail.com>
2021-01-30 18:13:54 -0300

    #

--- nodejs/proj0001/index.js
+++ nodejs/proj0001/index.js
@@ -3,6 +3,10 @@ const app = express();
 
 const porta = 3000;
 
+app.get('/', (res,req) => {
+    return res.send({message: 'Tudo ok com o método GET!'})
+})
+
 app.listen(porta);
 
 module.exports = app;
