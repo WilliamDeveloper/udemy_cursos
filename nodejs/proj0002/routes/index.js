bfea627aeadaab21ac1d00bd25315eec56ab11e1
William Goulart Pacheco <gtaskwill@gmail.com>
2021-01-30 18:46:07 -0300

    #

--- nodejs/proj0002/routes/index.js
+++ nodejs/proj0002/routes/index.js
@@ -1,4 +1,7 @@
-app.get('/', (req,res) => {
+const express = require('express');
+const router = express.Router();
+
+router.get('/', (req,res) => {
     let params = req.query;
     let respostaJson ={
         message: 'Tudo ok com o método GET!',
@@ -7,7 +10,10 @@ app.get('/', (req,res) => {
     return res.send(respostaJson)
 });
 
-app.post('/', (req,res) => {
+router.post('/', (req,res) => {
     let params = req.query;
     return res.send({message: 'Tudo ok com o método POST!'})
 });
+
+module.exports = router;
+
