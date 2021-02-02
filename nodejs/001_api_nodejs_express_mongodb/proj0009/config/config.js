307b1535b03e0ff4dfae5d9c112b92cfdb301971
William Goulart Pacheco <gtaskwill@gmail.com>
2021-02-02 14:22:16 -0300

    #

--- nodejs/001_api_nodejs_express_mongodb/proj0009/config/config.js
+++ nodejs/001_api_nodejs_express_mongodb/proj0009/config/config.js
@@ -4,17 +4,20 @@ const config = () =>{
     switch (env) {
         case 'dev':
         return {
-            bd_string: "mongodb+srv://usuario_admin:senha_admin@clusterapi.yh7yq.mongodb.net/data_base_name?retryWrites=true&w=majority"
+            bd_string: "mongodb+srv://usuario_admin:senha_admin@clusterapi.yh7yq.mongodb.net/data_base_name?retryWrites=true&w=majority",
+            jwt_pass: 'batatafrita2019'
         }
 
         case 'hml':
         return {
-            bd_string: "mongodb+srv://usuario_admin:senha_admin@clusterapi.yh7yq.mongodb.net/data_base_name?retryWrites=true&w=majority"
+            bd_string: "mongodb+srv://usuario_admin:senha_admin@clusterapi.yh7yq.mongodb.net/data_base_name?retryWrites=true&w=majority",
+            jwt_pass: 'batatafrita2019'
         }
 
         case 'prod':
         return {
-            bd_string: "mongodb+srv://usuario_admin:senha_admin@clusterapi.yh7yq.mongodb.net/data_base_name?retryWrites=true&w=majority"
+            bd_string: "mongodb+srv://usuario_admin:senha_admin@clusterapi.yh7yq.mongodb.net/data_base_name?retryWrites=true&w=majority",
+            jwt_pass: 'batatafrita2019'
         }
 
     }
--- nodejs/001_api_nodejs_express_mongodb/proj0009/middlewares/auth.js
+++ nodejs/001_api_nodejs_express_mongodb/proj0009/middlewares/auth.js
@@ -1,11 +1,12 @@
 const jwt = require('jsonwebtoken');
+const config = require('../config/config');
 
 const auth = (req, res, next) =>{
     const token_header = req.headers.auth;
 
     if(!token_header) return res.status(401).send({ error:'token nao enviado!'});
 
-    jwt.verify(token_header, 'batatafrita2019', (err, decoded) =>{
+    jwt.verify(token_header, config.jwt_pass, (err, decoded) =>{
         if(err) return res.status(401).send({error: 'token invalido'});
         res.locals.auth_data = decoded;
         return next();
