213990a76a806d6a0e505c75ed54b718db60c882
William Goulart Pacheco <gtaskwill@gmail.com>
2021-02-02 12:49:37 -0300

    #

--- nodejs/001_api_nodejs_express_mongodb/proj0007/routes/users.js
+++ nodejs/001_api_nodejs_express_mongodb/proj0007/routes/users.js
@@ -2,13 +2,12 @@ const express = require('express');
 const router = express.Router();
 const Users = require('../model/user');
 const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 
-// router.get('/', (req,res) => {
-//     Users.find({},(err, data) => {
-//         if (err)  return res.send({error:'Erro na consulta de usuarios!'});
-//         return res.send(data);
-//     })
-// });
+//funcoes auxiliares
+const createUserToken = (userId) =>{
+    return jwt.sign( {id:userId }, 'batatafrita2019', { expiresIn: '7d' });
+};
 
 router.get('/', async (req,res) => {
     try{
@@ -26,32 +25,7 @@ router.post('/', (req,res) => {
     return res.send({message: 'Tudo ok com o método POST da rota de usuarios!'})
 });
 
-//localhost:3000/users/create [Content-Type: application/json]
-// {
-//     "email": "will@will.com.br",
-//     "password": "123456"
-// }
-// router.post('/create', (req,res) => {
-//     let obj = req.body;
-//     console.log(obj);
-//
-//     const {email, password} = obj;
-//
-//     if(!email || !password) return res.send({error: 'dados insuficientes'});
-//
-//     Users.findOne({email:email}, (err, data) => {
-//        if (err) return res.send({error: 'erro ao buscar usuario'});
-//        if (data) return res.send({error: 'usuario ja registrado'});
-//
-//        Users.create({email:email, password:password}, (err, data)=>{
-//            if(err) return res.send({error: 'erro ao criar usuario!'});
-//
-//            data.password = undefined;
-//            return res.send(data);
-//
-//        });
-//     });
-// });
+
 
 router.post('/create', async (req,res) => {
     let obj = req.body;
@@ -66,35 +40,13 @@ router.post('/create', async (req,res) => {
 
         const user = await Users.create(obj);
         user.password = undefined;
-        return res.send(user);
+
+        return res.send({ user, token: createUserToken(user.id) });
 
     }catch(err){
         return res.send({error:'erro ao buscar usuario'});
     }
 });
-
-// router.post('/auth', (req,res)=>{
-//     let obj = req.body;
-//     console.log(obj);
-//
-//     const {email, password} = obj;
-//
-//     if(!email || !password) return res.send({error: 'dados insuficientes'});
-//
-//
-//     Users.findOne({email:email}, (err, data) => {
-//         if (err) return res.send({error: 'erro ao buscar usuario'});
-//         if (!data) return res.send({error: 'usuario nao registrado'});
-//
-//         bcrypt.compare(password,data.password, (err, same)=>{
-//             if (!same) return res.send({error: 'Error ao autenticar usuario!'});
-//
-//             data.password = undefined;
-//             return res.send(data);
-//         });
-//
-//     }).select('+password');
-// })
 
 router.post('/auth', async (req,res)=>{
     let obj = req.body;
@@ -114,7 +66,7 @@ router.post('/auth', async (req,res)=>{
         if(!pass_ok) return res.send({error: 'Error ao autenticar usuario!'});
 
         user.password = undefined;
-        return res.send(user);
+        return res.send({ user, token: createUserToken(user.id) });
 
     }catch(err){
         return res.send({error:'erro ao buscar usuario'});
