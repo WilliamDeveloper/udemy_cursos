62b6a3d81ca9c923a09dccf1519092e1e4b2c31d
William Goulart Pacheco <gtaskwill@gmail.com>
2021-02-02 11:54:39 -0300

    #

--- nodejs/001_api_nodejs_express_mongodb/proj0006/routes/users.js
+++ nodejs/001_api_nodejs_express_mongodb/proj0006/routes/users.js
@@ -3,12 +3,23 @@ const router = express.Router();
 const Users = require('../model/user');
 const bcrypt = require('bcrypt');
 
-router.get('/', (req,res) => {
-    Users.find({},(err, data) => {
-        if (err)  return res.send({error:'Erro na consulta de usuarios!'});
+// router.get('/', (req,res) => {
+//     Users.find({},(err, data) => {
+//         if (err)  return res.send({error:'Erro na consulta de usuarios!'});
+//         return res.send(data);
+//     })
+// });
+
+router.get('/', async (req,res) => {
+    try{
+        const users = await Users.find({})
         return res.send(data);
-    })
+    }catch(err){
+        return res.send({error:'Erro na consulta de usuarios!'});
+    }
 });
+
+
 
 router.post('/', (req,res) => {
     let params = req.query;
