3d508a0ecbb29618362ffdbc0978bf8f8830ea2a
William Goulart Pacheco <gtaskwill@gmail.com>
2021-02-02 11:11:55 -0300

    #

---
+++ nodejs/001_api_nodejs_express_mongodb/proj0005/readme.md
@@ -0,0 +1,27 @@
+- criar diretorio vazio
+````
+mkdir proj0005
+````
+
+- inicializar projeto node
+ ````
+ npm init
+ ````
+ 
+ - bibliotecas de api
+ ````
+ npm install express --save
+ npm install mongoose --save
+ npm install body-parser --save
+ ````
+ 
+ - instalar dependencias do projeto
+ ````
+ npm install
+ ````
+ 
+ - subir aplicacao
+ ````
+ cd proj0005
+ node app.js
+ ````
---
+++ nodejs/001_api_nodejs_express_mongodb/proj0005/routes/users.js
@@ -0,0 +1,44 @@
+const express = require('express');
+const router = express.Router();
+const Users = require('../model/user');
+
+router.get('/', (req,res) => {
+    Users.find({},(err, data) => {
+        if (err)  return res.send({error:'Erro na consulta de usuarios!'});
+        return res.send(data);
+    })
+});
+
+router.post('/', (req,res) => {
+    let params = req.query;
+    return res.send({message: 'Tudo ok com o método POST da rota de usuarios!'})
+});
+
+//localhost:3000/users/create [Content-Type: application/json]
+// {
+//     "email": "will@will.com.br",
+//     "password": "123456"
+// }
+router.post('/create', (req,res) => {
+    let obj = req.body;
+    console.log(obj);
+
+    const {email, password} = obj;
+
+    if(!email || !password) return res.send({error: 'dados insuficientes'});
+
+    Users.findOne({email:email}, (err, data) => {
+       if (err) return res.send({error: 'erro ao buscar usuario'});
+       if (data) return res.send({error: 'usuario ja registrado'});
+
+       Users.create({email:email, password:password}, (err, data)=>{
+           if(err) return res.send({error: 'erro ao criar usuario!'});
+
+           data.password = undefined;
+           return res.send(data);
+
+       });
+    });
+});
+
+module.exports = router;
