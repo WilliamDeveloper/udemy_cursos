5dfb5125a49525af48650c26aa8e94ad712e3889
William Goulart Pacheco <gtaskwill@gmail.com>
2021-02-02 13:58:42 -0300

    #

--- nodejs/001_api_nodejs_express_mongodb/proj0008/middlewares/auth.js
+++ nodejs/001_api_nodejs_express_mongodb/proj0008/middlewares/auth.js
@@ -3,10 +3,10 @@ const jwt = require('jsonwebtoken');
 const auth = (req, res, next) =>{
     const token_header = req.headers.auth;
 
-    if(!token_header) return res.send({ error:'token nao enviado!'});
+    if(!token_header) return res.status(401).send({ error:'token nao enviado!'});
 
     jwt.verify(token_header, 'batatafrita2019', (err, decoded) =>{
-        if(err) return res.send({error: 'token invalido'});
+        if(err) return res.status(401).send({error: 'token invalido'});
         res.locals.auth_data = decoded;
         return next();
     });
