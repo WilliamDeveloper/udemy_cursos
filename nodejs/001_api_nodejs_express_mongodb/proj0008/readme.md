dcadfd212a9d76ada3931bebfb2bab30813862cf
William Goulart Pacheco <gtaskwill@gmail.com>
2021-02-02 13:19:37 -0300

    #

---
+++ nodejs/001_api_nodejs_express_mongodb/proj0008/readme.md
@@ -0,0 +1,29 @@
+- criar diretorio vazio
+````
+mkdir proj0008
+````
+
+- inicializar projeto node
+ ````
+ npm init
+ ````
+ 
+ - bibliotecas de api
+ ````
+ npm install express --save
+ npm install mongoose --save
+ npm install body-parser --save
+ npm install bcrypt --save
+ npm install jsonwebtoken --save
+ ````
+ 
+ - instalar dependencias do projeto
+ ````
+ npm install
+ ````
+ 
+ - subir aplicacao
+ ````
+ cd proj0008
+ node app.js
+ ````
---
+++ nodejs/001_api_nodejs_express_mongodb/proj0008/routes/users.js
@@ -0,0 +1,91 @@
+const express = require('express');
+const router = express.Router();
+const Users = require('../model/user');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+const auth = require('../middlewares/auth');
+
+//funcoes auxiliares
+const createUserToken = (userId) =>{
+    return jwt.sign( {id:userId }, 'batatafrita2019', { expiresIn: '7d' });
+};
+
+// essa url so vai devolver os dados que o token valido mandado junto na requisicao
+//localhost:3000/users/info_privada
+router.get('/info_privada', auth, (req,res) => {
+    try{
+        console.log(res.locals.auth_data)
+        const users = {nome:'william', idade:18};
+        return res.send(users);
+    }catch(err){
+        return res.send({error:'Erro na consulta de usuarios!'});
+    }
+});
+
+
+router.get('/', async (req,res) => {
+    try{
+        const users = await Users.find({})
+        return res.send(data);
+    }catch(err){
+        return res.send({error:'Erro na consulta de usuarios!'});
+    }
+});
+
+
+
+router.post('/', (req,res) => {
+    let params = req.query;
+    return res.send({message: 'Tudo ok com o método POST da rota de usuarios!'})
+});
+
+
+
+router.post('/create', async (req,res) => {
+    let obj = req.body;
+    console.log(obj);
+
+    const {email, password} = obj;
+
+    if(!email || !password) return res.send({error: 'dados insuficientes'});
+
+    try{
+        if (await Users.findOne({email:email})) return res.send({error: 'usuario ja registrado'});
+
+        const user = await Users.create(obj);
+        user.password = undefined;
+
+        return res.send({ user, token: createUserToken(user.id) });
+
+    }catch(err){
+        return res.send({error:'erro ao buscar usuario'});
+    }
+});
+
+router.post('/auth', async (req,res)=>{
+    let obj = req.body;
+    console.log(obj);
+
+    const {email, password} = obj;
+
+    if(!email || !password) return res.send({error: 'dados insuficientes'});
+
+    try{
+        const user = await Users.findOne({email:email}).select('+password');
+
+        if (!user) return res.send({error: 'usuario nao registrado'});
+
+        const pass_ok = await bcrypt.compare(password,user.password);
+
+        if(!pass_ok) return res.send({error: 'Error ao autenticar usuario!'});
+
+        user.password = undefined;
+        return res.send({ user, token: createUserToken(user.id) });
+
+    }catch(err){
+        return res.send({error:'erro ao buscar usuario'});
+    }
+});
+
+
+module.exports = router;
