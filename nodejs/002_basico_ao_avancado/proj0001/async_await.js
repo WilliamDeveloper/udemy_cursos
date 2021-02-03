4eb8ebd48e8f4b975a378df54683c17abcc3f60c
William Goulart Pacheco <gtaskwill@gmail.com>
2021-02-03 09:25:15 -0300

    #

---
+++ nodejs/002_basico_ao_avancado/proj0001/async_await.js
@@ -0,0 +1,17 @@
+function  sum (x) {
+    return new Promise((resolve, reject)=>{
+        setTimeout( ()=>{
+            resolve(x+5000);
+        }, 3000)
+    })
+}
+
+async function main() {
+
+    try {
+        const resultado = await sum(230);
+        console.log(`Resultado com async/await: ${resultado}`);
+    }catch (e) {
+        console.log(`temos problemas: ${e}`);
+    }
+}
