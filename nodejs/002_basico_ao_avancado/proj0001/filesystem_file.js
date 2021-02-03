cb7153e6ea07ca8c8be37779d3d944c276a0a30e
William Goulart Pacheco <gtaskwill@gmail.com>
2021-02-03 13:14:22 -0300

    #

--- nodejs/002_basico_ao_avancado/proj0001/filesystem_file.js
+++ nodejs/002_basico_ao_avancado/proj0001/filesystem_file.js
@@ -18,18 +18,37 @@
 //         console.log('erro')
 //     })
 
-const fs = require('fs');
-const {writeFile} = require('fs');
+// const fs = require('fs');
+// const {writeFile} = require('fs');
+//
+// function criaArquivo(name, content) {
+//     return new Promise((resolve, reject) => {
+//         writeFile(name, content, (err)=>{
+//             if(err) return reject(err);
+//             resolve()
+//         })
+//     });
+// }
+//
+// criaArquivo('arquivo2.txt', 'pimba blau')
+// .then( () => console.log('arquivo criado com sucesso'))
+// .catch(err => console.log('erro'));
 
-function criaArquivo(name, content) {
-    return new Promise((resolve, reject) => {
-        writeFile(name, content, (err)=>{
-            if(err) return reject(err);
-            resolve()
-        })
-    });
+const fs = require('fs').promises; // apartir do node 12
+async function read(name) {
+    const data = await fs.readFile(name, 'binary')
+    return new Buffer.from(data)
 }
 
-criaArquivo('arquivo2.txt', 'pimba blau')
-.then( () => console.log('arquivo criado com sucesso'))
-.catch(err => console.log('erro'));
+
+try{
+    read('arquivo2.txt')
+        .then((retorno)=>{
+            console.log('lido com sucesso: ', retorno)
+        })
+
+}catch (e) {
+    console.log('erro ao ler o arquivo')
+
+
+}
