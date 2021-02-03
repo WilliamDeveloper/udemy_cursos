8f87a1791e89c4956356fdb1ce781d4869db3e1a
William Goulart Pacheco <gtaskwill@gmail.com>
2021-02-03 12:02:39 -0300

    #

--- nodejs/002_basico_ao_avancado/proj0001/process.js
+++ nodejs/002_basico_ao_avancado/proj0001/process.js
@@ -3,4 +3,20 @@ console.log(`Diretorio do arquivo: `, __dirname);
 console.log(`parametros de excecucao: `,process.argv);
 console.log(`ambiente do sevidor: `,process.platform);
 
+let argumento_2 = process.argv[2];
+switch (argumento_2) {
+    case '-a':
+        console.log('faz isso');
+        break;
+    case '-i':
+        console.log('faz isso2');
+        break;
+    case '-q':
+        console.log('saindo');
+        break;
+    default:
+        console.log('parametro invalido');
+        break;
+}
+
 // node process.js 1 2 3 4
