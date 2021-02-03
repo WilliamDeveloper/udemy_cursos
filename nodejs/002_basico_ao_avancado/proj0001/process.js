88578bc7fb9954ae5d828e3dc1a377a80ed88817
William Goulart Pacheco <gtaskwill@gmail.com>
2021-02-03 12:14:45 -0300

    #

--- nodejs/002_basico_ao_avancado/proj0001/process.js
+++ nodejs/002_basico_ao_avancado/proj0001/process.js
@@ -1,8 +1,16 @@
 console.log(`Nome do arquivo: `,__filename);
 console.log(`Diretorio do arquivo: `, __dirname);
+console.log(`Diretorio do arquivo em que foi invocado: `, process.cwd());
 console.log(`parametros de excecucao: `,process.argv);
 console.log(`ambiente do sevidor: `,process.platform);
 console.log(`variaveis de ambiente: `,process.env);
+
+
+console.log(`variaveis de ambiente os : `,process.env.OS);
+console.log(`variaveis de ambiente USERNAME: `,process.env.USERNAME);
+console.log(`variaveis de ambiente LANG: `,process.env.LANG);
+console.log(`variaveis de ambiente LANG2: `,process.env.LANG2);
+console.log(`variaveis de ambiente COMPUTERNAME: `,process.env.COMPUTERNAME);
 
 let argumento_2 = process.argv[2];
 switch (argumento_2) {
