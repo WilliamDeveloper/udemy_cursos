da891cf4b3e35beca89ce2f981a4f3fc5dac6128
William Goulart Pacheco <gtaskwill@gmail.com>
2021-02-03 13:06:34 -0300

    #

--- nodejs/002_basico_ao_avancado/proj0001/filesystem_file.js
+++ nodejs/002_basico_ao_avancado/proj0001/filesystem_file.js
@@ -1,8 +1,33 @@
-const fs = require('fs');
+// const fs = require('fs');
+// const {writeFile} = require('fs');
+//
+// writeFile("arquivo.txt", "criando arquivo", (err)=>{
+//     if(err) throw err;
+//     console.log('arquivo escrito')
+// })
 
+
+// const {promisify} = require('util');
+// const {writeFilePromisify} = promisify(require('fs').writeFile);
+//
+// writeFilePromisify("arquivoPromisify.txt", 'sjkdhaijsdhasidhjauish')
+//     .then((resultado)=>{
+//         console.log('sucessso')
+//     })
+//     .catch((err)=>{
+//         console.log('erro')
+//     })
+
+const fs = require('fs');
 const {writeFile} = require('fs');
 
-writeFile("arquivo.txt", "criando arquivo", (err)=>{
-    if(err) throw err;
-    console.log('arquivo escrito')
-})
+function criaArquivo(name, content) {
+    return new Promise((resolve, reject) => {
+        writeFile(name, content, (err)=>{
+            if(err) return reject(err);
+            resolve()
+        })
+    });
+}
+
+criaArquivo('arquivo2.txt', 'pimba blau')
