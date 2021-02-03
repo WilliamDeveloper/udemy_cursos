f7597ae4cd018f811893410ddd63cf2114f70eca
William Goulart Pacheco <gtaskwill@gmail.com>
2021-02-03 12:00:09 -0300

    #

---
+++ nodejs/002_basico_ao_avancado/proj0001/process.js
@@ -0,0 +1,6 @@
+console.log(`Nome do arquivo: `,__filename);
+console.log(`Diretorio do arquivo: `, __dirname);
+console.log(`parametros de excecucao: `,process.argv);
+console.log(`ambiente do sevidor: `,process.platform);
+
+// node process.js 1 2 3 4
