5c4ad78aa5be1d3bd385986cb891cc426fee5d6e
William Goulart Pacheco <gtaskwill@gmail.com>
2021-02-03 13:20:14 -0300

    #

--- nodejs/002_basico_ao_avancado/proj0001/http_index.js
+++ nodejs/002_basico_ao_avancado/proj0001/http_index.js
@@ -4,11 +4,21 @@ const hostname = "127.0.0.1";
 const port = 3000;
 const url = `http://${hostname}:${port}/`
 
+let conteudo = ''
+
+const {readFile} = require('fs');
+
+readFile('basico_exemplo.html', (err, data)=>{
+    if(err) throw err
+    conteudo = data;
+})
+
 const server  = http.createServer((req,res) =>{
     res.statusCode = 200;
     // res.setHeader('Content-Type','text/plain'); //padrao
     res.setHeader('Content-Type','text/html');
-    res.end("<h1>Olá mundo <br/> Meu primeiro Script </h1>");
+    // res.end("<h1>Olá mundo <br/> Meu primeiro Script </h1>");
+    res.end(conteudo);
 });
 
 server.listen(port, hostname, ()=>{
