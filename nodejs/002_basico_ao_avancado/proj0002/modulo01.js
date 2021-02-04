71088bf099ee10b77bcf204b1945b8232e8117c4
William Goulart Pacheco <gtaskwill@gmail.com>
2021-02-04 09:08:57 -0300

    #

---
+++ nodejs/002_basico_ao_avancado/proj0002/modulo01.js
@@ -0,0 +1,9 @@
+console.log('excecutando modulo01.js')
+
+const oculta = () =>{
+    console.log('excecutando funcao oculta')
+}
+
+module.exports.executa = ()=>{
+    console.log('excecutando a executa')
+}
