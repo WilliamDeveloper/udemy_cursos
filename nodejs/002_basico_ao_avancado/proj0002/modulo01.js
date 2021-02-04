a76a932e501d7b5074f891bae5150132c9d8afe6
William Goulart Pacheco <gtaskwill@gmail.com>
2021-02-04 09:38:04 -0300

    #

--- nodejs/002_basico_ao_avancado/proj0002/modulo01.js
+++ nodejs/002_basico_ao_avancado/proj0002/modulo01.js
@@ -2,11 +2,18 @@ console.log('excecutando modulo01.js')
 
 // funcao privada usavel somente ao arquivo atual
 // pois nao foi dado "module.exports"
+const oculta2 = () =>{
+    console.log('excecutando funcao oculta')
+}
+
 const oculta = () =>{
     console.log('excecutando funcao oculta')
 }
 
-// faz a funcao poder ser publica para outros arquivos q importar o modulo
-module.exports.executa = ()=>{
+
+const executa = ()=>{
     console.log('excecutando a executa')
 }
+
+// faz a funcao poder ser publica para outros arquivos q importar o modulo
+module.exports = { oculta, executa}
