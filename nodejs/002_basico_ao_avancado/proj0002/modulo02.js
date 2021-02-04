688295931a012a8e0f185e069252089584a49719
William Goulart Pacheco <gtaskwill@gmail.com>
2021-02-04 09:49:46 -0300

    #

--- nodejs/002_basico_ao_avancado/proj0002/modulo02.js
+++ nodejs/002_basico_ao_avancado/proj0002/modulo02.js
@@ -1,6 +1,15 @@
 // require("./modulo01.js")
-const module01 = require("./modulo01")
 
-module01.executa()
+// const module01 = require("./modulo01")
+//
+// module01.executa()
+//
+// module01.oculta()
 
-module01.oculta()
+
+
+const {executa, oculta} = require("./modulo01")
+
+executa()
+
+oculta()
