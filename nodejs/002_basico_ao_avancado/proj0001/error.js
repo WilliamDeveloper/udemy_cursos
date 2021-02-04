aa50aafc260a79702e73c939942224e1dd45f148
William Goulart Pacheco <gtaskwill@gmail.com>
2021-02-03 23:04:49 -0300

    #

--- nodejs/002_basico_ao_avancado/proj0001/error.js
+++ nodejs/002_basico_ao_avancado/proj0001/error.js
@@ -3,3 +3,20 @@ try{
 }catch(e){
     console.log(`temos um problema! ${e.message}`)
 }
+
+const {EventEmitter} = require('events')
+const emitter = new EventEmitter()
+
+const validaObjeto = (a) =>{
+    if(typeof a !== 'object'){
+        emitter.emit('error', new Error('tipo informado invalido'))
+    }
+}
+
+emitter.addListener('error',(err) =>{
+    console.log('Evento: '+err.message)
+})
+
+let dados = {    name:'hcode',course:'nodejs'}
+dados=4
+validaObjeto(dados)
