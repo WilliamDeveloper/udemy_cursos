f5f2b944213e3da088afbb7830d2ae947b0c81e9
William Goulart Pacheco <gtaskwill@gmail.com>
2021-02-03 22:18:54 -0300

    #

--- nodejs/002_basico_ao_avancado/proj0001/child_process.js
+++ nodejs/002_basico_ao_avancado/proj0001/child_process.js
@@ -1,6 +1,8 @@
+
+// usar bash do git
 const {spawn} = require('child_process')
 
-const ls = spawn('ls',['..'])
+const ls = spawn('ls',['..', '-lh','/usr'])
 
 ls.stdout.on('data',(data)=>{
     console.log(`stdout: ${data}`)
