2b6659f1bffa64bf98cb99cc100cbef2d7f92706
William Goulart Pacheco <gtaskwill@gmail.com>
2021-02-04 13:34:06 -0300

    #

---
+++ nodejs/002_basico_ao_avancado/proj0003/video.js
@@ -0,0 +1,33 @@
+const {spawn} = require('child_process')
+
+function  resize(video, quality) {
+    const p = new Promise((resolve, reject)=>{
+        const binarioFfmpeg = "D:/_dev_/bin/ffmpeg/v4.3.1/bin/ffmpeg"
+
+        const ffmpeg = spawn(binarioFfmpeg, [
+            '-i',
+            `${parent}/${video}.mp4`,
+            '-codec:v',
+            'libx264',
+            '-profile:v',
+            'main',
+            '-preset',
+            'slow',
+            '-b:v',
+            '400k',
+            '-maxrate',
+            '400k',
+            '-bufsize',
+            '800k',
+            '-vf',
+            `scale=-2:${quality}`,
+            '-threads',
+            '0',
+            '-b:a',
+            '128k',
+            `${parent}/resultado/${video}-${quality}.mp4`
+
+
+        ])
+    })
+}
