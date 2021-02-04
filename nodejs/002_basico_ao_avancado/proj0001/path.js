24d2da523e19e572adbe7a0aabba5876c2595a27
William Goulart Pacheco <gtaskwill@gmail.com>
2021-02-03 22:30:09 -0300

    #

--- nodejs/002_basico_ao_avancado/proj0001/path.js
+++ nodejs/002_basico_ao_avancado/proj0001/path.js
@@ -1,3 +1,3 @@
 const path = require('path')
 
-console.log(path.basename)
+console.log(path.basename('D:/_dev_/work/github/udemy_cursos/nodejs/002_basico_ao_avancado/proj0001/basico_exemplo.html'))
