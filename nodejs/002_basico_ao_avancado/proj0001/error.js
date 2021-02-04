c1b89aebb456f1e08652621dc0d018f56c29f21a
William Goulart Pacheco <gtaskwill@gmail.com>
2021-02-03 22:56:50 -0300

    #

--- nodejs/002_basico_ao_avancado/proj0001/error.js
+++ nodejs/002_basico_ao_avancado/proj0001/error.js
@@ -1,1 +1,5 @@
-throw  new Error('A nao nao eh possive na minha maqina fonuncia')
+try{
+    throw  new Error('A nao nao eh possive na minha maqina fonuncia')
+}catch(e){
+    console.log(`temos um problema! ${e.message}`)
+}
