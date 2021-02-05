5a09d1e219abe5bba98b29780f6b6466ba20dcd1
William Goulart Pacheco <gtaskwill@gmail.com>
2021-02-05 14:14:05 -0300

    #

--- nodejs/002_basico_ao_avancado/proj0012/server.js
+++ nodejs/002_basico_ao_avancado/proj0012/server.js
@@ -6,11 +6,8 @@ const path = require('path');
 
 app.use(bodyParser.urlencoded({extended:true}))
 
-app.get('/',(req, res)=>{
-    // res.json({message:'bem vindo'})
-    res.sendFile(__dirname+'/index.html')
-})
 
+// configuracao de upload
 const storage = multer.diskStorage({
     destination:(req, file, cb)=>{
         cb(null, 'uploads/')
@@ -23,6 +20,28 @@ const storage = multer.diskStorage({
 
 const upload = multer({storage: storage})
 
+
+// rotas
+app.get('/',(req, res)=>{
+    // res.json({message:'bem vindo'})
+    res.sendFile(__dirname+'/index.html')
+})
+
+
+app.post("/upload", upload.single('arquivo'), (req, res, next)=>{
+    const file = req.file
+
+    if(!file){
+        const err = new Error('Por Favor selecione um arquivo')
+        err.httpRequestStatusCode = 400
+        return next(err)
+    }
+    res.send(file)
+
+})
+
+
+// sempre a ultima coisa
 app.listen(3000,'127.0.0.1',()=>{
     console.log('server rodando em http://localhost:3000')
 })
