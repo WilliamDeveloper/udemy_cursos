01ebb6514b999b72436f6341e08f60a3ca3eda87
William Goulart Pacheco <gtaskwill@gmail.com>
2021-02-05 09:38:38 -0300

    #

--- nodejs/002_basico_ao_avancado/proj0006/app.js
+++ nodejs/002_basico_ao_avancado/proj0006/app.js
@@ -1,47 +1,15 @@
 const express = require('express');
 const app = express()
+const indexRoutes = require('./routes/index')
+const adminRoutes = require('./routes/admin')
 
 // funcao middleware ".use"
 app.use(express.json())
 
-app.get('/', (req,res)=>{
-    res.statusCode = 200;
-    res.send('hello worlds');
-});
+app.use('/',indexRoutes)
+app.use('/admin',adminRoutes)
 
 
-app.get('/admin', (req,res)=>{
-    res.statusCode = 200;
-    res.send('area administrativa');
-});
-
-app.get('/admin/:id', (req,res)=>{
-    res.statusCode = 200;
-    let id = req.params.id;
-    res.send('area administrativa '+ id);
-});
-
-app.post('/admin', (req,res)=>{
-
-    const corpo = `
-        Login: ${req.body.login}\n
-        Senha: ${req.body.senha}
-    `
-    res.send('area administrativa via post\n'+corpo);
-});
-
-app.patch('/admin', (req,res)=>{
-    res.send('area administrativa via PATCH\n');
-});
-
-app.put('/admin/:id', (req,res)=>{
-    res.send('area administrativa via PUT\n');
-});
-
-
-app.delete('/admin/:id', (req,res)=>{
-    res.send('area administrativa via DELETE\n');
-});
 
 
 app.listen(3000,()=>{
