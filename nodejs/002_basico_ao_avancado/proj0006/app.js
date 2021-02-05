3796a1b05a1060ecb8e273085fba717b3d1b1576
William Goulart Pacheco <gtaskwill@gmail.com>
2021-02-05 10:24:18 -0300

    #

--- nodejs/002_basico_ao_avancado/proj0006/app.js
+++ nodejs/002_basico_ao_avancado/proj0006/app.js
@@ -3,14 +3,14 @@ const app = express()
 
 const indexRoutes = require('./routes/index')
 const adminRoutes = require('./routes/admin')
-const usuarioRoutes = require('./routes/admin')
+const usuarioRoutes = require('./routes/usuario')
 
-// funcao middleware ".use"
+// funcao middleware ".use" , express.json do tipo built-in
 app.use(express.json())
 
 app.use('/',indexRoutes)
 app.use('/admin',adminRoutes)
-app.use('/admin',usuarioRoutes)
+app.use('/usuarios',usuarioRoutes)
 
 
 
