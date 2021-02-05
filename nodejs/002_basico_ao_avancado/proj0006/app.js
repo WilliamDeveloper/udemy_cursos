e58da2f032548b1e66419b95810891216a6ff16c
William Goulart Pacheco <gtaskwill@gmail.com>
2021-02-05 08:53:30 -0300

    #

--- nodejs/002_basico_ao_avancado/proj0006/app.js
+++ nodejs/002_basico_ao_avancado/proj0006/app.js
@@ -6,6 +6,18 @@ app.get('/', (req,res)=>{
     res.send('hello worlds');
 });
 
+
+app.get('/admin', (req,res)=>{
+    res.statusCode = 200;
+    res.send('area administrativa');
+});
+
+app.get('/admin/:id', (req,res)=>{
+    res.statusCode = 200;
+    let id = req.params.id;
+    res.send('area administrativa '+ id);
+});
+
 app.listen(3000,()=>{
     console.log(`rodando em http://localhost:3000`)
 })
