30e03e936fb157b74b9975f50817291aaa362502
William Goulart Pacheco <gtaskwill@gmail.com>
2021-02-05 11:05:17 -0300

    #

--- nodejs/002_basico_ao_avancado/proj0006/app.js
+++ nodejs/002_basico_ao_avancado/proj0006/app.js
@@ -8,6 +8,12 @@ const usuarioRoutes = require('./routes/usuario')
 // funcao middleware ".use" , express.json do tipo built-in
 app.use(express.json())
 
+//criando funcao de middleware personalizada
+app.use((req,res,next)=>{
+    console.log('logando sempre antes de cada rota')
+    return next()
+})
+
 app.use('/',indexRoutes)
 app.use('/admin',adminRoutes)
 app.use('/usuarios',usuarioRoutes)
