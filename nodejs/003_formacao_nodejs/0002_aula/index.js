55d954ce65e0b9b1f272e57a50737a38f5e78104
William Goulart Pacheco <gtaskwill@gmail.com>
2021-02-06 17:11:37 -0300

    #

--- nodejs/003_formacao_nodejs/0002_aula/index.js
+++ nodejs/003_formacao_nodejs/0002_aula/index.js
@@ -1,6 +1,20 @@
 const express = require('express')
-const app = express()
+const app = express();
 
+app.get('/', function (req, res) {
+    res.send('luciane linda')
+})
+
+
+app.get('/blog', function (req, res) {
+    res.send('bem vindo ao blog')
+})
+
+app.get('/canal/youtube', function (req, res) {
+    res.send('bem vindo ao youtube')
+})
+
+//iniciando servidor
 app.listen(4000,function (erro) {
     if (erro){
         console.log('um erro aconteceu')
