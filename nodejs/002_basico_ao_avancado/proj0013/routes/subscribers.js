7134ad414b38d5a7af8b0f35f1836d35bf5c4a39
William Goulart Pacheco <gtaskwill@gmail.com>
2021-02-05 21:42:27 -0300

    #

--- nodejs/002_basico_ao_avancado/proj0013/routes/subscribers.js
+++ nodejs/002_basico_ao_avancado/proj0013/routes/subscribers.js
@@ -1,16 +1,32 @@
 const express = require('express')
 const router = express.Router()
+const Subscriber = require('../models/subscriber')
 
-router.get('/',(req,res)=>{
-    res.send('rest api ok')
+router.get('/',async (req,res)=>{
+    try{
+        const subscrinbers = await Subscriber.find()
+        res.json(subscrinbers)
+    }catch (e) {
+        res.status(500).json({message: e.message})
+    }
 })
 
 router.get('/:id',(req,res)=>{
     res.send('rest api ID')
 })
 
-router.post('/',(req,res)=>{
-
+router.post('/',async(req,res)=>{
+    const subscriber = new Subscriber({
+        userName : req.body.userName,
+        userChannel: req.body.userChannel
+    })
+    
+    try{
+        const newSubscriber = await subscriber.save()
+        res.status(201).json(newSubscriber)
+    }catch (e) {
+        res.status(400).json({message: e.message})
+    }
 })
 
 router.patch('/:id',(req,res)=>{
