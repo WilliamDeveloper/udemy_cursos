756b71a806661ca13951301755c17deb9042e36a
William Goulart Pacheco <gtaskwill@gmail.com>
2021-02-06 18:13:49 -0300

    #

--- nodejs/003_formacao_nodejs/0002_aula/index.js
+++ nodejs/003_formacao_nodejs/0002_aula/index.js
@@ -14,6 +14,15 @@ app.get('/canal/youtube', function (req, res) {
     res.send('bem vindo ao youtube')
 })
 
+//http://localhost:4000/ola/Luciane/verbo
+// quando tem "?" significa q o parametro nao eh obrigatorio
+app.get('/ola/:nome?/:empresa?', function (req, res) {
+    console.log(req.params)
+    let nome = req.params.nome
+    let empresa = req.params.empresa
+    res.send(`ola ${nome} ${empresa}`)
+})
+
 //iniciando servidor
 app.listen(4000,function (erro) {
     if (erro){
