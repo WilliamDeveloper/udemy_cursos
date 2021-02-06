a52c9d3e0afc93c8e4931f05da89dec24613c8e2
William Goulart Pacheco <gtaskwill@gmail.com>
2021-02-06 14:03:53 -0300

    #

--- nodejs/003_formacao_nodejs/0001_aula/http_test.js
+++ nodejs/003_formacao_nodejs/0001_aula/http_test.js
@@ -1,4 +1,6 @@
 let http_test = require('http');
 
-http_test.createServer().listen(8181);
+http_test.createServer(function (requisicao, resposta) {
+    resposta.end("bem vindo ao site!")
+}).listen(8181);
 console.log('meu servidor esta rodando')
