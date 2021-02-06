906698fab8e3f2fa90c60ed7d26f4363b4e07970
William Goulart Pacheco <gtaskwill@gmail.com>
2021-02-05 21:14:17 -0300

    #

--- nodejs/002_basico_ao_avancado/proj0013/server.js
+++ nodejs/002_basico_ao_avancado/proj0013/server.js
@@ -1,12 +1,20 @@
+require('dotenv').config()
+
 const express = require('express');
 const app = express()
 const mongoose = require('mongoose');
 
-mongoose.connect("mongodb://localhost/subscribers",{useNewUrlParser:true, useUnifiedTopology:true})
-const db = mongoose.connection
+mongoose.connect(process.env.DATABASE_STRING,{useNewUrlParser:true, useUnifiedTopology:true})
 
+
+const db = mongoose.connection
 db.on('error',(err)=>{console.log(err)})
+
 db.once('open',()=>{console.log('Database Connected')})
+app.use(express.json())
+
+const subscribersRouter = require('./routes/subscribers')
+app.use('/subscribers', subscribersRouter)
 
 
 
