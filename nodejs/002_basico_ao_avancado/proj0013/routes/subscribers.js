f333f253edd74a633587f27b2a1962fcacdfd2cc
William Goulart Pacheco <gtaskwill@gmail.com>
2021-02-05 21:18:05 -0300

    #

--- nodejs/002_basico_ao_avancado/proj0013/routes/subscribers.js
+++ nodejs/002_basico_ao_avancado/proj0013/routes/subscribers.js
@@ -2,11 +2,11 @@ const express = require('express')
 const router = express.Router()
 
 router.get('/',(req,res)=>{
-
+    res.send('rest api ok')
 })
 
 router.get('/:id',(req,res)=>{
-
+    res.send('rest api ID')
 })
 
 router.post('/',(req,res)=>{
