3327ca861037fd2dfdfd35044fd3e5b832601175
William Goulart Pacheco <gtaskwill@gmail.com>
2021-02-06 18:19:39 -0300

    #

--- nodejs/003_formacao_nodejs/0002_aula/index.js
+++ nodejs/003_formacao_nodejs/0002_aula/index.js
@@ -5,7 +5,6 @@ app.get('/', function (req, res) {
     res.send('luciane linda')
 })
 
-
 app.get('/blog', function (req, res) {
     res.send('bem vindo ao blog')
 })
@@ -14,6 +13,7 @@ app.get('/canal/youtube', function (req, res) {
     res.send('bem vindo ao youtube')
 })
 
+// parametros FIXOS na rota
 //http://localhost:4000/ola/Luciane/verbo
 // quando tem "?" significa q o parametro nao eh obrigatorio
 app.get('/ola/:nome?/:empresa?', function (req, res) {
@@ -21,6 +21,13 @@ app.get('/ola/:nome?/:empresa?', function (req, res) {
     let nome = req.params.nome
     let empresa = req.params.empresa
     res.send(`ola ${nome} ${empresa}`)
+})
+
+// parametros dinamicos na rota
+app.get('/query_params', function (req, res) {
+    console.log(req.query)
+    let canal = req.query["canal"]
+    res.send(`ola ${canal} `)
 })
 
 //iniciando servidor
