d5ce09cc88d8ed7cfe015b53be08fb7a67a8e966
William Goulart Pacheco <gtaskwill@gmail.com>
2021-02-06 22:41:53 -0300

    #

--- nodejs/003_formacao_nodejs/0003_aula/index.js
+++ nodejs/003_formacao_nodejs/0003_aula/index.js
@@ -9,6 +9,12 @@ app.get("/",(req,res)=>{
     res.render('index')// extensao nao obrigatoria
 });
 
+app.get("/home",(req,res)=>{
+    // res.render('index.ejs')// extensao nao obrigatoria
+    res.render('home')// extensao nao obrigatoria
+});
+
+
 app.listen(8181,()=>{
     console.log("servidor no ar")
 })
