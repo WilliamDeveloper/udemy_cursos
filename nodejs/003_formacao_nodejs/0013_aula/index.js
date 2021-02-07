79b7ec8cee54ca728abde887442391eecb77f063
William Goulart Pacheco <gtaskwill@gmail.com>
2021-02-07 17:59:55 -0300

    #

---
+++ nodejs/003_formacao_nodejs/0013_aula/index.js
@@ -0,0 +1,86 @@
+const express  = require("express")
+const app = express()
+const bodyParser = require('body-parser')
+const Pergunta = require('./database/Pergunta')
+
+const connection =  require('./database/database')
+connection
+    .authenticate()
+    .then(()=>{
+        console.log('conexao feita com o banco de dados')
+    })
+    .catch((msgErro)=>{
+        console.log(msgErro)
+    })
+
+
+// dizer para o express usar o EJS como view engine
+app.set('view engine','ejs')
+
+// configuracao de reconhecimento de conteudo de arquivos estaticos
+app.use(express.static('node_modules'))
+app.use(express.static('public'))
+
+
+// configuracao do body parser
+app.use(bodyParser.urlencoded({extended:false}))
+
+//permitir receber dados via json
+app.use(express.json())
+
+//rotas
+app.get("/perguntar", (req, res)=>{
+    res.render("perguntar.ejs")
+})
+
+app.post('/salvarPergunta',(req,res)=>{
+
+    console.log(req.body);
+
+    let titulo = req.body.titulo;
+    let description = req.body.descricao;
+
+    Pergunta.create({
+        titulo:titulo,
+        description: description
+    }).then(()=>{
+        res.redirect("/")
+    })
+
+    res.send(`formulario recebido ${titulo} ${description}`);
+})
+
+
+app.get("/:nome?/:lang?",(req,res)=>{
+    let nome = req.params.nome || "William Pacheco"
+    let lang = req.params.lang ||"javascript"
+    let exibirMsg = true
+    let produtos =[
+        {nome:'doritos',preco:1.30},
+        {nome:'fandangos',preco:1.80}
+    ]
+    let params = {
+        nome:nome,
+        lang:lang,
+        empresa : "guia programador",
+        inscritos : 1500,
+        msg : exibirMsg,
+        produtos:produtos,
+    }
+    res.render('index', params)// extensao nao obrigatoria
+});
+
+app.get("/home",(req,res)=>{
+    // res.render('index.ejs')// extensao nao obrigatoria
+    res.render('home')// extensao nao obrigatoria
+});
+
+app.get("/perfil",(req,res)=>{
+    // res.render('index.ejs')// extensao nao obrigatoria
+    res.render('principal/perfil')// extensao nao obrigatoria
+});
+
+
+app.listen(8181,()=>{
+    console.log("servidor no ar")
+})
---
+++ nodejs/003_formacao_nodejs/0013_aula/readme.md
@@ -0,0 +1,34 @@
+--- ---------------------------------------------------
+- criado com
+--- ---------------------------------------------------
+npm init -y
+
+--- ---------------------------------------------------
+- bibliotecas global de sistema
+--- ---------------------------------------------------
+- biblioteca para o script de node se auto dar restart quando tiver uma modificacao
+ npm install -g nodemon 
+ 
+ --- ---------------------------------------------------
+ - bibliotecas aplicacao
+ --- ---------------------------------------------------
+
+- para rotas da aplicacao
+npm install --save express
+
+- EJS - embedded javascript templating -  motor de html - template engine
+npm install ejs --save
+
+- bootstrap
+npm install --save jquery
+npm install --save popper.js
+npm install --save bootstrap
+
+- body parser para pegar a conversao em objeto de uma req post no body
+npm install --save body-parser
+
+-- biblioteca que permise usar abstracao para falar com banco de dados
+npm install --save sequelize
+npm install --save mysql2
+
+
