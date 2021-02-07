79f5fe7eb4930d17cd4ebef2d574e70a5e49f078
William Goulart Pacheco <gtaskwill@gmail.com>
2021-02-06 22:38:28 -0300

    #

--- nodejs/003_formacao_nodejs/0003_aula/index.js
+++ nodejs/003_formacao_nodejs/0003_aula/index.js
@@ -5,7 +5,7 @@ const app = express()
 app.set('view engine','ejs')
 
 app.get("/",(req,res)=>{
-    res.send(' bem vindo ao site')
+    res.render('index.ejs')// extensao nao obrigatoria
 });
 
 app.listen(8181,()=>{
