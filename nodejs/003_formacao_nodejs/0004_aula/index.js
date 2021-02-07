102e83d93168f324179e6f90c6ccca6a5d75d7a1
William Goulart Pacheco <gtaskwill@gmail.com>
2021-02-06 22:48:29 -0300

    #

---
+++ nodejs/003_formacao_nodejs/0004_aula/index.js
@@ -0,0 +1,25 @@
+const express  = require("express")
+const app = express()
+
+// dizer para o express usar o EJS como view engine
+app.set('view engine','ejs')
+
+app.get("/",(req,res)=>{
+    // res.render('index.ejs')// extensao nao obrigatoria
+    res.render('index')// extensao nao obrigatoria
+});
+
+app.get("/home",(req,res)=>{
+    // res.render('index.ejs')// extensao nao obrigatoria
+    res.render('home')// extensao nao obrigatoria
+});
+
+app.get("/perfil",(req,res)=>{
+    // res.render('index.ejs')// extensao nao obrigatoria
+    res.render('principal/perfil')// extensao nao obrigatoria
+});
+
+
+app.listen(8181,()=>{
+    console.log("servidor no ar")
+})
