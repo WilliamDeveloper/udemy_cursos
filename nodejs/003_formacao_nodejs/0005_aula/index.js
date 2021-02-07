995115c9a014fa7b8b18297b92ad5f5848bd9014
William Goulart Pacheco <gtaskwill@gmail.com>
2021-02-06 23:04:24 -0300

    #

---
+++ nodejs/003_formacao_nodejs/0005_aula/index.js
@@ -0,0 +1,32 @@
+const express  = require("express")
+const app = express()
+
+// dizer para o express usar o EJS como view engine
+app.set('view engine','ejs')
+
+app.get("/:nome?/:lang?",(req,res)=>{
+    let nome = req.params.nome || "William Pacheco"
+    let lang = req.params.lang ||"javascript"
+    let params = {
+        nome:nome,
+        lang:lang,
+        empresa : "guia programador",
+        inscritos : 1500
+    }
+    res.render('index', params)// extensao nao obrigatoria
+});
+
+app.get("/home",(req,res)=>{
+    // res.render('index.ejs')// extensao nao obrigatoria
+    res.render('home')// extensao nao obrigatoria
+});
+
+app.get("/perfil",(req,res)=>{
+    // res.render('index.ejs')// extensao nao obrigatoria
+    res.render('principal/perfil')// extensao nao obrigatoria
+});
+
+
+app.listen(8181,()=>{
+    console.log("servidor no ar")
+})
---
+++ nodejs/003_formacao_nodejs/0005_aula/readme.md
@@ -0,0 +1,21 @@
+--- ---------------------------------------------------
+- criado com
+--- ---------------------------------------------------
+npm init -y
+
+--- ---------------------------------------------------
+- bibliotecas global de sistema
+--- ---------------------------------------------------
+- biblioteca para o script de node se auto dar restart quando tiver uma modificacao
+ npm install -g nodemon 
+ 
+ --- ---------------------------------------------------
+ - bibliotecas aplicacao
+ --- ---------------------------------------------------
+
+- para rotas da aplicacao
+npm install express --save
+
+- EJS - embedded javascript templating -  motor de html - template engine
+npm install ejs --save
+
