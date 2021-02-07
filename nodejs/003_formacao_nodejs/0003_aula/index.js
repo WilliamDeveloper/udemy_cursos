95a08d10471dcfc7275151a33f6696c741dceacc
William Goulart Pacheco <gtaskwill@gmail.com>
2021-02-06 22:20:52 -0300

    #

---
+++ nodejs/003_formacao_nodejs/0003_aula/index.js
@@ -0,0 +1,10 @@
+const express  = require("express")
+const app = express()
+
+app.get("/",(req,res)=>{
+    console.log(' bem vindo ao site')
+});
+
+ap.listen(8080,()=>{
+    console.log("servidor no ar")
+})
