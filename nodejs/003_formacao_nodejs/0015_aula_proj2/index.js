f42e3a9676f9163965b0b5fe66a37b4da530295b
William Goulart Pacheco <gtaskwill@gmail.com>
2021-02-07 21:39:55 -0300

    #

---
+++ nodejs/003_formacao_nodejs/0015_aula_proj2/index.js
@@ -0,0 +1,41 @@
+const express = require("express");
+const app = express();
+const bodyParser = require("body-parser");
+const connection = require("./database/database")
+
+// dizer para o express usar o EJS como view engine
+app.set('view engine','ejs')
+
+
+// configuracao de reconhecimento de conteudo de arquivos estaticos
+app.use(express.static('node_modules'))
+app.use(express.static('public'))
+
+
+// configuracao do body parser
+app.use(bodyParser.urlencoded({extended:false}))
+
+//permitir receber dados via json
+app.use(express.json())
+
+
+//Database
+connection
+    .authenticate()
+    .then(() => {
+        console.log("Conexão feita com o banco de dados!")
+    })
+    .catch((msgErro) => {
+        console.log(msgErro);
+    })
+
+
+app.get("/",(req,res)=>{
+    res.render("index.ejs")
+});
+
+
+
+app.listen(8181,()=>{
+    console.log("servidor no ar")
+})
