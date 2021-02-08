d0a406cc752a2a3ac7ab2e49ec7313cc1ea15b7c
William Goulart Pacheco <gtaskwill@gmail.com>
2021-02-08 20:42:31 -0300

    #

--- nodejs/003_formacao_nodejs/0021_aula_proj2/database/database.js
+++ nodejs/003_formacao_nodejs/0021_aula_proj2/database/database.js
@@ -1,10 +1,10 @@
 const Sequelize = require("sequelize")
 
-// const connection = new Sequelize("guiapress",'root','123456',{
-//     host:'localhost',
-//     dialect:'mysql',
-//     timezone:'-03:00'
-// })
+const connection = new Sequelize("guiapress",'root','123456',{
+    host:'localhost',
+    dialect:'mysql',
+    timezone:'-03:00'
+})
 
 // const connection = new Sequelize({
 //     dialect:'sqlite',
