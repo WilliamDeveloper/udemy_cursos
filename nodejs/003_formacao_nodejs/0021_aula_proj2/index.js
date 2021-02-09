87122356a3a7d379429f2bb021272868669e413c
William Goulart Pacheco <gtaskwill@gmail.com>
2021-02-09 13:48:16 -0300

    #

--- nodejs/003_formacao_nodejs/0021_aula_proj2/index.js
+++ nodejs/003_formacao_nodejs/0021_aula_proj2/index.js
@@ -77,6 +77,33 @@ app.get("/:slug",(req,res)=>{
 
 
 
+app.get('/category/:slug',(req,res)=>{
+    let slug  = req.params.slug;
+    Category.findOne({
+        where:{
+            slug:slug
+        },
+        include:[{model: Article}]
+    }).then( (category)=>{
+        if(category!=undefined){
+
+            Category.findAll().then(categories =>{
+                res.render('index',{
+                    articles: category.articles,
+                    categories:categories
+                })
+            })
+
+        }else{
+            res.redirect('/')
+        }
+    }).catch( err =>{
+        res.redirect('/')
+    })
+})
+
+
+
 app.listen(8181,()=>{
     console.log("servidor no ar on port 8181")
 })
