a68baaed9731a5df31074f01f951aafacdf1362f
William Goulart Pacheco <gtaskwill@gmail.com>
2021-02-10 07:56:15 -0300

    #

--- nodejs/003_formacao_nodejs/0021_aula_proj2/users/UsersController.js
+++ nodejs/003_formacao_nodejs/0021_aula_proj2/users/UsersController.js
@@ -5,7 +5,9 @@ const bcrypt = require("bcrypt")
 const User = require('./User')
 
 router.get('/admin/users', (req, res)=>{
-    res.render('admin/users/index')
+    User.findAll().then((users)=>{
+        res.render('admin/users/index',{users:users})
+    })
 })
 
 router.get('/admin/users/create', (req, res)=>{
