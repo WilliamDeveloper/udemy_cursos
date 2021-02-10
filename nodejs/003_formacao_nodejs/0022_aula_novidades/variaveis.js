e5cb9bc98605fc4121c946cef7d256898ec7abb0
William Goulart Pacheco <gtaskwill@gmail.com>
2021-02-10 13:10:30 -0300

    #

--- nodejs/003_formacao_nodejs/0022_aula_novidades/variaveis.js
+++ nodejs/003_formacao_nodejs/0022_aula_novidades/variaveis.js
@@ -7,3 +7,29 @@
 // cria uma variavel dentro de um bloco de if a variavel fica com escopo global
 
 // quando se usar let o escopo de bloco passa a ser respeitado
+
+
+if(true){
+    //essa variavel vai ser tornar global , inxergam ateh fora do if
+    var variavel = 'nao deveria sem acessado fora do if'
+}
+
+console.log(variavel)
+
+///----------------------------------------------------------
+
+if(true){
+    // escopo local so o bloco de if inxerga
+    let variavel2 = 'nao deveria sem acessado fora do if'
+}
+
+console.log(variavel2)
+
+
+function f() {
+    // escopo local so a function inxerga
+    var a = 'pimba'
+}
+
+f()
+console.log(a)
