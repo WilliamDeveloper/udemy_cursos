7bfffa40273d0896128fd6e92f2569fee842a507
William Goulart Pacheco <gtaskwill@gmail.com>
2021-02-10 14:08:00 -0300

    #

--- nodejs/003_formacao_nodejs/0022_aula_novidades/arrow-function.js
+++ nodejs/003_formacao_nodejs/0022_aula_novidades/arrow-function.js
@@ -6,3 +6,23 @@ var mult = function (a,b) {
     return a*b
 }
 
+var mul2 = (a,b)=>{
+    return a*b
+}
+
+// --------------------------------------
+var msg = function (msg) {
+    console.log(msg)
+}
+
+var msg2 = (msg)=>{
+    console.log(msg)
+}
+
+var msg3 = msg =>{
+    console.log(msg)
+}
+
+var msg4 = msg => console.log(msg)
+
+// --------------------------------------
