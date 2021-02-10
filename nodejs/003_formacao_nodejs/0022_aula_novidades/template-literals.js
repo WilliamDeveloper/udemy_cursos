46999e962671f7c8384b076dff35fa2530a24f33
William Goulart Pacheco <gtaskwill@gmail.com>
2021-02-10 14:26:28 -0300

    #

--- nodejs/003_formacao_nodejs/0022_aula_novidades/template-literals.js
+++ nodejs/003_formacao_nodejs/0022_aula_novidades/template-literals.js
@@ -7,3 +7,11 @@ console.log(frase)
 //utilizando do recurso da interpolacao
 var frase2 = `ola meu nome é ${nome} e tenho ${idade}`
 console.log(frase2)
+
+
+//utilizando do recurso da interpolacao - aceita quebra de linha
+var frase3 = `
+    ola meu nome é ${nome} 
+    e tenho ${idade}
+`
+console.log(frase3)
