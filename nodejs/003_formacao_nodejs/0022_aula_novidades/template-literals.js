2ef0a9d574f90c43d99eae0b0422966d380bc85b
William Goulart Pacheco <gtaskwill@gmail.com>
2021-02-10 14:25:03 -0300

    #

--- nodejs/003_formacao_nodejs/0022_aula_novidades/template-literals.js
+++ nodejs/003_formacao_nodejs/0022_aula_novidades/template-literals.js
@@ -4,5 +4,6 @@ var idade = 18
 var frase = "ola meu nome é "+nome+" e tenho "+idade
 console.log(frase)
 
+//utilizando do recurso da interpolacao
 var frase2 = `ola meu nome é ${nome} e tenho ${idade}`
 console.log(frase2)
