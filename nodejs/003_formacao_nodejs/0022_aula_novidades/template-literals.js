8c85a72ba24ef2f12b0a25e15eadf40687a55cd0
William Goulart Pacheco <gtaskwill@gmail.com>
2021-02-10 14:24:02 -0300

    #

---
+++ nodejs/003_formacao_nodejs/0022_aula_novidades/template-literals.js
@@ -0,0 +1,8 @@
+var nome= 'william'
+var idade = 18
+
+var frase = "ola meu nome é "+nome+" e tenho "+idade
+console.log(frase)
+
+var frase2 = `ola meu nome é ${nome} e tenho ${idade}`
+console.log(frase2)
