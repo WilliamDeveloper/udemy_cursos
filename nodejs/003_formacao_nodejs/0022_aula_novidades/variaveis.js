d6bfb70ad27a519dc3bee30c0e9182420d6b1239
William Goulart Pacheco <gtaskwill@gmail.com>
2021-02-10 12:46:35 -0300

    #

---
+++ nodejs/003_formacao_nodejs/0022_aula_novidades/variaveis.js
@@ -0,0 +1,9 @@
+// explicacao muito boa de var e let
+
+// https://www.youtube.com/watch?v=EXcj8jhZj8I
+
+// basicamente quando se usar "VAR"
+
+// cria uma variavel dentro de um bloco de if a variavel fica com escopo global
+
+// quando se usar let o escopo de bloco passa a ser respeitado
