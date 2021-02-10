8577d415fbc52faa8f4e139f3469b31c4521ea74
William Goulart Pacheco <gtaskwill@gmail.com>
2021-02-10 09:00:43 -0300

    #

--- nodejs/003_formacao_nodejs/0021_aula_proj2/middlewares/adminAuth.js
+++ nodejs/003_formacao_nodejs/0021_aula_proj2/middlewares/adminAuth.js
@@ -2,5 +2,12 @@ function adminAuth(req,res,next) {
 
     // como um middleware fica entre a requisicao e resposta
     // enquanto o middleware nao dar next a rota nao termina de responder
-    next();
+    if(req.session.user != undefined){
+        next()
+    }else{
+        res.redirect("/login")
+    }
+
 }
+
+module.exports=adminAuth
