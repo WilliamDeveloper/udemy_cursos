e24188f1dae9f26d15d01702601ed391b45a5d0c
William Goulart Pacheco <gtaskwill@gmail.com>
2021-02-10 13:44:10 -0300

    #

--- nodejs/003_formacao_nodejs/0022_aula_novidades/desestruturacao.js
+++ nodejs/003_formacao_nodejs/0022_aula_novidades/desestruturacao.js
@@ -6,3 +6,10 @@ var user = {
     site: 'guiadoprogramador.com',
     email: 'guia@doprogramador.com'
 }
+
+var nome = user.nome;
+var idade = user.idade
+console.log(nome, idade)
+
+var {cidade,site,email} = user
+console.log(cidade,site,email)
