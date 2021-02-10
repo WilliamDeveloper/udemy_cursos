9035e31819fa134f8078f4a993dbc3c2d2a3824a
William Goulart Pacheco <gtaskwill@gmail.com>
2021-02-10 13:17:21 -0300

    #

---
+++ nodejs/003_formacao_nodejs/0022_aula_novidades/operador-spread.js
@@ -0,0 +1,20 @@
+var nome ='william'
+var idade = 18
+var empresa = 'guia programador'
+
+var user = {
+    nome:nome,
+    idade: idade,
+    empresa: empresa
+}
+
+console.log(user)
+
+
+var user2 = {
+    nome,
+    idade,
+    empresa
+}
+
+console.log(user2)
