ea0483c86b96ff5c5d243b2742a832a7df62fa8d
William Goulart Pacheco <gtaskwill@gmail.com>
2021-02-10 22:58:22 -0300

    #

---
+++ nodejs/003_formacao_nodejs/0023_orientacao_objetos/Composicao.js
@@ -0,0 +1,23 @@
+class A{
+    constructor(){
+        this.nome="william"
+    }
+}
+
+
+class B{
+    constructor(){
+        this.idade=18
+    }
+}
+
+class C{
+    constructor(){
+        this.a=new A()
+        this.b=new B()
+    }
+}
+
+let c = new C()
+console.log(c.a.nome)
+console.log(c.b.idade)
