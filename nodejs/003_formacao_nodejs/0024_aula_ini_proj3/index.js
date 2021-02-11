fc92f2ed61ec3159740f201b2a85d94e00032310
William Goulart Pacheco <gtaskwill@gmail.com>
2021-02-11 13:59:33 -0300

    #

--- nodejs/003_formacao_nodejs/0024_aula_ini_proj3/index.js
+++ nodejs/003_formacao_nodejs/0024_aula_ini_proj3/index.js
@@ -4,9 +4,43 @@ let nomeArquivo ="./arquivo.txt"
 let readFileOptions = {
     encoding:'utf-8'
 }
+
+//----------------------------------------------------------------------------------
+// LENDO
+//----------------------------------------------------------------------------------
 fs.readFile(nomeArquivo,readFileOptions, (erro, dados)=>{
     if(erro){
-        console.log('falha ao ler o arquivo')
+        console.log('falha ao ler no arquivo')
+    }else{
+        console.log(dados)
+    }
+})
+
+
+//----------------------------------------------------------------------------------
+// ESCREVENDO
+//----------------------------------------------------------------------------------
+let novoConteudoArquivo = `
+    dsadsadsadsadasdasdasdasdsadsaddsadsadsadsadasdasdasdasdsadsaddsadsadsadsadasdasdasdasdsadsaddsadsadsadsadasdasdasdasdsadsaddsadsadsadsadasdasdasdasdsadsad
+    dsadsadsadsadasdasdasdasdsadsaddsadsadsadsadasdasdasdasdsadsaddsadsadsadsadasdasdasdasdsadsaddsadsadsadsadasdasdasdasdsadsaddsadsadsadsadasdasdasdasdsadsad
+    dsadsadsadsadasdasdasdasdsadsaddsadsadsadsadasdasdasdasdsadsaddsadsadsadsadasdasdasdasdsadsaddsadsadsadsadasdasdasdasdsadsaddsadsadsadsadasdasdasdasdsadsad
+    dsadsadsadsadasdasdasdasdsadsaddsadsadsadsadasdasdasdasdsadsaddsadsadsadsadasdasdasdasdsadsaddsadsadsadsadasdasdasdasdsadsaddsadsadsadsadasdasdasdasdsadsad
+    dsadsadsadsadasdasdasdasdsadsaddsadsadsadsadasdasdasdasdsadsaddsadsadsadsadasdasdasdasdsadsaddsadsadsadsadasdasdasdasdsadsaddsadsadsadsadasdasdasdasdsadsad
+    dsadsadsadsadasdasdasdasdsadsaddsadsadsadsadasdasdasdasdsadsaddsadsadsadsadasdasdasdasdsadsaddsadsadsadsadasdasdasdasdsadsaddsadsadsadsadasdasdasdasdsadsad
+    dsadsadsadsadasdasdasdasdsadsaddsadsadsadsadasdasdasdasdsadsaddsadsadsadsadasdasdasdasdsadsaddsadsadsadsadasdasdasdasdsadsaddsadsadsadsadasdasdasdasdsadsad
+    dsadsadsadsadasdasdasdasdsadsaddsadsadsadsadasdasdasdasdsadsaddsadsadsadsadasdasdasdasdsadsaddsadsadsadsadasdasdasdasdsadsaddsadsadsadsadasdasdasdasdsadsad
+    dsadsadsadsadasdasdasdasdsadsaddsadsadsadsadasdasdasdasdsadsaddsadsadsadsadasdasdasdasdsadsaddsadsadsadsadasdasdasdasdsadsaddsadsadsadsadasdasdasdasdsadsad
+    dsadsadsadsadasdasdasdasdsadsaddsadsadsadsadasdasdasdasdsadsaddsadsadsadsadasdasdasdasdsadsaddsadsadsadsadasdasdasdasdsadsaddsadsadsadsadasdasdasdasdsadsad
+    dsadsadsadsadasdasdasdasdsadsaddsadsadsadsadasdasdasdasdsadsaddsadsadsadsadasdasdasdasdsadsaddsadsadsadsadasdasdasdasdsadsaddsadsadsadsadasdasdasdasdsadsad
+    dsadsadsadsadasdasdasdasdsadsaddsadsadsadsadasdasdasdasdsadsaddsadsadsadsadasdasdasdasdsadsaddsadsadsadsadasdasdasdasdsadsaddsadsadsadsadasdasdasdasdsadsad
+    dsadsadsadsadasdasdasdasdsadsaddsadsadsadsadasdasdasdasdsadsaddsadsadsadsadasdasdasdasdsadsaddsadsadsadsadasdasdasdasdsadsaddsadsadsadsadasdasdasdasdsadsad
+    dsadsadsadsadasdasdasdasdsadsaddsadsadsadsadasdasdasdasdsadsaddsadsadsadsadasdasdasdasdsadsaddsadsadsadsadasdasdasdasdsadsaddsadsadsadsadasdasdasdasdsadsad
+    dsadsadsadsadasdasdasdasdsadsaddsadsadsadsadasdasdasdasdsadsaddsadsadsadsadasdasdasdasdsadsaddsadsadsadsadasdasdasdasdsadsaddsadsadsadsadasdasdasdasdsadsad
+    dsadsadsadsadasdasdasdasdsadsaddsadsadsadsadasdasdasdasdsadsaddsadsadsadsadasdasdasdasdsadsaddsadsadsadsadasdasdasdasdsadsaddsadsadsadsadasdasdasdasdsadsad
+`
+fs.writeFile(nomeArquivo,novoConteudoArquivo, (erro, dados)=>{
+    if(erro){
+        console.log('falha ao escrever no arquivo')
     }else{
         console.log(dados)
     }
