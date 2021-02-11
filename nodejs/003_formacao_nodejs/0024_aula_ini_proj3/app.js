ae11c33cbeaf398460916ca4993b87a7e23a450c
William Goulart Pacheco <gtaskwill@gmail.com>
2021-02-11 18:57:18 -0300

    #

--- nodejs/003_formacao_nodejs/0024_aula_ini_proj3/app.js
+++ nodejs/003_formacao_nodejs/0024_aula_ini_proj3/app.js
@@ -1,9 +1,12 @@
 const Reader = require('./uteis/Reader')
+const Writer = require('./uteis/Writer')
 const ProcessorCSV = require('./uteis/ProcessorCSV')
 const Table = require('./uteis/Table')
 const HtmlParser = require('./uteis/HtmlParser')
 
-let leitor = new Reader()
+const leitor = new Reader()
+const escritor = new Writer()
+
 let filepath = "./arquivo.csv"
 
 async function main() {
@@ -18,6 +21,13 @@ async function main() {
     console.log("res: ",usuarios.RowCount)
     console.log("res: ",usuarios.ColumnCount)
     console.log("res: ",html)
+
+    let resp = await escritor.Write("./uteis/teste.html", html)
+    if(resp){
+        console.log('gravado com sucesso')
+    }else{
+        console.log('ocorreu uma falha na gravacao')
+    }
 
 }
 
