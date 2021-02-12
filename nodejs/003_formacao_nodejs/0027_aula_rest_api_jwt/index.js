967362da9cefc6395c6721887cbaa2d6ecff6fd9
William Goulart Pacheco <gtaskwill@gmail.com>
2021-02-12 15:31:01 -0300

    #

--- nodejs/003_formacao_nodejs/0027_aula_rest_api_jwt/index.js
+++ nodejs/003_formacao_nodejs/0027_aula_rest_api_jwt/index.js
@@ -12,7 +12,7 @@ const msgCodeHttp ={
     200:'200 - requisicao efetuada com sucesso',
     400:'400 - requisicao invalida',
     404:'404 - recurso nao encontrado',
-    401:'401 - credenciais invalidas'
+    401:'401 - credenciais invalidas',
 }
 
 let DB={
