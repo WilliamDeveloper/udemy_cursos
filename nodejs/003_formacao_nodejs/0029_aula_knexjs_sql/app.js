9b360709f9e7fa79423f5e6c897d1448ba0e41e4
William Goulart Pacheco <gtaskwill@gmail.com>
2021-02-13 14:37:34 -0300

    #

--- nodejs/003_formacao_nodejs/0029_aula_knexjs_sql/app.js
+++ nodejs/003_formacao_nodejs/0029_aula_knexjs_sql/app.js
@@ -7,6 +7,15 @@ let dados = {
     cd_value : 'pimba'
 }
 
+let select = database.select('*').table('t411pasi')
+console.log(select.toQuery())
+select.then(data=>{
+    console.log(data)
+})
+.catch(err=>{
+    console.log(err)
+})
+
 
 let query = database.insert(dados).into("t411pasi")
 console.log(query.toQuery())
