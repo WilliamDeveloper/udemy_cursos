36dc8ad94ba13839c5d3b80900beeeadeb6a1af6
William Goulart Pacheco <gtaskwill@gmail.com>
2021-02-13 14:53:04 -0300

    #

--- nodejs/003_formacao_nodejs/0029_aula_knexjs_sql/app.js
+++ nodejs/003_formacao_nodejs/0029_aula_knexjs_sql/app.js
@@ -3,24 +3,33 @@ let database = require('./database')
 
 
 let dados = {
-    cd_key : 'blau',
-    cd_value : 'pimba'
+    CD_KEY : 'blau',
+    CD_VALUE : 'pimba'
 }
 
-let select = database.select('*').table("t411pasi")
-console.log(select.toQuery())
-select.then(data=>{
+// let select = database.select('*').from("T411PASI")
+// console.log(select.toQuery())
+// select.then(data=>{
+//     console.log(data)
+// })
+// .catch(err=>{
+//     console.log(err)
+// })
+
+
+let insert = database.insert(dados).into("T411PASI")
+console.log(insert.toQuery())
+
+insert.then(data=>{
     console.log(data)
 })
 .catch(err=>{
     console.log(err)
 })
 
-
-let query = database.insert(dados).into("t411pasi")
-console.log(query.toQuery())
-
-query.then(data=>{
+let select2 = database.select('*').from("T411PASI").where({CD_KEY:'blau'})
+console.log(select2.toQuery())
+select2.then(data=>{
     console.log(data)
 })
 .catch(err=>{
