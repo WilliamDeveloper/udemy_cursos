694010fdba5e9596cce8cdc93adfb0f6e9dbda95
William Goulart Pacheco <gtaskwill@gmail.com>
2021-02-14 19:02:01 -0300

    #

--- nodejs/003_formacao_nodejs/0032_aula_vuejs/guiaclientes/src/App.vue
+++ nodejs/003_formacao_nodejs/0032_aula_vuejs/guiaclientes/src/App.vue
@@ -1,11 +1,18 @@
 <template>
   <div id="app">
     <h1>Guia clientes</h1>
+    <div v-for="cliente in clientes" :key="cliente.id">
+      <p>testando v-for</p>
+      <Cliente :nome="cliente.nome" :email="cliente.email" :idade="cliente.idade"/>
+    </div>
     <input type="text" v-model="nomeDoWilliam" size="50" >
     <Cliente :nome="nomeDoWilliam" email="a@b.com.br" idade="18" :showIdade="true"/>
     <Cliente nome="Luciane" email="a@b.com.br" idade="19"/>
     <Cliente nome="Mariluci" email="a@b.com.br" idade="20"/>
     <Cliente :cliente="clienteWilliamDeveloper"/>
+
+
+
     <Produto></Produto>
   </div>
 </template>
@@ -29,6 +36,26 @@ export default {
         email:'a@b.com.br',
         idade:'19',
       },
+      clientes : [
+        {
+          id:1,
+          nome:'William Developer1',
+          email:'a@b.com.br',
+          idade:'19',
+        },
+        {
+          id:2,
+          nome:'William Developer3',
+          email:'a@b.com.br',
+          idade:'19',
+        },
+        {
+          id:3,
+          nome:'William Developer3',
+          email:'a@b.com.br',
+          idade:'19',
+        },
+      ]
     }
   }
 }
