30e0faaad156d680d0ccdc30c235b65a3b77ed16
William Goulart Pacheco <gtaskwill@gmail.com>
2021-02-14 20:10:15 -0300

    #

--- nodejs/003_formacao_nodejs/0032_aula_vuejs/guiaclientes/src/App.vue
+++ nodejs/003_formacao_nodejs/0032_aula_vuejs/guiaclientes/src/App.vue
@@ -78,7 +78,12 @@ export default {
   methods:{
     cadastrar: function ($event) {
       console.log($event)
-
+      this.clientes.push({
+          id: Date.now(),
+          nome:this.nomeField,
+          email:this.emailField,
+          idade:this.idadeField
+      })
     }
   }
 }
