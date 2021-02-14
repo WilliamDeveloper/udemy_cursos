9763d66fea5f793cd7283799f1c3e07bdc9303c7
William Goulart Pacheco <gtaskwill@gmail.com>
2021-02-14 15:36:04 -0300

    #

--- nodejs/003_formacao_nodejs/0031_aula_vuejs/guiaclientes/src/App.vue
+++ nodejs/003_formacao_nodejs/0031_aula_vuejs/guiaclientes/src/App.vue
@@ -1,14 +1,19 @@
 <template>
   <div id="app">
     <h1>Guia clientes</h1>
+    <Cliente/>
   </div>
 </template>
 
 <script>
 
+  import Cliente from './components/Cliente'
+
 export default {
   name: 'App',
-
+  components:{
+    Cliente:Cliente
+  }
 }
 </script>
 
