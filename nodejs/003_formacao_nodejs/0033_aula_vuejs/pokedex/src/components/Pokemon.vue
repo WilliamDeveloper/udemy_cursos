b7bff7f4c55899c9f9284f13144d0a9b03c0835a
William Goulart Pacheco <gtaskwill@gmail.com>
2021-02-15 00:56:00 -0300

    #

--- nodejs/003_formacao_nodejs/0033_aula_vuejs/pokedex/src/components/Pokemon.vue
+++ nodejs/003_formacao_nodejs/0033_aula_vuejs/pokedex/src/components/Pokemon.vue
@@ -1,6 +1,6 @@
 <template>
     <div>
-        <h1>{{num}} {{name}}</h1>
+        <h1>{{num}} {{name | upper}}</h1>
         <small>{{url}}</small>
     </div>
     
@@ -14,6 +14,12 @@
             name: String,
             url: String
         },
+        filters:{
+            upper: function (value) {
+                let newName = value[0].toUpperCase() + value.slice(1)
+                return newName
+            },
+        }
     }
 </script>
 
