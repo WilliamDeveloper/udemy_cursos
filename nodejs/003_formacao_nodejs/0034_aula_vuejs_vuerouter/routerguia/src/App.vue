8c4b09535de712dabd58d8234c0a1758fe774d26
William Goulart Pacheco <gtaskwill@gmail.com>
2021-02-15 10:20:30 -0300

    #

--- nodejs/003_formacao_nodejs/0034_aula_vuejs_vuerouter/routerguia/src/App.vue
+++ nodejs/003_formacao_nodejs/0034_aula_vuejs_vuerouter/routerguia/src/App.vue
@@ -4,7 +4,9 @@
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link>
     </div>
+    <p>inicio rota</p>
     <router-view/>
+    <p>fim rota</p>
   </div>
 </template>
 
