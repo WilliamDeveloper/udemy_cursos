6f01058a47bd70d2289a4a5cc7d1878f7c78b342
William Goulart Pacheco <gtaskwill@gmail.com>
2021-02-15 11:28:55 -0300

    #

---
+++ nodejs/003_formacao_nodejs/0034_aula_vuejs_vuerouter/routerguia/src/views/Cadastro.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        <input type="text" name="email" id="email" placeholder="email"><br>
+        <input type="text" name="nome" id="nome" placeholder="nome"><br>
+        <button>Teste</button>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "Cadastro"
+    }
+</script>
+
+<style scoped>
+
+</style>
