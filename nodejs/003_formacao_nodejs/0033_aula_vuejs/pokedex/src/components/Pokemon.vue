0fbe90cb8e6ad02331785dc11cba70ce321ab5a7
William Goulart Pacheco <gtaskwill@gmail.com>
2021-02-15 01:05:19 -0300

    #

--- nodejs/003_formacao_nodejs/0033_aula_vuejs/pokedex/src/components/Pokemon.vue
+++ nodejs/003_formacao_nodejs/0033_aula_vuejs/pokedex/src/components/Pokemon.vue
@@ -7,8 +7,27 @@
 </template>
 
 <script>
+    import axios from 'axios'
+
     export default {
         name: "Pokemon",
+        created: function(){
+            axios.get(this.url).then((res)=>{
+                console.log(res)
+                this.pokemon.type = res.data.types[0].type.name
+                this.pokemon.front =res.data.sprites.front_default
+                this.pokemon.back =res.data.sprites.back_default
+                console.log(this.pokemon)
+
+            }).catch(error=>{
+                console.log(error)
+            })
+        },
+        data: function(){
+            return {
+                pokemon:{}
+            }
+        },
         props:{
             num: Number,
             name: String,
