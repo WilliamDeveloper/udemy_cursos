2e75ebbb61028d86bbc2aa5fe4a8728c4a94475e
William Goulart Pacheco <gtaskwill@gmail.com>
2021-02-15 22:39:59 -0300

    #

--- nodejs/003_formacao_nodejs/0036_aula_proj5_api_rest_knex_jwt/constantes/constantes.js
+++ nodejs/003_formacao_nodejs/0036_aula_proj5_api_rest_knex_jwt/constantes/constantes.js
@@ -1,9 +1,4 @@
-const httpStatusCode ={
-    200:'200 - requisicao efetuada com sucesso',
-    400:'400 - requisicao invalida',
-    404:'404 - recurso nao encontrado',
-    401:'401 - credenciais invalidas',
-}
+const httpStatusCode = require('httpStatusCode')
 
 const msg ={
     httpStatusCode
