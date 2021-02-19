cbd2ef6b93ea2b2cf5b3d74249abc64e1d957037
William Goulart Pacheco <gtaskwill@gmail.com>
2021-02-19 09:39:37 -0300

    #

--- nodejs/003_formacao_nodejs/0037_aula_integracao_proj6/frontend/src/views/Register.vue
+++ nodejs/003_formacao_nodejs/0037_aula_integracao_proj6/frontend/src/views/Register.vue
@@ -5,7 +5,14 @@
 
         <div class="columns is-centered">
             <div class="column is-half">
+                <p>Nome</p>
                 <input type="text" placeholder="Nome do usuario" class="input">
+                <p>Email</p>
+                <input type="text" placeholder="email@email.com" class="input">
+                <p>Senha</p>
+                <input type="password" placeholder="******" class="input">
+                <hr>
+                <button class="button is-success">Cadastrar</button>
             </div>
         </div>
 
