c4926e2aa0980bb455c951fe30a891066f9434c9
William Goulart Pacheco <gtaskwill@gmail.com>
2021-02-19 15:40:24 -0300

    #

--- nodejs/003_formacao_nodejs/0037_aula_integracao_proj6/frontend/src/views/Users.vue
+++ nodejs/003_formacao_nodejs/0037_aula_integracao_proj6/frontend/src/views/Users.vue
@@ -6,6 +6,7 @@
         <table class="table">
             <thead>
                 <tr>
+                    <th>Id</th>
                     <th>Nome</th>
                     <th>Email</th>
                     <th>Cargo</th>
@@ -20,11 +21,41 @@
                        <td>{{user.role | processRole}}</td>
                        <td>
                            <button class="button is-success">Editar</button> | |
-                           <button class="button is-danger">Deletar</button>
+                           <button class="button is-danger" @click="doShowModal(user.id)">Deletar</button>
                        </td>
                    </tr>
             </tbody>
         </table>
+
+
+        <div :class="{modal : true, 'is-active' : showModal}">
+            <div class="modal-background"></div>
+            <div class="modal-content">
+
+
+                <div class="card">
+                    <header class="card-header">
+                        <p class="card-header-title">
+                            Voce quer mesmo deletar o usuario ?
+                        </p>
+
+                    </header>
+                    <div class="card-content">
+                        <div class="content">
+                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus nec iaculis mauris.</p>
+                        </div>
+                    </div>
+                    <footer class="card-footer">
+                        <a href="#" class="card-footer-item" @click="doHideModal()">Cancelar</a>
+                        <a href="#" class="card-footer-item">Sim, quero deletar!</a>
+                    </footer>
+                </div>
+
+            </div>
+            <button class="modal-close is-large" aria-label="close" @click="doHideModal()"></button>
+        </div>
+
+
     </div>
 </template>
 
@@ -51,13 +82,18 @@
         },
         data(){
             return {
-                users: []
+                users: [],
+                showModal:true,
             }
         },
         methods:{
-            ola(){
-                console.log('ola')
-            }
+            doHideModal(){
+                this.showModal = false
+            },
+            doShowModal(id){
+                console.log(id)
+                this.showModal = true
+            },
         },
         filters:{
             processRole: function (value) {
