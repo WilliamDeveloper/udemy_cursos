6917fb8a5ec93f6cb236dc4e98549690cb987e8f
William Goulart Pacheco <gtaskwill@gmail.com>
2021-02-19 13:06:11 -0300

    #

---
+++ nodejs/003_formacao_nodejs/0037_aula_integracao_proj6/frontend/src/views/Login.vue
@@ -0,0 +1,64 @@
+<template>
+    <div>
+        <h2>Login</h2>
+        <hr>
+
+        <div class="columns is-centered">
+            <div class="column is-half">
+
+                <div v-if="error != undefined">
+                    <div class="notification is-danger">
+                        <p>{{error}}</p>
+                    </div>
+                </div>
+
+                <p>Email</p>
+                <input type="text" placeholder="email@email.com" class="input" v-model="email">
+                <p>Senha</p>
+                <input type="password" placeholder="******" class="input" v-model="password">
+                <hr>
+                <button class="button is-success" @click="logar($event)">Logar</button>
+            </div>
+        </div>
+
+    </div>
+</template>
+
+<script>
+    import axios from 'axios'
+
+    export default {
+        data(){
+            return {
+                email: '',
+                password: '',
+                error: undefined,
+            }
+        },
+        methods:{
+            logar(){
+                console.log(this.email)
+                console.log(this.password)
+
+                // let params = {
+                //     email:this.email,
+                //     password:this.password
+                // }
+                // axios.post('http://localhost:3000/user', params).then( resp =>{
+                //     console.log('resposta: ',resp)
+                //     this.error =''
+                //     this.$router.push({name:'Home'})
+                // }).catch( error => {
+                //     let msgError = error.response.data.status
+                //     this.error = msgError
+                //     console.log( msgError)
+                // })
+            },
+        },
+
+    }
+</script>
+
+<style>
+
+</style>
