0eb60980766c1545151d1974ee360858754e561a
William Goulart Pacheco <gtaskwill@gmail.com>
2021-02-19 09:36:28 -0300

    #

--- nodejs/003_formacao_nodejs/0037_aula_integracao_proj6/frontend/src/views/Register.vue
+++ nodejs/003_formacao_nodejs/0037_aula_integracao_proj6/frontend/src/views/Register.vue
@@ -1,6 +1,14 @@
 <template>
     <div>
         <h2>Registro de Usuario</h2>
+        <hr>
+
+        <div class="columns is-centered">
+            <div class="column is-half">
+                <input type="text" placeholder="Nome do usuario" class="input">
+            </div>
+        </div>
+
     </div>
 </template>
 
