7a891739da76ea6070d32ce4a5fa1092d11cd19d
William Goulart Pacheco <gtaskwill@gmail.com>
2021-02-23 14:18:15 -0300

    #

--- nodejs/003_formacao_nodejs/0041_aula_proj8_fullcalendar/index.js
+++ nodejs/003_formacao_nodejs/0041_aula_proj8_fullcalendar/index.js
@@ -81,19 +81,21 @@ app.post('/cadastro/finish', async (req, res)=>{
 
 })
 
-app.get('/cadastro/list/search', async (req, res)=>{
-    let {search} = req.query
-    let listaConsultas = await  appointmentService.Search({termoBuscaQuery:search})
-    res.json({listaConsultas})
-})
-
 app.get('/cadastro/list', async (req, res)=>{
     let listaConsultas = await  appointmentService.Search({email:'dsadsadsa@dsadsads'})
     let consultas = await  appointmentService.GetAll({showFinished: true})
     res.render('list',{consultas})
 })
 
-
+app.get('/cadastro/list/search', async (req, res)=>{
+    let {search} = req.query
+    if(search == undefined || search.length == 0){
+        res.redirect('/cadastro/list')
+        return
+    }
+    let consultas = await  appointmentService.Search({termoBuscaQuery:search})
+    res.render('list',{consultas})
+})
 
 app.listen(3000,()=>{
     console.log('servidor rodando')
