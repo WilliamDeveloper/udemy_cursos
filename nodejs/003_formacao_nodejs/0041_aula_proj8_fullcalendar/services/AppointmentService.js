8c3a68326a3bd6d16d042f7fe089a059a0b51f9c
William Goulart Pacheco <gtaskwill@gmail.com>
2021-02-22 21:30:59 -0300

    #

---
+++ nodejs/003_formacao_nodejs/0041_aula_proj8_fullcalendar/services/AppointmentService.js
@@ -0,0 +1,24 @@
+const appointment = require('../models/Appointment')
+
+const mongoose = require('mongoose')
+
+const Appointment = mongoose.model('Appointment', appointment)
+
+class AppointmentService{
+    async create({ name, email, description, cpf, date, time }){
+        var newAppointment = new Appointment({
+            name, email, description, cpf, date, time, finished:false
+        })
+
+        try {
+            await  newAppointment.save()
+            return {success: true}
+        }catch (e) {
+            console.log(e)
+            return {success: false, error:e}
+        }
+
+    }
+}
+
+module.exports = new AppointmentService()
