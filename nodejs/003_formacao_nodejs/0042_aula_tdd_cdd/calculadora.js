d7ddf4816609473d7135e73761f2e7d6f84c74ea
William Goulart Pacheco <gtaskwill@gmail.com>
2021-02-23 22:48:41 -0300

    #'

--- nodejs/003_formacao_nodejs/0042_aula_tdd_cdd/calculadora.js
+++ nodejs/003_formacao_nodejs/0042_aula_tdd_cdd/calculadora.js
@@ -2,6 +2,11 @@ function soma(a,b) {
     return a + b
 }
 
+function mult(a,b) {
+    return a * b
+}
+
 module.exports = {
     soma,
+    mult,
 }
--- nodejs/003_formacao_nodejs/0042_aula_tdd_cdd/test/calculadora.test.js
+++ nodejs/003_formacao_nodejs/0042_aula_tdd_cdd/test/calculadora.test.js
@@ -4,3 +4,8 @@ test("deve retornar o valor 10 ao somar 5 + 5",()=>{
     let resultado = calculadora.soma(5,5)
     expect(resultado).toEqual(10)
 })
+
+test("deve retornar o valor 10 ao multiplicar 2 * 5",()=>{
+    let resultado = calculadora.mult(2,5)
+    expect(resultado).toEqual(10)
+})
