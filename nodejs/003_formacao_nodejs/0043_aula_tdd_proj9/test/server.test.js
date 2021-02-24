77b7313db470983450036fecd4d00361571269b5
William Goulart Pacheco <gtaskwill@gmail.com>
2021-02-24 00:12:19 -0300

    #'

--- nodejs/003_formacao_nodejs/0043_aula_tdd_proj9/test/server.test.js
+++ nodejs/003_formacao_nodejs/0043_aula_tdd_proj9/test/server.test.js
@@ -4,4 +4,11 @@ const request = supertest(app)
 
 test("deve responder na porta 3131",()=>{
 
+    //sempre dar um retorno na frente de uma promise por causa do jest
+    return request.get('/').then(res => {
+        const status =res.statusCode
+        expect(status).toEqual(200)
+        expect(res.body.cor).toEqual('vermelho')
+    })
+
 })
