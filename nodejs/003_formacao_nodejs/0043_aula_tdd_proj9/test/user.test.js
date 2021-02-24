510de15e9cede253a437e223e7ec16d13c0e946b
William Goulart Pacheco <gtaskwill@gmail.com>
2021-02-24 10:06:23 -0300

    #

---
+++ nodejs/003_formacao_nodejs/0043_aula_tdd_proj9/test/user.test.js
@@ -0,0 +1,11 @@
+const app = require('../src/app')
+const supertest = require('supertest')
+const request = supertest(app)
+
+
+describe("",()=>{
+
+    test("deve responder na porta 3131",()=>{
+    })
+
+})
