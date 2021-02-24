1eb87aa318ea53b847365b707b0751868876bc8c
William Goulart Pacheco <gtaskwill@gmail.com>
2021-02-23 22:27:11 -0300

    #'

--- nodejs/003_formacao_nodejs/0042_aula_tdd_cdd/index.js
+++ nodejs/003_formacao_nodejs/0042_aula_tdd_cdd/index.js
@@ -9,3 +9,7 @@ test("deve cadastrar um usuario quando receber um e-mail x",()=>{
 test("deve recusar o cadastro de usuario quando receber um email ja cadastrado",()=>{
 
 })
+
+it("should do something",()=>{
+
+})
