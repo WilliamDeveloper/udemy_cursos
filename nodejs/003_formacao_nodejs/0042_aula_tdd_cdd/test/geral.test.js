f2216c851cddf223cfb37a009c0701e904b29adb
William Goulart Pacheco <gtaskwill@gmail.com>
2021-02-23 23:13:39 -0300

    #'

--- nodejs/003_formacao_nodejs/0042_aula_tdd_cdd/test/geral.test.js
+++ nodejs/003_formacao_nodejs/0042_aula_tdd_cdd/test/geral.test.js
@@ -1,3 +1,8 @@
-test("a aplicacao deve responder na porta 31311",()=>{
+const supertest = require('supertest')
 
+let request = supertest('www.google.com.br')
+
+test("a aplicacao deve responder na porta 3131",()=>{
+    return request.get('/')
+        .then(res => expect(res.statusCode).toEqual(200))
 })
