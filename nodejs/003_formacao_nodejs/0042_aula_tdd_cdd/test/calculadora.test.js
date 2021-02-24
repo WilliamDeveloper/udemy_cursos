c7d286d8c72fc59417c221c09c5b1db80bd0cae4
William Goulart Pacheco <gtaskwill@gmail.com>
2021-02-23 22:42:59 -0300

    #'

--- nodejs/003_formacao_nodejs/0042_aula_tdd_cdd/test/calculadora.test.js
+++ nodejs/003_formacao_nodejs/0042_aula_tdd_cdd/test/calculadora.test.js
@@ -1,1 +1,6 @@
-test
+const calculadora = require("../calculadora")
+
+test("deve retornar o valor 10 ao somar 5 + 5",()=>{
+    let resultado = calculadora.soma(5,5)
+    expect(resultado).toEqual(10)
+})
