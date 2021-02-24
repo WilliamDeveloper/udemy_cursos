47d5acbe562002543126930a9032f5f6669439b2
William Goulart Pacheco <gtaskwill@gmail.com>
2021-02-23 23:40:44 -0300

    #'

--- nodejs/003_formacao_nodejs/0042_aula_tdd_cdd/src/app.js
+++ nodejs/003_formacao_nodejs/0042_aula_tdd_cdd/src/app.js
@@ -3,7 +3,10 @@ const app = express()
 
 app.get('/',(req,res)=>{
     res.json({success:true,name:'william'})
+})
 
+app.get('/cor/william',(req,res)=>{
+    res.json({success:true,cor:'vermelho'})
 })
 
 module.exports = app
--- nodejs/003_formacao_nodejs/0042_aula_tdd_cdd/test/geral2.test.js
+++ nodejs/003_formacao_nodejs/0042_aula_tdd_cdd/test/geral2.test.js
@@ -6,3 +6,7 @@ let request = supertest(app)
 test("deve responder na porta 3131",()=>{
     return request.get('/').then(res => expect(res.statusCode).toEqual(200))
 })
+
+test("deve retornar vermelho como cor favorita do william",()=>{
+    return request.get('/cor/william').then(res => expect(res.body.cor).toEqual('vermelho'))
+})
