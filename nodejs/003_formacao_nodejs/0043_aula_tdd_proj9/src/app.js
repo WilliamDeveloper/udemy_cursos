955974c1875e70218af113c2e9d16bf3729333e6
William Goulart Pacheco <gtaskwill@gmail.com>
2021-02-24 00:16:46 -0300

    #'

--- nodejs/003_formacao_nodejs/0043_aula_tdd_proj9/src/app.js
+++ nodejs/003_formacao_nodejs/0043_aula_tdd_proj9/src/app.js
@@ -1,6 +1,10 @@
 const express = require('express')
 const app = express()
 
+//bodyparser
+app.use(express.urlencoded({extended:false}))
+app.use(express.json())
+
 app.get('/',(req,res)=>{
     res.json({success:true,name:'william'})
 })
