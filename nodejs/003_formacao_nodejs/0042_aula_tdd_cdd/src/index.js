4c0b6749a29d3353cce27a3df0722b5d805e5de8
William Goulart Pacheco <gtaskwill@gmail.com>
2021-02-23 23:23:38 -0300

    #'

--- nodejs/003_formacao_nodejs/0042_aula_tdd_cdd/src/index.js
+++ nodejs/003_formacao_nodejs/0042_aula_tdd_cdd/src/index.js
@@ -1,1 +1,11 @@
 const express = require('express')
+const app = express()
+
+app.get('/',(req,res)=>{
+    res.statusCode(200)
+    res.send('oi')
+})
+
+app.listen(3131,()=>{
+    console.log('rodando app na porta 3131')
+})
--- nodejs/003_formacao_nodejs/0042_aula_tdd_cdd/test/geral.test.js
+++ nodejs/003_formacao_nodejs/0042_aula_tdd_cdd/test/geral.test.js
@@ -1,8 +1,7 @@
 const supertest = require('supertest')
 
-let request = supertest('www.google.com.br')
+let request = supertest('http://localhost:3131')
 
-test("a aplicacao deve responder na porta 3131",()=>{
-    return request.get('/')
-        .then(res => expect(res.statusCode).toEqual(200))
+test("deve responder na porta 3131",()=>{
+    return request.get('/').then(res => expect(res.statusCode).toEqual(200))
 })
