e77da6b593954081c3b2b3e4f9832f7e04bddb4d
William Goulart Pacheco <gtaskwill@gmail.com>
2021-02-24 10:04:15 -0300

    #

--- nodejs/003_formacao_nodejs/0043_aula_tdd_proj9/src/app.js
+++ nodejs/003_formacao_nodejs/0043_aula_tdd_proj9/src/app.js
@@ -1,9 +1,13 @@
 const express = require('express')
 const app = express()
+const mongoose = require('mongoose')
 
 //bodyparser
 app.use(express.urlencoded({extended:false}))
 app.use(express.json())
+
+//ir no cmd -> "mongo" -> "use guiapics"
+mongoose.connect('mongodb://localhost:27017/guiapics', {useNewUrlParser:true,useUnifiedTopology:true})
 
 app.get('/',(req,res)=>{
     res.json({success:true,name:'william'})
