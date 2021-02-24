5fda8c590e85559ddbe805c6c4974e20c7f344cf
William Goulart Pacheco <gtaskwill@gmail.com>
2021-02-24 10:18:37 -0300

    #

--- nodejs/003_formacao_nodejs/0043_aula_tdd_proj9/test/user.test.js
+++ nodejs/003_formacao_nodejs/0043_aula_tdd_proj9/test/user.test.js
@@ -6,7 +6,18 @@ const request = supertest(app)
 describe("Cadastro de usuario",()=>{
 
     test("Deve cadastrar um usuario com sucesso",()=>{
+        let time = Date.now()
+        let email = `${time}@gmail.com`
+        let user = {name:'William', email: email, password: '123456'}
 
+        return request
+            .post("/user").send(user).then(res=>{
+                let status = res.statusCode
+                expect(status).toEqual(200)
+                expect(res.body.email).toEqual(email)
+            }).catch(error=>{
+                fail(error)
+            })
     })
 
 })
