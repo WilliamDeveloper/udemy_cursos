2ecb30fa9990d0933191df8809f2a152009b678d
William Goulart Pacheco <gtaskwill@gmail.com>
2021-02-26 12:27:35 -0300

    #

--- nodejs/003_formacao_nodejs/0044_aula_mercado_pago/index.js
+++ nodejs/003_formacao_nodejs/0044_aula_mercado_pago/index.js
@@ -2,9 +2,12 @@ const express = require('express')
 const MercadoPago = require('mercadopago')
 const app = express()
 
+const mp_access_token = process.env['MB_ACCESS_TOKEN']
+const mp_public_key = process.env['MP_PUBLIC_KEY']
+
 MercadoPago.configure({
     sandbox:true,
-    acces_token: 'Test'
+    acces_token: access_token
 })
 
 app.listen(3000, (req,res)=>{
