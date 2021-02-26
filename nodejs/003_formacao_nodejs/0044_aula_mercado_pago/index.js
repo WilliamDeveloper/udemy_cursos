68bf92883ae0be94f3cc74443f32d86b2c127ff0
William Goulart Pacheco <gtaskwill@gmail.com>
2021-02-26 11:06:38 -0300

    #

---
+++ nodejs/003_formacao_nodejs/0044_aula_mercado_pago/index.js
@@ -0,0 +1,11 @@
+const express = require('express')
+const MercadoPago = require('mercadopago')
+const app = express()
+
+MercadoPago.configure({
+    sandbox:true
+})
+
+app.listen(3000, (req,res)=>{
+    console.log('rodando servidor')
+})
