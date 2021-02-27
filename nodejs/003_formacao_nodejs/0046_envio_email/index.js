fa2076e0a017bd3a3c984740e365d8bfc7cf2e78
William Goulart Pacheco <gtaskwill@gmail.com>
2021-02-27 11:04:18 -0300

    #

--- nodejs/003_formacao_nodejs/0046_envio_email/index.js
+++ nodejs/003_formacao_nodejs/0046_envio_email/index.js
@@ -1,13 +1,23 @@
 const nodemailer = require('nodemailer')
 
+// const credenciaisEmail = {
+//     host: process.env['MAIL_MAILTRAP_HOST'],
+//     port: process.env['MAIL_MAILTRAP_PORT'],
+//     auth: {
+//         user: process.env['MAIL_MAILTRAP_USER'],
+//         pass: process.env['MAIL_MAILTRAP_PASS']
+//     }
+// }
+
 const credenciaisEmail = {
-    host: process.env['MAIL_MAILTRAP_HOST'],
-    port: process.env['MAIL_MAILTRAP_PORT'],
+    host: process.env['MAIL_GMAIL_HOST_SMTP'],
+    port: process.env['MAIL_GMAIL_PORT_SMTP'],
     auth: {
-        user: process.env['MAIL_MAILTRAP_USER'],
-        pass: process.env['MAIL_MAILTRAP_PASS']
+        user: process.env['MAIL_GMAIL_USER'],
+        pass: process.env['MAIL_GMAIL_PASS']
     }
 }
+
 
 console.log(credenciaisEmail)
 
@@ -15,10 +25,10 @@ const transporter = nodemailer.createTransport(credenciaisEmail)
 
 const dadosEmail = {
     from: "William Pacheco <blau2@gmail.com>",
-    to: "dg2@gmail.com",
+    to: "blau@gmail.com, blau2@gmail.com",
     subject: "Oi, este eh um email de teste",
-    text: "ola sou teobaldo",
-    html: 'ola sou teobando2'
+    text: "ola sou nodeman",
+    html: 'ola sou nodeman2'
 
 }
 
