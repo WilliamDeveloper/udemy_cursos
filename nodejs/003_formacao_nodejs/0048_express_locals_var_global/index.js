c8cf171ff92df922af85b07a30e355b66d2fc1ea
William Goulart Pacheco <gtaskwill@gmail.com>
2021-02-27 12:36:32 -0300

    #

--- nodejs/003_formacao_nodejs/0048_express_locals_var_global/index.js
+++ nodejs/003_formacao_nodejs/0048_express_locals_var_global/index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express()
 
-//bodyparser
+//bodyparser -> que agora ta dentro do express
 app.use(express.urlencoded({extended:false}))
 app.use(express.json());
 
