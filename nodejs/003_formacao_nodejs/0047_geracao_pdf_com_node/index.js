74537ef74aefc76246f0ffd758b1c27b631c59c3
William Goulart Pacheco <gtaskwill@gmail.com>
2021-02-27 12:14:27 -0300

    #

--- nodejs/003_formacao_nodejs/0047_geracao_pdf_com_node/index.js
+++ nodejs/003_formacao_nodejs/0047_geracao_pdf_com_node/index.js
@@ -1,4 +1,5 @@
 const pdf = require('html-pdf')
+const ejs = require('ejs')
 
 const nome= "william"
 const imagem = 'https://lh3.googleusercontent.com/proxy/thw2h7yRvN-mcWrgdz-LzBIYPlMrRUffmdZnf4ojuM2cbzCLj3yH1FaW5tXbXFKstVIvZo_0EUGWXIqRAEn349VKBb5FqMo811wLHXFAXwZlTR7BOGn4s3ZZEk-M4Sikn6Vn01suLfH3rC0Se1t1zRJKRuMkSWU'
@@ -14,6 +15,26 @@ const htmlConteudo = `
 pdf
     .create(htmlConteudo,{})
     .toFile("./meupdflindao.pdf",(error, res)=>{
+        if(error){
+            console.log('error: ', error)
+        }else{
+            console.log('res: ', res)
+        }
+    })
+
+
+
+const htmlConteudo = `
+    <h1 style="color:red"> Agora sim heim!</h1>
+    <hr>  
+    <p>Esse pdf tem muito conteudo heim!</p>
+    <p>${nome}</p>
+    <img src="${imagem}" alt="">
+`
+
+pdf
+    .create(htmlConteudo,{})
+    .toFile("./meupdflindao2.pdf",(error, res)=>{
         if(error){
             console.log('error: ', error)
         }else{
