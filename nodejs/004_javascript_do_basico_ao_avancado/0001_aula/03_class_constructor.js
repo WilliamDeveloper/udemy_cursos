86f51113e53fbfc721b32aba8ff039273574440a
William Goulart Pacheco <gtaskwill@gmail.com>
2021-02-28 19:02:50 -0300

    #

---
+++ nodejs/004_javascript_do_basico_ao_avancado/0001_aula/03_class_constructor.js
@@ -0,0 +1,12 @@
+function criaCachorro(raca, patas, cor) {
+    const cachorro = Object.create({})
+    cachorro.raca = raca
+    cachorro.patas = patas
+    cachorro.cor = cor
+    return cachorro
+    
+}
+
+const dog = criaCachorro("pitbul",4, "verde")
+
+console.log(dog)
