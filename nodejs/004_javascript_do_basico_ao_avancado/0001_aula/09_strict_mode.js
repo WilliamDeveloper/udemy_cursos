4f58fca71dbd975a53073b79f6055a94e7014950
William Goulart Pacheco <gtaskwill@gmail.com>
2021-02-28 20:38:46 -0300

    #

--- nodejs/004_javascript_do_basico_ao_avancado/0001_aula/09_strict_mode.js
+++ nodejs/004_javascript_do_basico_ao_avancado/0001_aula/09_strict_mode.js
@@ -1,10 +1,23 @@
-"use strict"
+// "use strict"
 
 
 // "use strict" previne q façamos erros bobos no codigo javascript
 
 let opa = "teste"
 
+// com use strict essa linha daria erro evitando erro bobo
 delete Object.prototype
 
 console.log(opa)
+
+
+function funcao() {
+    "use strict"
+
+    let blau = "festa"
+
+    console.log(blau)
+}
+
+
+funcao()
