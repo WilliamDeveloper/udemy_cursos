ed00b18613a42884f612a9f838e95633b4b67e16
William Goulart Pacheco <gtaskwill@gmail.com>
2021-02-28 19:34:47 -0300

    #

---
+++ nodejs/004_javascript_do_basico_ao_avancado/0001_aula/05_class_constructor_symbols.js
@@ -0,0 +1,47 @@
+//e3s6 - versao 2015
+class Cachorro {
+
+    constructor(raca, patas, cor){
+        this.raca = raca
+        this.patas = patas
+        this.cor = cor
+    }
+
+    uivar() {
+        console.log('uivar')
+    }
+
+    latir () {
+        console.log('outra forma de declarar funcao em uma classe')
+    }
+}
+
+let patas = Symbol()
+
+//criando symbol
+Cachorro.prototype[patas] = 4
+
+
+
+console.log(Cachorro.prototype.raca)
+console.log(Cachorro.prototype.patas)
+console.log(Cachorro.prototype.cor)
+
+//modificando a definicao de valor default de um atributo de classe na hora
+Cachorro.prototype.cor2=44
+
+const dog = new Cachorro("pitbul",4, "verde")
+
+console.log(Cachorro.prototype.raca)
+console.log(Cachorro.prototype.patas)
+console.log(Cachorro.prototype.cor)
+console.log(Cachorro.prototype.cor2)
+
+
+console.log(dog)
+dog.uivar()
+dog.latir()
+
+// acessando symbol
+console.log(Cachorro.prototype[patas])
+console.log(dog[patas])
