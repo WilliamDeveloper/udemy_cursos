86788c49fe188ec5d214d6a6a5241a8898f4b5f1
William Goulart Pacheco <gtaskwill@gmail.com>
2021-02-28 19:27:07 -0300

    #

--- nodejs/004_javascript_do_basico_ao_avancado/0001_aula/04_class_constructor_es6.js
+++ nodejs/004_javascript_do_basico_ao_avancado/0001_aula/04_class_constructor_es6.js
@@ -18,6 +18,8 @@ class Cachorro {
 console.log(Cachorro.prototype.raca)
 console.log(Cachorro.prototype.patas)
 console.log(Cachorro.prototype.cor)
+
+//modificando a definicao de valor default de um atributo de classe na hora
 Cachorro.prototype.cor2=44
 
 const dog = new Cachorro("pitbul",4, "verde")
