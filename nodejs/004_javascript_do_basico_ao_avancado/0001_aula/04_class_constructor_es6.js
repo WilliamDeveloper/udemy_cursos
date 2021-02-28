27ed2f759eef61b0ed8c722e8979c51c8eb149aa
William Goulart Pacheco <gtaskwill@gmail.com>
2021-02-28 19:26:19 -0300

    #

--- nodejs/004_javascript_do_basico_ao_avancado/0001_aula/04_class_constructor_es6.js
+++ nodejs/004_javascript_do_basico_ao_avancado/0001_aula/04_class_constructor_es6.js
@@ -15,9 +15,18 @@ class Cachorro {
     }
 }
 
-
+console.log(Cachorro.prototype.raca)
+console.log(Cachorro.prototype.patas)
+console.log(Cachorro.prototype.cor)
+Cachorro.prototype.cor2=44
 
 const dog = new Cachorro("pitbul",4, "verde")
+
+console.log(Cachorro.prototype.raca)
+console.log(Cachorro.prototype.patas)
+console.log(Cachorro.prototype.cor)
+console.log(Cachorro.prototype.cor2)
+
 
 console.log(dog)
 dog.uivar()
