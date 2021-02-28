9206021c1673def715600cc9d7cd7c6354c7bad3
William Goulart Pacheco <gtaskwill@gmail.com>
2021-02-28 19:07:38 -0300

    #

--- nodejs/004_javascript_do_basico_ao_avancado/0001_aula/03_class_constructor_new.js
+++ nodejs/004_javascript_do_basico_ao_avancado/0001_aula/03_class_constructor_new.js
@@ -2,8 +2,17 @@ function Cachorro(raca, patas, cor) {
    this.raca = raca
     this.patas = patas
     this.cor = cor
+    this.uivar = function () {
+        console.log('uivar')
+    }
+}
+
+Cachorro.prototype.latir = function () {
+    console.log('outra forma de declarar funcao em uma classe')
 }
 
 const dog = new Cachorro("pitbul",4, "verde")
 
 console.log(dog)
+dog.uivar()
+dog.latir()
