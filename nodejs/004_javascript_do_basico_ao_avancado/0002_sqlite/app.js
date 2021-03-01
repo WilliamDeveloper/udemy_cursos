566abe5e250f607e39bc38d304de65681d8ad6e6
William Goulart Pacheco <gtaskwill@gmail.com>
2021-03-01 20:57:53 -0300

    #

--- nodejs/004_javascript_do_basico_ao_avancado/0002_sqlite/app.js
+++ nodejs/004_javascript_do_basico_ao_avancado/0002_sqlite/app.js
@@ -1,12 +1,23 @@
 const express = require('express')
 const app = express()
 
-const PORT = 3000
+const db = require('./db/connection')
 
+
+//db connection
+db.authenticate().then(()=>{
+    console.log('conectou ao banco com sucesso')
+})
+
+
+
+//routes
 app.get('/',(req,res)=>{
     res.send('OK')
 })
 
+
+const PORT = 3000
 app.listen(PORT, ()=>{
     console.log('servidor rodando')
 })
