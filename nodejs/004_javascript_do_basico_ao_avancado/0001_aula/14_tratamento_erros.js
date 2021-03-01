980e470478022dca0f4b28864a6e0fd076e956ca
William Goulart Pacheco <gtaskwill@gmail.com>
2021-02-28 22:39:33 -0300

    #'

--- nodejs/004_javascript_do_basico_ao_avancado/0001_aula/14_tratamento_erros.js
+++ nodejs/004_javascript_do_basico_ao_avancado/0001_aula/14_tratamento_erros.js
@@ -3,4 +3,20 @@ try{
     throw new  Error("errro de proposito")
 }catch (e) {
     console.log(e)
+}finally {
+    console.log("excecuta sempre")
 }
+
+console.log("continuou")
+
+
+try{
+    let a = 5
+
+}catch (e) {
+    console.log(e)
+}finally {
+    console.log("excecuta sempre")
+}
+
+console.log("continuou")
