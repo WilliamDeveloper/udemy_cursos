16d37ed5db56f2fd088fa6d834947d0018731779
William Goulart Pacheco <gtaskwill@gmail.com>
2021-03-01 14:27:52 -0300

    #

---
+++ nodejs/004_javascript_do_basico_ao_avancado/0001_aula/17_elementos_dom.js
@@ -0,0 +1,10 @@
+console.log(document.body)
+console.log(document.body.childNodes)
+console.log(document.body.childNodes[0])
+console.log(document.body.childNodes[0].childNodes[0])
+console.log(document.body.childNodes[0].childNodes[0].innerText)
+
+console.log(document.getElementsByTagName("h1"))
+console.log(document.getElementsByClassName("h1"))
+console.log(document.getElementsByName("h1"))
+console.log(document.getElementById("h1"))
