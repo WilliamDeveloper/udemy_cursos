a9a8dea4e49c271fcb2b07820880ebfea8bfee86
William Goulart Pacheco <gtaskwill@gmail.com>
2021-03-01 10:00:59 -0300

    #

--- nodejs/004_javascript_do_basico_ao_avancado/0001_aula/15_expressao_regular.js
+++ nodejs/004_javascript_do_basico_ao_avancado/0001_aula/15_expressao_regular.js
@@ -35,12 +35,20 @@ console.log('contem digitos ? ',sentenca.test("jhdsjahdjah12345jjhdhadhag"))
 
 // nao possui numero ?
 sentenca = new RegExp("\D")
-console.log('nao possui numero ? ',sentenca.test("jhdsjahdjah12345jjhdhadhag"))
+console.log('nao possui numero ? ',sentenca.test("jhdsjahdjahjjhdhadhag"))
+console.log('nao possui numero ? ',sentenca.test("11111111"))
+console.log('nao possui numero ? ',sentenca.test("11111111dsadsadsa"))
 
 
 //contem so letra ?
 sentenca = new RegExp("\w")
 console.log('contem so letra ? ',sentenca.test("wwwdsadsa"))
+
+//contem so letra ?
+sentenca = new RegExp("\W")
+console.log('nao possui letra ? ',sentenca.test("wwwdsadsa4"))
+console.log('nao possui letra ? ',sentenca.test("dadadsadsadsa"))
+console.log('nao possui letra ? ',sentenca.test("43434344"))
 
 //possui algum espaco em branco ?
 sentenca = new RegExp("\s")
