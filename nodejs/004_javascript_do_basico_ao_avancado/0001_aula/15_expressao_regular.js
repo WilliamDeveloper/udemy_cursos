e955d4bead2c9218748767801c80803e8872c627
William Goulart Pacheco <gtaskwill@gmail.com>
2021-03-01 09:41:39 -0300

    #

--- nodejs/004_javascript_do_basico_ao_avancado/0001_aula/15_expressao_regular.js
+++ nodejs/004_javascript_do_basico_ao_avancado/0001_aula/15_expressao_regular.js
@@ -7,11 +7,16 @@ console.log(reg1.test("hdsjadhasjtestjdashjdsah"))
 console.log(reg1.test("sdadasdsadsadsa"))
 
 
-let sentenca
+let sentenca;
+//-------------------------------
 // conjunto de caracteres
+//-------------------------------
 
+//ver se a string possui algum desses numeros
 sentenca = new RegExp("[12345]")
-console.log(sentenca.test("jhdsjahdjah12345jjhdhadhag"))
+console.log(sentenca.test("jhdsjahdjah6jjhdhadhag"))
+console.log(sentenca.test("jhdsjahdjah35jjhdhadhag"))
 
+//ver se a string possui algum desses numeros de 0 a 9
 sentenca = new RegExp("[0-9]")
 console.log(sentenca.test("jhdsjahdjah12345jjhdhadhag"))
