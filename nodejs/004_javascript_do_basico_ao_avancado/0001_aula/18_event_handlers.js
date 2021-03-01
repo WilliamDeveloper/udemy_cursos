cfc035fddcc3e2cb27bcdd059360644d1a281655
William Goulart Pacheco <gtaskwill@gmail.com>
2021-03-01 15:27:12 -0300

    #

--- nodejs/004_javascript_do_basico_ao_avancado/0001_aula/18_event_handlers.js
+++ nodejs/004_javascript_do_basico_ao_avancado/0001_aula/18_event_handlers.js
@@ -1,6 +1,14 @@
 let btn = document.querySelector("#btn")
 
 // atrelando um handler(tratamento de evento) ao evento click
-btn.addEventListener("click",function () {
+function msg () {
     console.log("clicou")
-})
+}
+
+btn.addEventListener("click",msg)
+
+
+setTimeout(function () {
+    console.log('removendo listener de evento do botao')
+    btn.removeEventListener("click",msg )
+},10*1000)
