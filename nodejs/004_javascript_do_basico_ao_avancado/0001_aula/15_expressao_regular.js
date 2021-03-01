4e873f668f42cc4dc7608054015b8883ee41a1a1
William Goulart Pacheco <gtaskwill@gmail.com>
2021-03-01 09:53:49 -0300

    #

--- nodejs/004_javascript_do_basico_ao_avancado/0001_aula/15_expressao_regular.js
+++ nodejs/004_javascript_do_basico_ao_avancado/0001_aula/15_expressao_regular.js
@@ -24,3 +24,19 @@ console.log(sentenca.test("jhdsjahdjah35jjhdhadhag"))
 //ver se a string possui algum desses numeros de 0 a 9
 sentenca = new RegExp("[0-9]")
 console.log(sentenca.test("jhdsjahdjah12345jjhdhadhag"))
+
+//-------------------------------
+// caracteres especiais
+//-------------------------------
+
+// contem digitos ?
+sentenca = new RegExp("\d")
+console.log('contem digitos ? ',sentenca.test("jhdsjahdjah12345jjhdhadhag"))
+
+//contem so letra ?
+sentenca = new RegExp("\w")
+console.log('contem so letra ? ',sentenca.test("wwwdsadsa"))
+
+//possui algum espaco em branco ?
+sentenca = new RegExp("\s")
+console.log("possui algum espaco em branco ? ",sentenca.test("wwwdsadsa asdasdsa"))
