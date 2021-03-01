12242c09e06b0e529c14451520eaeaadffd16d81
William Goulart Pacheco <gtaskwill@gmail.com>
2021-02-28 23:14:49 -0300

    #'

--- nodejs/004_javascript_do_basico_ao_avancado/0001_aula/15_expressao_regular.js
+++ nodejs/004_javascript_do_basico_ao_avancado/0001_aula/15_expressao_regular.js
@@ -5,3 +5,10 @@ let reg2 = /test/
 
 console.log(reg1.test("hdsjadhasjtestjdashjdsah"))
 console.log(reg1.test("sdadasdsadsadsa"))
+
+
+let sentenca
+// conjunto de caracteres
+
+sentenca = new RegExp("[12345]")
+console.log(sentenca.test("jhdsjahdjah12345jjhdhadhag"))
