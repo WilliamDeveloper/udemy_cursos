5e725905a0f297cf33d825eaf559a9728c7f172c
William Goulart Pacheco <gtaskwill@gmail.com>
2021-03-01 09:43:14 -0300

    #

--- nodejs/004_javascript_do_basico_ao_avancado/0001_aula/15_expressao_regular.js
+++ nodejs/004_javascript_do_basico_ao_avancado/0001_aula/15_expressao_regular.js
@@ -1,13 +1,17 @@
-let reg1 = new RegExp("test")
-//ou
-let reg2 = /test/
-
-
-console.log(reg1.test("hdsjadhasjtestjdashjdsah"))
-console.log(reg1.test("sdadasdsadsadsa"))
-
-
 let sentenca;
+//-------------------------------
+// termo exato
+//-------------------------------
+
+sentenca = new RegExp("test")
+console.log(sentenca.test("hdsjadhasjtestjdashjdsah"))
+
+//ou
+sentenca = /test/
+console.log(sentenca.test("sdadasdsadsadsa"))
+
+
+
 //-------------------------------
 // conjunto de caracteres
 //-------------------------------
