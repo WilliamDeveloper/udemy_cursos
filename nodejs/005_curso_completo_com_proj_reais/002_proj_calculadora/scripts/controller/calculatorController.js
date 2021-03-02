bcf924a007f95557b9a64234d6f53ba8a27caab8
William Goulart Pacheco <gtaskwill@gmail.com>
2021-03-02 20:41:33 -0300

    #

--- nodejs/005_curso_completo_com_proj_reais/002_proj_calculadora/scripts/controller/calculatorController.js
+++ nodejs/005_curso_completo_com_proj_reais/002_proj_calculadora/scripts/controller/calculatorController.js
@@ -8,25 +8,22 @@ class CalculatorController{
         this.dateEl = document.querySelector("#data")
         this.timeEl = document.querySelector("#hora")
 
-        this._displayCalculator = 0
         this._currenteDate = ''
         this.initialize()
     }
 
     initialize(){
-
-
         displayCalculatorEl.innerHTML = '4567'
         dateEl.innerHTML = '01/05/2020'
         timeEl.innerHTML = '20:30'
     }
 
     get displayCalculator(){
-        return this._displayCalculator
+        return this.displayCalculatorEl.innerHTML
     }
 
     set displayCalculator(value){
-        this._displayCalculator = value
+        this.displayCalculatorEl.innerHTML = value
     }
 
 
