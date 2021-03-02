a37328d55cab686feaac18171acebebe656e6fff
William Goulart Pacheco <gtaskwill@gmail.com>
2021-03-02 19:07:24 -0300

    #

--- nodejs/005_curso_completo_com_proj_reais/001_aula/06_array.js
+++ nodejs/005_curso_completo_com_proj_reais/001_aula/06_array.js
@@ -2,4 +2,9 @@ let carros =['palio 98', "toro", "uno"]
 console.log(carros)
 
 console.log(carros.length)
+console.log("valor na posicao 0 ",carros[0])
 console.log("posicao elemento uno no array ",carros.indexOf("uno"))
+
+carros.forEach( carro =>{
+    console.log(carro)
+})
