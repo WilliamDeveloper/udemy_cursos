f7cc918ccb613b75367d48f8338fa6b41f20d46e
William Goulart Pacheco <gtaskwill@gmail.com>
2021-03-01 21:25:07 -0300

    #

--- nodejs/004_javascript_do_basico_ao_avancado/0002_sqlite/app.js
+++ nodejs/004_javascript_do_basico_ao_avancado/0002_sqlite/app.js
@@ -1,8 +1,12 @@
 const express = require('express')
 const app = express()
+const jobRoute = require('./routes/jobRoutes')
 
 const db = require('./db/connection')
 
+//bodyparser -> que agora ta dentro do express
+app.use(express.urlencoded({extended:false}))
+app.use(express.json());
 
 //db connection
 db.authenticate().then(()=>{
@@ -11,10 +15,14 @@ db.authenticate().then(()=>{
 
 
 
+
+
 //routes
 app.get('/',(req,res)=>{
     res.send('OK')
 })
+
+app.use('/job', jobRoute)
 
 
 const PORT = 3000
--- nodejs/004_javascript_do_basico_ao_avancado/0002_sqlite/routes/jobRoutes.js
+++ nodejs/004_javascript_do_basico_ao_avancado/0002_sqlite/routes/jobRoutes.js
@@ -1,3 +1,13 @@
 const express = require('express')
 const router = express.Router()
 const Job = require("../models/Job")
+
+//
+router.post('/add',(req,res)=>{
+    let {title,salary,company,description,email,new_job} = req.body
+
+    Job.create({
+        title,salary,company,description,email,new_job
+    }).then(res=>{console.log('criado')})
+     .catch(error=>{ console.log('error',error)})
+})
