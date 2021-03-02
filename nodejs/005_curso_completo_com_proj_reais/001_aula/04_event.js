7f8df0aed7fc5ec82e263c9b8fd954ef1b56e759
William Goulart Pacheco <gtaskwill@gmail.com>
2021-03-02 18:46:07 -0300

    #

--- nodejs/005_curso_completo_com_proj_reais/001_aula/04_event.js
+++ nodejs/005_curso_completo_com_proj_reais/001_aula/04_event.js
@@ -1,7 +1,18 @@
+
+//evento na janela
+/*
+historico usuario,
+refresh janela,
+propriedade da janela,
+abrir nova janela window.open,
+fechar janela window.close
+*/
 window.addEventListener('focus', event =>{
     console.log('focus')
 })
 
+
+// evento na pagina
 document.addEventListener('click', event =>{
     console.log('click')
 })
