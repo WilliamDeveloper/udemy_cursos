03818f3225d5708dfc073b1b03456878a98e1b5b
William Goulart Pacheco <gtaskwill@gmail.com>
2021-03-01 21:05:46 -0300

    #

---
+++ nodejs/004_javascript_do_basico_ao_avancado/0002_sqlite/db/criar_tabela.sql
@@ -0,0 +1,15 @@
+drop table jobs;
+
+CREATE TABLE jobs(
+
+  id integer primary key autoincrement,
+  title text,
+  salary text,
+  company text,
+  email text,
+  new integer,
+  createdAt text,
+  updatedAt text
+
+
+)
