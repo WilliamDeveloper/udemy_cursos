58fb7b809a8299ec8d91a0238a09f0d2eea16807
William Goulart Pacheco <gtaskwill@gmail.com>
2021-03-02 18:49:16 -0300

    #

--- nodejs/005_curso_completo_com_proj_reais/001_aula/05_datas.js
+++ nodejs/005_curso_completo_com_proj_reais/001_aula/05_datas.js
@@ -1,2 +1,2 @@
-let agoraInTimeStamp = Date.now()
-console.log('agoraInTimeStamp desde 01/01/1970',agoraInTimeStamp)
+let agoraInTimeStampInTimeMiles = Date.now()
+console.log('agoraInTimeStamp desde 01/01/1970',agoraInTimeStampInTimeMiles)
