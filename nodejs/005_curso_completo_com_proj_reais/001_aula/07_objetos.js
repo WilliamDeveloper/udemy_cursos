5fd80a92919cf999b73a37f781d03186a1d8b8f0
William Goulart Pacheco <gtaskwill@gmail.com>
2021-03-02 19:18:12 -0300

    #

--- nodejs/005_curso_completo_com_proj_reais/001_aula/07_objetos.js
+++ nodejs/005_curso_completo_com_proj_reais/001_aula/07_objetos.js
@@ -1,3 +1,12 @@
 //funcoes -> metodos
 // variaveis sao propriedades
 // objeto = instancia de uma classe
+
+let celular = function () {
+    //atributo da classe
+    this.cor = "prata"
+
+}
+
+let objeto = new celular()
+console.log(objeto)
