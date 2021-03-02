3ad54543c0aa405153c00a12d852c7d703866ae8
William Goulart Pacheco <gtaskwill@gmail.com>
2021-03-02 18:27:56 -0300

    #

---
+++ nodejs/005_curso_completo_com_proj_reais/001_aula/03_funcao.js
@@ -0,0 +1,12 @@
+function calc(x1,operator, x2) {
+    return eval(`${x1} ${operator} ${x2}`)
+}
+
+console.log('resultado ',calc(4,'+',2))
+console.log('resultado ',calc(4,'-',2))
+console.log('resultado ',calc(4,'*',2))
+console.log('resultado ',calc(4,'/',2))
+
+
+let result = calc(4,'+',2)
+console.log(result)
