0dc7572a338f2d70c4ee2640cac8c17f7d77de63
William Goulart Pacheco <gtaskwill@gmail.com>
2021-03-03 20:16:09 -0300

    #

--- nodejs/005_curso_completo_com_proj_reais/003_gerenciamento_usuarios/controllers/UserController.js
+++ nodejs/005_curso_completo_com_proj_reais/003_gerenciamento_usuarios/controllers/UserController.js
@@ -4,6 +4,20 @@ class UserController {
         this.formEl = document.getElementById(formId)
     }
 
+    onSubmit(){
+
+
+        this.formEl.addEventListener("submit",(event)=>{
+
+            event.preventDefault()
+
+            console.log('vou submeter')
+
+            this.getValues()
+
+        })
+    }
+
     getValues(){
         let user = {}
         //descobrir no console os atributos do objeto
@@ -30,6 +44,24 @@ class UserController {
             user.photo,
             user.admin
         )
+    }
+
+    addLine(dataUser, tableId){
+
+        document.getElementById(tableId).innerHTML = `
+            <tr>
+                <td><img src="dist/img/user1-128x128.jpg" alt="User Image" class="img-circle img-sm"></td>
+                <td>${dataUser.name}</td>
+                <td>${dataUser.email}</td>
+                <td>${dataUser.admin}</td>
+                <td>${dataUser.birth}</td>
+                <td>
+                <button type="button" class="btn btn-primary btn-xs btn-flat">Editar</button>
+                <button type="button" class="btn btn-danger btn-xs btn-flat">Excluir</button>
+                </td>
+            </tr>
+    `
+
     }
 
 
--- nodejs/005_curso_completo_com_proj_reais/003_gerenciamento_usuarios/index.js
+++ nodejs/005_curso_completo_com_proj_reais/003_gerenciamento_usuarios/index.js
@@ -1,72 +1,9 @@
-let name = document.querySelector('#exampleInputName')
-let gender = document.querySelectorAll('#form-user-create [name=gender]:checked')
-let birth = document.querySelector('#exampleInputBirth')
-let country = document.querySelector('#exampleInputCountry')
-let email = document.querySelector('#exampleInputEmail1')
-let password = document.querySelector('#exampleInputPassword1')
-let photo = document.querySelector('#exampleInputFile')
-let admin = document.querySelector('#exampleInputAdmin')
+// let name = document.querySelector('#exampleInputName')
+// let gender = document.querySelectorAll('#form-user-create [name=gender]:checked')
+// let birth = document.querySelector('#exampleInputBirth')
+// let country = document.querySelector('#exampleInputCountry')
+// let email = document.querySelector('#exampleInputEmail1')
+// let password = document.querySelector('#exampleInputPassword1')
+// let photo = document.querySelector('#exampleInputFile')
+// let admin = document.querySelector('#exampleInputAdmin')
 
-exampleInputName.value = 'William'
-
-let user = {}
-
-function addLine(dataUser){
-    console.log(dataUser)
-
-    document.getElementById("table-users").innerHTML = `
-            <tr>
-                <td><img src="dist/img/user1-128x128.jpg" alt="User Image" class="img-circle img-sm"></td>
-                <td>${dataUser.name}</td>
-                <td>${dataUser.email}</td>
-                <td>${dataUser.admin}</td>
-                <td>${dataUser.birth}</td>
-                <td>
-                <button type="button" class="btn btn-primary btn-xs btn-flat">Editar</button>
-                <button type="button" class="btn btn-danger btn-xs btn-flat">Excluir</button>
-                </td>
-            </tr>
-    `
-
-}
-
-let fields = document.querySelectorAll("#form-user-create [name]")
-
-
-
-// document.querySelectorAll("button").forEach((button)=>{
-//     button.addEventListener("click",()=>{
-//         console.log('clicou em mim')
-//     })
-// })
-
-document.getElementById("form-user-create").addEventListener("submit",(event)=>{
-    event.preventDefault()
-    console.log('vou submeter')
-
-    fields.forEach(field=>{
-
-        if(field.name == "gender"){
-            if(field.checked){
-                user[field.name] = field.value
-            }
-        }else{
-            user[field.name] = field.value
-        }
-
-
-    })
-
-    let objectUser = new User(
-        user.name,
-        user.gender,
-        user.birth,
-        user.country,
-        user.email,
-        user.password,
-        user.photo,
-        user.admin
-    )
-    addLine(objectUser)
-
-})
