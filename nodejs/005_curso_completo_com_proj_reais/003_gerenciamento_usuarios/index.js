d8dc9b2960d00fd285dd8a43d79ebf6271f838e6
William Goulart Pacheco <gtaskwill@gmail.com>
2021-03-03 19:09:30 -0300

    #

--- nodejs/005_curso_completo_com_proj_reais/003_gerenciamento_usuarios/index.js
+++ nodejs/005_curso_completo_com_proj_reais/003_gerenciamento_usuarios/index.js
@@ -11,6 +11,26 @@ exampleInputName.value = 'William'
 
 let user = {}
 
+function addLine(dataUser){
+    console.log(dataUser)
+
+    let tr = document.createElement('tr')
+    tr.innerHTML = `
+            <tr>
+                <td><img src="dist/img/user1-128x128.jpg" alt="User Image" class="img-circle img-sm"></td>
+                <td>${dataUser.name}</td>
+                <td>${dataUser.email}</td>
+                <td>${dataUser.admin}</td>
+                <td>${dataUser.birth}</td>
+                <td>
+                <button type="button" class="btn btn-primary btn-xs btn-flat">Editar</button>
+                <button type="button" class="btn btn-danger btn-xs btn-flat">Excluir</button>
+                </td>
+            </tr>
+    `
+    document.getElementById("table-users").appendChild(tr)
+
+}
 
 let fields = document.querySelectorAll("#form-user-create [name]")
 
@@ -39,5 +59,6 @@ document.getElementById("form-user-create").addEventListener("submit",(event)=>{
 
     })
 
-    console.log(user)
+    addLine(user)
+
 })
