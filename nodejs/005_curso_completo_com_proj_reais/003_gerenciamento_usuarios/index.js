f43f613a70389de87e128472777aed39cbc346d7
William Goulart Pacheco <gtaskwill@gmail.com>
2021-03-03 17:23:20 -0300

    #

---
+++ nodejs/005_curso_completo_com_proj_reais/003_gerenciamento_usuarios/index.js
@@ -0,0 +1,11 @@
+let name = document.querySelector('#exampleInputName')
+let gender = document.querySelectorAll('#form-user-create [name=gender]:checked')
+
+let exampleInputBirth = document.querySelector('#exampleInputBirth')
+let exampleInputCountry = document.querySelector('#exampleInputCountry')
+let exampleInputEmail1 = document.querySelector('#exampleInputEmail1')
+let exampleInputPassword1 = document.querySelector('#exampleInputPassword1')
+let exampleInputFile = document.querySelector('#exampleInputFile')
+let exampleInputNameEl = document.querySelector('#exampleInputName')
+
+exampleInputName.value = 'William'
