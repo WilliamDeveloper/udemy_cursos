a21ee7f7701103534b45bc3f3dfdc63659ddab00
William Goulart Pacheco <gtaskwill@gmail.com>
2021-03-03 22:00:59 -0300

    #'

--- nodejs/005_curso_completo_com_proj_reais/003_gerenciamento_usuarios/controllers/UserController.js
+++ nodejs/005_curso_completo_com_proj_reais/003_gerenciamento_usuarios/controllers/UserController.js
@@ -87,7 +87,7 @@ class UserController {
 
         this.tableEl.innerHTML = `
             <tr>
-                <td><img src="dist/img/user1-128x128.jpg" alt="User Image" class="img-circle img-sm"></td>
+                <td><img src="${dataUser.photo}" alt="User Image" class="img-circle img-sm"></td>
                 <td>${dataUser.name}</td>
                 <td>${dataUser.email}</td>
                 <td>${dataUser.admin}</td>
