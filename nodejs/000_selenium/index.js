ec7bb124b697cd292e66b3c9ece19004a68712d4
William Goulart Pacheco <gtaskwill@gmail.com>
2021-03-05 10:23:03 -0300

    #

--- nodejs/000_selenium/index.js
+++ nodejs/000_selenium/index.js
@@ -1,3 +1,13 @@
+const chromeNavigator = "C:/Program Files (x86)/Google/Chrome/Application/chrome.exe"
+const chromeDriver = "D:/_dev_/bin/webdriver/chrome/chromedriver.exe"
+
+const firefoNavigator="D:/_dev_/_ide_/browsers/firefox-36.0/FirefoxPortable.exe"
+const firefoxDriver = "D:/_dev_/bin/webdriver/firefox/geckodriver.exe"
+
+
+process.env['PATH'] =`${firefoNavigator};${process.env['PATH']}`
+process.env['PATH'] =`${firefoxDriver};${process.env['PATH']}`
+
 const webdriver = require('selenium-webdriver')
 const By = webdriver.By
 
