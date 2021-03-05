149b7f46a4d302392e690af8892959bba27320e3
William Goulart Pacheco <gtaskwill@gmail.com>
2021-03-05 10:13:28 -0300

    #

--- nodejs/000_selenium/index.js
+++ nodejs/000_selenium/index.js
@@ -5,3 +5,8 @@ const driver = new webdriver.Builder().forBrowser('firefox').build()
 const url ='www.google.com.br'
 driver.manage().window().maximize()
 driver.get(url)
+
+let email = 'a1@gmail.com'
+driver.findElement(By.name('name')).sendKeys('hack')
+driver.findElement(By.name('email')).sendKeys(email)
+driver.findElement(By.tagName('button')).click()
