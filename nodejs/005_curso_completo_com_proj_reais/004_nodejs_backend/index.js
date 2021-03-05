f8089c59b475eac91bddce5e60c9f01a3ff84842
William Goulart Pacheco <gtaskwill@gmail.com>
2021-03-05 15:50:52 -0300

    #

--- nodejs/005_curso_completo_com_proj_reais/004_nodejs_backend/index.js
+++ nodejs/005_curso_completo_com_proj_reais/004_nodejs_backend/index.js
@@ -3,7 +3,18 @@ const http = require('http')
 let server = http.createServer((req,res)=>{
     console.log('URL: ', req.url)
     console.log('METHOD: ', req.method)
-    res.end('OK')
+
+    switch (req.url) {
+        case '/':
+
+            res.statusCode = 200
+            res.setHeader("Content-Type", 'text/html')
+            res.end('<h1> ola</h1>')
+
+            break;
+
+    }
+
 })
 
 server.listen(3000,'127.0.0.1',()=>{
