906f3490565eb36a3f5fb58f58550913a1545c91
William Goulart Pacheco <gtaskwill@gmail.com>
2021-03-06 18:01:12 -0300

    #

---
+++ nodejs/005_curso_completo_com_proj_reais/006_node_generator/backend/index.js
@@ -0,0 +1,32 @@
+const express = require('express')
+const app = express()
+
+//bodyparser -> que agora ta dentro do express
+app.use(express.urlencoded({extended:false}))
+app.use(express.json());
+
+//versao 5.3.1
+const  expressValidator  = require('express-validator');
+app.use(expressValidator());
+
+const consign = require('consign')
+
+consign()
+    .include('routes')
+    .include('utils')
+    .into(app)
+
+/*
+const indexRouter = require("./routes/index")
+const usersRouter = require("./routes/users")
+
+app.use('/',indexRouter)
+app.use('/users',usersRouter)
+*/
+
+
+
+
+app.listen(3000,'127.0.0.1',()=>{
+    console.log('servidor rodando')
+})
---
+++ nodejs/005_curso_completo_com_proj_reais/006_node_generator/backend/routes/users.js
@@ -0,0 +1,115 @@
+const NeDB = require('nedb')
+const db = new NeDB({
+    filename:'users.db',
+    autoload:true
+})
+
+//habilita validacao expresss
+const {check, expressValidator, body, validationResult} = require("express-validator")
+
+module.exports = (app)=>{
+
+    let route = app.route('/users')
+
+    route.get((req,res)=>{
+
+        //sort 1 crescente sort -1 descrescente
+        db.find({}).sort({name:1}).exec( (error,users)=>{
+            if(error){
+                //auto importado pela config do consign
+                app.utils.error.send(error, req,res)
+            }else{
+                res.status(200).json({users})
+            }
+        })
+
+    })
+
+
+    route.post((req,res)=>{
+
+        console.log(req.body)
+
+        if(!app.utils.validator.user(app,req,res)) return false
+
+        db.insert(req.body,(error, user)=>{
+            if(error){
+                //auto importado pela config do consign
+                app.utils.error.send(error, req,res)
+            }else{
+                res.status(200).json(user)
+            }
+
+        })
+
+    })
+
+    app.get('/users/:id',(req,res)=>{
+
+        db.findOne({_id: req.params.id}).exec( (error,user)=>{
+            if(error){
+                //auto importado pela config do consign
+                app.utils.error.send(error, req,res)
+            }else{
+                res.status(200).json({user})
+            }
+        })
+
+    })
+
+    app.put('/users/:id',(req,res)=>{
+
+        if(!app.utils.validator.user(app,req,res)) return false
+
+        //filter/dadosToUpdate/calbackerror
+        db.update({_id: req.params.id}, req.body, (error)=>{
+            if(error){
+                //auto importado pela config do consign
+                app.utils.error.send(error, req,res)
+            }else{
+                //fazendo merge dos objetos
+                let resposta = Object.assign(req.params, req.body)
+                res.status(200).json(resposta)
+                // res.status(200).json({body: req.body, params: req.params})
+            }
+        })
+
+    })
+
+    app.delete('/users/:id',(req,res)=>{
+
+        //filter/dadosToUpdate/calbackerror
+        db.remove({_id: req.params.id}, {}, (error)=>{
+            if(error){
+                //auto importado pela config do consign
+                app.utils.error.send(error, req,res)
+            }else{
+
+                res.status(200).json({id:req.params.id})
+                // res.status(200).json({body: req.body, params: req.params})
+            }
+        })
+
+    })
+
+
+    app.get("/users/json",(req,res)=>{
+        res.statusCode = 200
+        res.setHeader("Content-Type", 'application/json')
+        let user = {
+            nome:'william',
+            idade:18,
+            admin:true
+        }
+        // res.end(JSON.stringify(user))
+        res.json(user)
+    })
+
+    app.get("/users/admin",(req,res)=>{
+        res.statusCode = 200
+        res.setHeader("Content-Type", 'application/json')
+
+        res.json({msg:'user/admin'})
+    })
+
+}
