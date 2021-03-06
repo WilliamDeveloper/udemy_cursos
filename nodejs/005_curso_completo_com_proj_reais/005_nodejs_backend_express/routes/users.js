cc9c3ee194ff2c80375834ae564a8bfc50695642
William Goulart Pacheco <gtaskwill@gmail.com>
2021-03-05 22:01:02 -0300

    #

--- nodejs/005_curso_completo_com_proj_reais/005_nodejs_backend_express/routes/users.js
+++ nodejs/005_curso_completo_com_proj_reais/005_nodejs_backend_express/routes/users.js
@@ -1,5 +1,31 @@
 module.exports = (app)=>{
 
+    app.get("/users",(req,res)=>{
+        res.statusCode = 200
+        res.setHeader("Content-Type", 'application/json')
+        let user = {
+            nome:'william',
+            idade:18,
+            admin:true
+        }
+        // res.end(JSON.stringify(user))
+        res.json(user)
+    })
+
+
+    app.post("/users",(req,res)=>{
+        res.statusCode = 200
+        res.setHeader("Content-Type", 'application/json')
+        let user = {
+            nome:'william',
+            idade:18,
+            admin:true
+        }
+        // res.end(JSON.stringify(user))
+        res.json(user)
+    })
+
+
     app.get("/users/json",(req,res)=>{
         res.statusCode = 200
         res.setHeader("Content-Type", 'application/json')
