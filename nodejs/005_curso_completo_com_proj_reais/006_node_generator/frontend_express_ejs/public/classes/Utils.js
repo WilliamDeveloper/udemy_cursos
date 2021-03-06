db4041ec0f74b794ee9939bf293561db34de5a50
William Goulart Pacheco <gtaskwill@gmail.com>
2021-03-06 19:02:37 -0300

    #

---
+++ nodejs/005_curso_completo_com_proj_reais/006_node_generator/frontend_express_ejs/public/classes/Utils.js
@@ -0,0 +1,12 @@
+class Utils{
+
+    static dateFormate(date){
+        let value = ''
+            + date.getDate() + "/" + (date.getMonth()+1) + "/" + date.getFullYear()
+            + ' '
+            + date.getHours() + ":" + (date.getMinutes()+1) + ":" + date.getSeconds()
+        // console.log(value)
+        return value
+    }
+
+}
---
+++ nodejs/005_curso_completo_com_proj_reais/006_node_generator/frontend_express_ejs/public/index.js
@@ -0,0 +1,11 @@
+// let name = document.querySelector('#exampleInputName')
+// let gender = document.querySelectorAll('#form-user-create [name=gender]:checked')
+// let birth = document.querySelector('#exampleInputBirth')
+// let country = document.querySelector('#exampleInputCountry')
+// let email = document.querySelector('#exampleInputEmail1')
+// let password = document.querySelector('#exampleInputPassword1')
+// let photo = document.querySelector('#exampleInputFile')
+// let admin = document.querySelector('#exampleInputAdmin')
+
+let userController = new UserController("form-user-create", "form-user-update","table-users")
+
