65b4595969cb36e946917d27cad4e2e4e05dc041
William Goulart Pacheco <gtaskwill@gmail.com>
2021-03-05 23:27:23 -0300

    #

--- nodejs/005_curso_completo_com_proj_reais/005_nodejs_backend_express/routes/users.js
+++ nodejs/005_curso_completo_com_proj_reais/005_nodejs_backend_express/routes/users.js
@@ -58,6 +58,7 @@ module.exports = (app)=>{
                 //auto importado pela config do consign
                 app.utils.error.send(error, req,res)
             }else{
+                //fazendo merge dos objetos
                 let resposta = Object.assign(req.params, req.body)
                 res.status(200).json(resposta)
                 // res.status(200).json({body: req.body, params: req.params})
