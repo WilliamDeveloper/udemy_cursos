af255c55fe113f67ee5ad9efd9a9a1c9b66e003d
William Goulart Pacheco <gtaskwill@gmail.com>
2021-03-05 22:28:18 -0300

    #

--- nodejs/005_curso_completo_com_proj_reais/005_nodejs_backend_express/routes/users.js
+++ nodejs/005_curso_completo_com_proj_reais/005_nodejs_backend_express/routes/users.js
@@ -1,3 +1,9 @@
+const NeDB = require('nedb')
+const db = new NeDB({
+    filename:'users.db',
+    autoload:true
+})
+
 module.exports = (app)=>{
 
     app.get("/users",(req,res)=>{
@@ -14,15 +20,19 @@ module.exports = (app)=>{
 
 
     app.post("/users",(req,res)=>{
-        res.statusCode = 200
-        res.setHeader("Content-Type", 'application/json')
-        let user = {
-            nome:'william',
-            idade:18,
-            admin:true
-        }
-        // res.end(JSON.stringify(user))
-        res.json(user)
+
+        let request = req.body
+
+        db.insert(request,(error, user)=>{
+            if(error){
+                console.log(error)
+                res.status(400).json({error:error})
+            }else{
+                res.status(200).json({user})
+            }
+
+        })
+
     })
 
 
