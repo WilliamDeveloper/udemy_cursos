6f351880d83d2051b9e813a5fb0bd40ffa10c626
William Goulart Pacheco <gtaskwill@gmail.com>
2021-03-05 21:43:14 -0300

    #

--- nodejs/005_curso_completo_com_proj_reais/005_nodejs_backend_express/index.js
+++ nodejs/005_curso_completo_com_proj_reais/005_nodejs_backend_express/index.js
@@ -4,7 +4,7 @@ const app = express()
 const indexRouter = require("./routes/index")
 const usersRouter = require("./routes/users")
 
-app.use('',indexRouter)
+app.use('/',indexRouter)
 app.use('/users',usersRouter)
 
 
