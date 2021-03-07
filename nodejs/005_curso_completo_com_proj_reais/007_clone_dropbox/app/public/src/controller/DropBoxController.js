9bc870a031b700bfcb9a69339f022b4fe11b14e8
William Goulart Pacheco <gtaskwill@gmail.com>
2021-03-07 14:23:58 -0300

    #

--- nodejs/005_curso_completo_com_proj_reais/007_clone_dropbox/app/public/src/controller/DropBoxController.js
+++ nodejs/005_curso_completo_com_proj_reais/007_clone_dropbox/app/public/src/controller/DropBoxController.js
@@ -4,6 +4,7 @@ class DropBoxController{
         this.btnSendFileEl = document.querySelector('#btn-send-file')
         this.inputFilesEl = document.querySelector('#files')
         this.snackModalEl = document.querySelector('#react-snackbar-root')
+        this.progressBarEl = this.snackModalEl.querySelector('.mc-progress-bar-fg')
 
         this.initEvents()
     }
@@ -67,5 +68,8 @@ class DropBoxController{
         let total = event.total
 
         let porcent = parseInt((loaded/total) * 100)
+
+        this.progressBarEl.style.width = porcent+'px'
+
     }
 }
