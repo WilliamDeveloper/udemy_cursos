9640be7892ee487506a606eb9819333b055acbe3
William Goulart Pacheco <gtaskwill@gmail.com>
2021-03-07 11:57:37 -0300

    #

--- nodejs/005_curso_completo_com_proj_reais/006_node_generator/frontend_express_ejs/app.js
+++ nodejs/005_curso_completo_com_proj_reais/006_node_generator/frontend_express_ejs/app.js
@@ -14,7 +14,9 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 app.use(logger('dev'));
-app.use(express.json());
+
+//limite de dados de um requisicao post/json
+app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
