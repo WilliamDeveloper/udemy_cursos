7f80207c2c89375d2301ada64ddf874c7f51a461
William Goulart Pacheco <gtaskwill@gmail.com>
2021-03-06 23:43:48 -0300

    #'

--- nodejs/005_curso_completo_com_proj_reais/006_node_generator/frontend_express_ejs/routes/users.js
+++ nodejs/005_curso_completo_com_proj_reais/006_node_generator/frontend_express_ejs/routes/users.js
@@ -59,6 +59,7 @@ router.delete('/:id', function(req, res, next) {
 
 
 router.post('/', function(req, res, next) {
+  console.log(req.body)
 
   client.post(`/users`,  req.body,function (err, request, response, obj) {
     assert.ifError(err)
