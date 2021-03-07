693b94efc40c28d8170d11e30821d26e5e2506d2
William Goulart Pacheco <gtaskwill@gmail.com>
2021-03-07 13:59:06 -0300

    #

--- nodejs/005_curso_completo_com_proj_reais/007_clone_dropbox/app/routes/users.js
+++ nodejs/005_curso_completo_com_proj_reais/007_clone_dropbox/app/routes/users.js
@@ -6,4 +6,8 @@ router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
+router.post('/upload', function(req, res, next) {
+  res.json(req.body);
+});
+
 module.exports = router;
