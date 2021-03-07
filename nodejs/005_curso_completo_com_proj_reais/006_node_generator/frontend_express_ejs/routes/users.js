9c5ba70f53c68ae552ded5ecdbc2b552464859d9
William Goulart Pacheco <gtaskwill@gmail.com>
2021-03-06 22:17:05 -0300

    #

--- nodejs/005_curso_completo_com_proj_reais/006_node_generator/frontend_express_ejs/routes/users.js
+++ nodejs/005_curso_completo_com_proj_reais/006_node_generator/frontend_express_ejs/routes/users.js
@@ -16,7 +16,55 @@ router.get('/', function(req, res, next) {
     assert.ifError(err)
     console.log(JSON.stringify(obj,null,2))
 
-    res.end(JSON.stringify(obj,null,2))
+    res.json(obj)
+  })
+
+});
+
+
+router.get('/:id', function(req, res, next) {
+
+  client.get(`/users/${req.params.id}`, function (err, request, response, obj) {
+    assert.ifError(err)
+    console.log(JSON.stringify(obj,null,2))
+
+    res.json(obj)
+  })
+
+});
+
+router.put('/:id', function(req, res, next) {
+
+  client.put(`/users/${req.params.id}`, req.body,function (err, request, response, obj) {
+    assert.ifError(err)
+    console.log(JSON.stringify(obj,null,2))
+
+    res.json(obj)
+  })
+
+});
+
+
+
+router.delete('/:id', function(req, res, next) {
+
+  client.del(`/users/${req.params.id}`, function (err, request, response, obj) {
+    assert.ifError(err)
+    console.log(JSON.stringify(obj,null,2))
+
+    res.json(obj)
+  })
+
+});
+
+
+router.post('/:id', function(req, res, next) {
+
+  client.post(`/users`,  req.body,function (err, request, response, obj) {
+    assert.ifError(err)
+    console.log(JSON.stringify(obj,null,2))
+
+    res.json(obj)
   })
 
 });
