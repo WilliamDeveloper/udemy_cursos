df7f008738c7fabbf05d3a493d84a57e7900b99b
William Goulart Pacheco <gtaskwill@gmail.com>
2021-03-08 00:00:46 -0300

    #

--- nodejs/005_curso_completo_com_proj_reais/007_clone_dropbox/app/public/src/controller/DropBoxController.js
+++ nodejs/005_curso_completo_com_proj_reais/007_clone_dropbox/app/public/src/controller/DropBoxController.js
@@ -38,6 +38,15 @@ class DropBoxController{
     initEvents(){
 
         this.btnRename.addEventListener('click', e=>{
+            let li = this.getSelection()[0]
+            let file = JSON.parse(li.dataset.file)
+
+            let name = prompt("Ronomeando o arquivo",file.name)
+
+            if(name){
+              file.name = name
+              this.getFirebaseRef().child(li.dataset.key).set(file)
+            }
 
         })
 
@@ -370,6 +379,7 @@ class DropBoxController{
 
         let li = document.createElement('li')
         li.dataset.key = key
+        li.dataset.file = JSON.stringify(file)
 
         li.innerHTML =  `
                 ${this.getFileIconView(file)}
