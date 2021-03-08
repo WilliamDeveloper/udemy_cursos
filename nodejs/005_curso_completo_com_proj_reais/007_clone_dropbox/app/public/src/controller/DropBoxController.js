c05cf141d45598033e3a18e01ba1806ac7b5ae15
William Goulart Pacheco <gtaskwill@gmail.com>
2021-03-07 22:30:53 -0300

    #

--- nodejs/005_curso_completo_com_proj_reais/007_clone_dropbox/app/public/src/controller/DropBoxController.js
+++ nodejs/005_curso_completo_com_proj_reais/007_clone_dropbox/app/public/src/controller/DropBoxController.js
@@ -7,6 +7,7 @@ class DropBoxController{
         this.progressBarEl = this.snackModalEl.querySelector('.mc-progress-bar-fg')
         this.nameFileEl = this.snackModalEl.querySelector('.filename')
         this.timeleftEl = this.snackModalEl.querySelector('.timeleft')
+        this.listFilesEl = document.querySelector('#list-of-files-and-directories')
 
         this.connectFireBase()
         this.initEvents()
@@ -327,22 +328,31 @@ class DropBoxController{
         }
     }
 
-    getFileView(file){
-        return `
-            <li>
+    getFileView(file, key){
+
+        let li = document.createElement('li')
+        li.dataset.key = key
+
+        li.innerHTML =  `
                 ${this.getFileIconView(file)}
-                <div class="name text-center">${file.name}</div>
-            </li>
+                <div class="name text-center">${file.name}</div>        
         `
+        return li
     }
 
     readFiles(){
+
         this.getFirebaseRef().on('value', snapshot =>{
+
+            this.listFilesEl.innerHTML = ''
+
             snapshot.forEach( snapshotItem =>{
                 console.log(snapshotItem)
                 let key = snapshotItem.key
                 let data = snapshotItem.val()
                 console.log(key, data)
+
+                this.listFilesEl.appendChild(this.getFileView(data, key))
             })
         })
     }
