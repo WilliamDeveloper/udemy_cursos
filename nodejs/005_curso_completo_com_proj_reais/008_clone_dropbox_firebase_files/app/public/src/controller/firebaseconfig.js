443de1e626309784045594baadd3d145dd937409
William Goulart Pacheco <gtaskwill@gmail.com>
2021-03-09 13:12:07 -0300

    #

---
+++ nodejs/005_curso_completo_com_proj_reais/008_clone_dropbox_firebase_files/app/public/src/controller/firebaseconfig.js
@@ -0,0 +1,12 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBJW5pTxN-V7trW8prTR52ujuEh4tuX9dY",
+    authDomain: "dropbox-clone-c0e0b.firebaseapp.com",
+    databaseURL: "https://dropbox-clone-c0e0b-default-rtdb.firebaseio.com",
+    projectId: "dropbox-clone-c0e0b",
+    storageBucket: "dropbox-clone-c0e0b.appspot.com",
+    messagingSenderId: "319661520219",
+    appId: "1:319661520219:web:6350ab13610ea3eb0dd67e",
+    measurementId: "G-243FY3GW09"
+};
+
+window.firebaseConfig= firebaseConfig
