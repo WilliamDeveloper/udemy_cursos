943755eab147d80feab41cc58a708c0fc4ba8fa3
William Goulart Pacheco <gtaskwill@gmail.com>
2021-03-09 19:34:27 -0300

    #

--- nodejs/005_curso_completo_com_proj_reais/009_clone_whatsapp/base/src/controller/WhatsAppController.js
+++ nodejs/005_curso_completo_com_proj_reais/009_clone_whatsapp/base/src/controller/WhatsAppController.js
@@ -100,21 +100,11 @@ class WhatsAppController{
 
             console.log(this.el.inputPhoto.files.toArray())
 
-
             // EX_A - alternativa EX_B abstraida na classe prototype
             this.el.inputPhoto.files.toArray().forEach( file=>{
                 console.log('file ', file)
             })
 
-            // EX_B - funciona em muitos browser
-            Array.prototype.forEach.call(this.el.inputPhoto.files, (item) => {
-                console.log('file ', file)
-            })
-
-            // EX_C - nao funciona em todos browser
-            [...this.el.inputPhoto.files].forEach( (item) => {
-                console.log('file ', file)
-            })
 
         })
 
