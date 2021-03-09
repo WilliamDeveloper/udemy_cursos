12db743bc623725faa16cb3a736a3b47f62b5166
William Goulart Pacheco <gtaskwill@gmail.com>
2021-03-08 22:36:55 -0300

    #

--- nodejs/005_curso_completo_com_proj_reais/007_clone_dropbox/app/public/src/controller/DropBoxController.js
+++ nodejs/005_curso_completo_com_proj_reais/007_clone_dropbox/app/public/src/controller/DropBoxController.js
@@ -6,6 +6,8 @@ class DropBoxController{
 
         this.onselectionchange = new Event('selectionchange')
 
+        this.navEl = document.querySelector('#browse-location')
+
         this.btnSendFileEl = document.querySelector('#btn-send-file')
         this.inputFilesEl = document.querySelector('#files')
         this.snackModalEl = document.querySelector('#react-snackbar-root')
@@ -500,7 +502,57 @@ class DropBoxController{
 
         if(this.lastFolder) this.getFirebaseRef(this.lastFolder).off('value')
 
+        this.renderNav()
         this.readFiles()
+    }
+
+    renderNav(){
+
+        let nav = document.createElement('nav')
+        let path = []
+
+
+        for (let i =0; i< this.currentFolder.length; i++){
+            let folderName = this.currentFolder[i]
+            let span = document.createElement('span')
+
+            path.push(folderName)
+
+
+            if( (i+1) == this.currentFolder.length){
+                span.innerHTML=folderName
+            }else{
+                span.className = 'breadcrumb-segment__wrapper'
+                span.innerHTML= `
+                 <span class="ue-effect-container uee-BreadCrumbSegment-link-0">
+                    <a href="#" data-path="${path.join('/')}" class="breadcrumb-segment">${folderName}</a>
+                </span>
+                <svg width="24" height="24" viewBox="0 0 24 24" class="mc-icon-template-stateless" style="top: 4px; position: relative;">
+                    <title>arrow-right</title>
+                    <path d="M10.414 7.05l4.95 4.95-4.95 4.95L9 15.534 12.536 12 9 8.464z" fill="#637282" fill-rule="evenodd"></path>
+                </svg>
+                `
+            }
+
+            nav.appendChild(span)
+        }
+
+        this.navEl.innerHTML = nav.innerHTML
+
+        /*
+
+        <span class="breadcrumb-segment__wrapper">
+            <span class="ue-effect-container uee-BreadCrumbSegment-link-0">
+                <a href="https://www.dropbox.com/work" class="breadcrumb-segment">HCODE</a>
+            </span>
+            <svg width="24" height="24" viewBox="0 0 24 24" class="mc-icon-template-stateless" style="top: 4px; position: relative;">
+                <title>arrow-right</title>
+                <path d="M10.414 7.05l4.95 4.95-4.95 4.95L9 15.534 12.536 12 9 8.464z" fill="#637282" fill-rule="evenodd"></path>
+            </svg>
+        </span>
+
+
+        * */
     }
 
     initEventsLi(li){
