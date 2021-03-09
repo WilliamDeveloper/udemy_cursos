61cb3b67b56d0f951814c06bd7348080cc1e1b7d
William Goulart Pacheco <gtaskwill@gmail.com>
2021-03-09 13:50:42 -0300

    #

--- nodejs/005_curso_completo_com_proj_reais/009_clone_whatsapp/base/src/controller/WhatsAppController.js
+++ nodejs/005_curso_completo_com_proj_reais/009_clone_whatsapp/base/src/controller/WhatsAppController.js
@@ -1,6 +1,15 @@
 class WhatsAppController{
     constructor(){
         console.log('class WhatsAppController ok')
+
+        this.loadElements()
     }
 
+    loadElements(){
+        this.el = {}
+
+        document.querySelectorAll('[id]').forEach(element=>{
+            this.el[Format.getCamelCase(element.id)]
+        })
+    }
 }
