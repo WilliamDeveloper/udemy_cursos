e49f2c32d7dccf3c0e3a5da6401097bd9f8c2966
William Goulart Pacheco <gtaskwill@gmail.com>
2021-03-09 23:02:58 -0300

    #'

--- nodejs/005_curso_completo_com_proj_reais/009_clone_whatsapp/base/src/controller/WhatsAppController.js
+++ nodejs/005_curso_completo_com_proj_reais/009_clone_whatsapp/base/src/controller/WhatsAppController.js
@@ -216,7 +216,7 @@ class WhatsAppController{
                 // this.el.inputText.append(img)
                 let cursor = window.getSelection()
 
-                if(cursor.focusNode.id == 'input-text'){
+                if(!cursor.focusNode.id == 'input-text'){
 
                 }
 
