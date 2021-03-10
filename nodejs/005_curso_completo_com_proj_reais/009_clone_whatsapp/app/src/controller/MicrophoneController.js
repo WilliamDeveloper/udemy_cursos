182f8b10d7767ca1313c54ccb050824c1ab4d711
William Goulart Pacheco <gtaskwill@gmail.com>
2021-03-10 19:05:06 -0300

    #

--- nodejs/005_curso_completo_com_proj_reais/009_clone_whatsapp/app/src/controller/MicrophoneController.js
+++ nodejs/005_curso_completo_com_proj_reais/009_clone_whatsapp/app/src/controller/MicrophoneController.js
@@ -5,12 +5,16 @@ export class MicrophoneController extends ClassEvent{
     constructor(audioEl){
         super()
 
+        this._mimeType = 'audio/webm'
+        this._available = false
+
         this._audioEl = audioEl
 
         navigator.mediaDevices.getUserMedia({
             audio : true
         }).then( stream =>{
             console.log('stream ', stream)
+            this._available = true
             this._stream = stream
 
             let audio = new Audio()
@@ -21,7 +25,12 @@ export class MicrophoneController extends ClassEvent{
 
         }).catch( error =>{
             console.log(error)
+            this._available = false
         })
+    }
+
+    isAvailable(){
+        return this._available
     }
 
     stop(){
@@ -30,6 +39,34 @@ export class MicrophoneController extends ClassEvent{
         })
     }
 
+    startRecorder(){
+        if ( this.isAvailable()){
+            this._mediaRecorder = new MediaRecorder(this._stream, this._mimeType )
+            this._recordedChunks = []
+            this._mediaRecorder.addEventListener('dataavailable', e=>{
+                if(e.data.size > 0){
+                    this._recordedChunks.push(e.data)
+                }
+            })
 
+            this._mediaRecorder.addEventListener('stop', e=>{
+                let blob = new Blob(this._recordedChunks,{
+                    type:this._mimeType
+                })
+                let fileName = `rec-${Date.now()}.webm`
+                let file = new File([blob], fileName, {
+                    type: this._mimeType,
+                    lastModified: Date.now()
+                })
+
+            })
+        }
+    }
+
+    stoptRecorder(){
+        if ( this.isAvailable()){
+
+        }
+    }
 
 }
