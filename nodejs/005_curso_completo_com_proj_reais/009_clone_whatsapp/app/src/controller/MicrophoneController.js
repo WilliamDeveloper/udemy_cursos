4e77a08b6e4ca00ebb064d3bee3d30d70e3b529b
William Goulart Pacheco <gtaskwill@gmail.com>
2021-03-10 18:08:41 -0300

    #

--- nodejs/005_curso_completo_com_proj_reais/009_clone_whatsapp/app/src/controller/MicrophoneController.js
+++ nodejs/005_curso_completo_com_proj_reais/009_clone_whatsapp/app/src/controller/MicrophoneController.js
@@ -1,6 +1,10 @@
-export class MicrophoneController {
+import {ClassEvent} from "../util/ClassEvent";
+
+export class MicrophoneController extends ClassEvent{
 
     constructor(videoEl){
+        super()
+
         this._videoEl = videoEl
 
         navigator.mediaDevices.getUserMedia({
