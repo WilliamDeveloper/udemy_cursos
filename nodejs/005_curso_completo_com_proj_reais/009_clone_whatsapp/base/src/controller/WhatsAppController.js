b442f56ca1cfb8308e436158e4e1ebbdc06c5aaa
William Goulart Pacheco <gtaskwill@gmail.com>
2021-03-09 23:26:24 -0300

    #'

--- nodejs/005_curso_completo_com_proj_reais/009_clone_whatsapp/base/src/controller/WhatsAppController.js
+++ nodejs/005_curso_completo_com_proj_reais/009_clone_whatsapp/base/src/controller/WhatsAppController.js
@@ -218,7 +218,18 @@ class WhatsAppController{
 
                 if(!cursor.focusNode || !cursor.focusNode.id == 'input-text'){
                     this.el.inputText.focus()
+                    cursor = window.getSelection()
                 }
+
+                let range = document.createRange()
+                range = cursor.getRangeAt(0)
+                range.deleteContents()
+
+                let frag = document.createDocumentFragment()
+                frag.appendChild(img)
+
+                range.insertNode(frag)
+                range.setStartAfter(img)
 
                 // provocar um evento real
                 this.el.inputText.dispatchEvent(new Event('keyup'))
