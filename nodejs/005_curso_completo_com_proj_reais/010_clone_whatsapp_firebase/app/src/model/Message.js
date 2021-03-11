f8393d26e24e8f55b695e9c9ae60f64c45e04944
William Goulart Pacheco <gtaskwill@gmail.com>
2021-03-11 14:46:48 -0300

    #

--- nodejs/005_curso_completo_com_proj_reais/010_clone_whatsapp_firebase/app/src/model/Message.js
+++ nodejs/005_curso_completo_com_proj_reais/010_clone_whatsapp_firebase/app/src/model/Message.js
@@ -25,7 +25,7 @@ export class Message extends Model{
 
             case 'contact':
                 div.innerHTML = `
-                        <div class="_3_7SH kNKwo message-in tail">
+                        <div class="_3_7SH kNKwo tail">
                             <span class="tail-container"></span>
                             <span class="tail-container highlight"></span>
                             <div class="_1YNgi copyable-text">
@@ -72,7 +72,7 @@ export class Message extends Model{
 
             case 'image':
                 div.innerHTML = `
-                        <div class="_3_7SH _3qMSo message-out">
+                        <div class="_3_7SH _3qMSo">
                             <div class="KYpDv">
                                 <div>
                                     <div class="_3v3PK" style="width: 330px; height: 330px;">
@@ -128,7 +128,7 @@ export class Message extends Model{
 
             case 'document':
                 div.innerHTML = `
-                    <div class="_3_7SH _1ZPgd message-out">
+                    <div class="_3_7SH _1ZPgd">
                         <div class="_1fnMt _2CORf">
                             <a class="_1vKRe" href="#">
                                 <div class="_2jTyA" style="background-image: url()"></div>
@@ -180,7 +180,7 @@ export class Message extends Model{
                 div.innerHTML = `
                         <div class="message">
                             
-                            <div class="_3_7SH _17oKL message-out">
+                            <div class="_3_7SH _17oKL">
                                 <div class="_2N_Df LKbsn">
                                     <div class="_2jfIu">
                                         <div class="_2cfqh">
@@ -268,7 +268,7 @@ export class Message extends Model{
 
             default:
                 div.innerHTML = `
-                    <div class="font-style _3DFk6 message-in tail">
+                    <div class="font-style _3DFk6 tail">
                         <span class="tail-container"></span>
                         <span class="tail-container highlight"></span>
                         <div class="Tkt2p">
