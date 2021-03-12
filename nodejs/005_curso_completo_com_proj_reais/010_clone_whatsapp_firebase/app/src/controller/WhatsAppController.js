28222e864d1ffad7e11f0a917790945a1de2d517
William Goulart Pacheco <gtaskwill@gmail.com>
2021-03-12 12:58:39 -0300

    #

--- nodejs/005_curso_completo_com_proj_reais/010_clone_whatsapp_firebase/app/src/controller/WhatsAppController.js
+++ nodejs/005_curso_completo_com_proj_reais/010_clone_whatsapp_firebase/app/src/controller/WhatsAppController.js
@@ -443,13 +443,31 @@ export default class WhatsAppController{
             let ext = mimeType.split('/')[1]
             let filename = `camera${Date.now()}.${ext}`
 
-            fetch(this.el.pictureCamera.src)
-                .then( res=>{ return res.arrayBuffer() })
-                .then(buffer=>{ return new File([buffer], filename, {type:mimeType})})
-                .then(file=>{
-                    Message.sendImage(this._contactAtive.chatId, this._user.email, file)
-                    this.el.btnSendPicture.disabled = true
-                })
+            let picture = new Image()
+            picture.src = this.el.pictureCamera.src
+            picture.onload = e =>{
+                let canvas = document.createElement('canvas')
+                let context = canvas.getContext('2d')
+
+                canvas.width = picture.width
+                canvas.height = picture.height
+
+                context.translate(picture.width, 0)
+                context.scale(-1,1)
+
+                context.drawImage(picture,0,0, canvas.width, canvas.height)
+
+                // fetch(this.el.pictureCamera.src)
+                fetch(canvas.toDataURL(mimeType))
+                    .then( res=>{ return res.arrayBuffer() })
+                    .then(buffer=>{ return new File([buffer], filename, {type:mimeType})})
+                    .then(file=>{
+                        Message.sendImage(this._contactAtive.chatId, this._user.email, file)
+                        this.el.btnSendPicture.disabled = true
+                    })
+            }
+
+
         })
 
 
