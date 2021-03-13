c937a5fb6ba29619074e63178a1fc469ecc527de
William Goulart Pacheco <gtaskwill@gmail.com>
2021-03-13 15:50:31 -0300

    banco_lixo

--- nodejs/005_curso_completo_com_proj_reais/012_projeto_restaurante/saboroso/routes/index.js
+++ nodejs/005_curso_completo_com_proj_reais/012_projeto_restaurante/saboroso/routes/index.js
@@ -1,9 +1,27 @@
 var express = require('express');
 var router = express.Router();
+var conn = require('./../inc/db-mysql')
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Restaurante Saborosoo!' });
+
+
+  conn.query(
+      `
+    select * from tb_menus order by title
+  `,
+  (error, results)=>{
+    if(error){
+      res.json(error)
+    }else{
+
+    }
+  })
+
+  res.render('index', {
+    title: 'Restaurante Saborosoo!',
+    menus: results
+  });
 });
 
 module.exports = router;
