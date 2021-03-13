c4adc4b25bad8e44de2b8386b6309567bb45b7b8
William Goulart Pacheco <gtaskwill@gmail.com>
2021-03-13 00:21:58 -0300

    #

--- nodejs/005_curso_completo_com_proj_reais/011_clone_whatsapp_firebase/app/config/firebaseconfig.js
+++ nodejs/005_curso_completo_com_proj_reais/011_clone_whatsapp_firebase/app/config/firebaseconfig.js
@@ -1,5 +1,11 @@
 var firebaseConfig = {
-
+    apiKey: "AIzaSyBs5hP_0h_oOzk2JXuMBhP1ivgxSFHZZ0w",
+    authDomain: "whatsapp-clone-8ad21.firebaseapp.com",
+    projectId: "whatsapp-clone-8ad21",
+    storageBucket: "whatsapp-clone-8ad21.appspot.com",
+    messagingSenderId: "473500921602",
+    appId: "1:473500921602:web:a6e97a4a4e5c225a161c1b",
+    measurementId: "G-6Q4Q25DJ4S"
 };
 
 // window.firebaseConfig= firebaseConfig
