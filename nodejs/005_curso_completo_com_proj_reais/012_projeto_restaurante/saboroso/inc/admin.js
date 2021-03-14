89a19de8b466a84d654f55250a6b5db2ffea87b7
William Goulart Pacheco <gtaskwill@gmail.com>
2021-03-14 17:39:02 -0300

    banco_lixo

--- nodejs/005_curso_completo_com_proj_reais/012_projeto_restaurante/saboroso/inc/admin.js
+++ nodejs/005_curso_completo_com_proj_reais/012_projeto_restaurante/saboroso/inc/admin.js
@@ -17,7 +17,7 @@ module.exports ={
        let menus =  [
            {
                text : 'Tela Inicial',
-               href:"/admin",
+               href:"/admin/",
                icon:'home',
                active: false
            },
