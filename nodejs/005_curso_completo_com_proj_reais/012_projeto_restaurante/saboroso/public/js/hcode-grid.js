0e6606b18eaeedfc684ceeae68269a99fd121b93
William Goulart Pacheco <gtaskwill@gmail.com>
2021-03-15 19:49:24 -0300

    banco_lixo

---
+++ nodejs/005_curso_completo_com_proj_reais/012_projeto_restaurante/saboroso/public/js/hcode-grid.js
@@ -0,0 +1,115 @@
+class HCodeGrid{
+
+    constructor(configs){
+        console.log('hcode-grid')
+
+        let objectDefault = {
+            formCreate : '#modal-create form',
+            formUpdate : '#modal-update form',
+            btnUpdate : '.btn-update',
+            btnDelete : '.btn-delete',
+        }
+        // vai mergiar os dois objetos e manter as propriedades passadas por ultimo
+        this.options = Object.assign({}, objectDefault, configs)
+
+        this.initForms()
+        this.initButtons()
+    }
+
+    initForms(){
+        this.formCreate = document.querySelector(this.options.formCreate)
+
+        this.formCreate.save().then(json=>{
+            console.log('json ', json)
+            window.location.reload()
+        }).catch( error =>{
+            console.log('error ', error)
+        })
+
+        this.formUpdate = document.querySelector(this.options.formUpdate)
+
+        this.formUpdate.save().then(json=>{
+            console.log('json ', json)
+            window.location.reload()
+        }).catch( error =>{
+            console.log('error ', error)
+        })
+    }
+
+    initButtons(){
+
+
+        Array.prototype.forEach.call( document.querySelectorAll(this.options.btnUpdate), (btn) => {
+            console.log('btn')
+
+            btn.addEventListener('click', e=>{
+                let tr = e.path.find( el => {
+                    return (el.tagName.toUpperCase() === 'TR')
+                })
+
+                console.log('tr ',tr)
+
+                let data = JSON.parse(tr.dataset.row)
+                console.log('data', data)
+
+                for (let name in data){
+                    console.log('name ', name)
+                    let input = this.formUpdate.querySelector(`[name=${name}]`)
+                    switch (name) {
+                        // case 'register':
+                        //
+                        //   break
+                        case 'photo':
+                            this.formUpdate.querySelector('img').src= '/'+data[name]
+                            break
+                        default:
+                            if(input){
+                                input.value = data[name]
+                            }
+
+                    }
+
+                }
+
+                $('#modal-update').modal('open')
+
+            })
+
+
+        })
+        ///---------------
+        Array.prototype.forEach.call( document.querySelectorAll(this.options.btnDelete), (btn) => {
+            console.log('btn')
+
+            btn.addEventListener('click', e=>{
+
+
+
+                let tr = e.path.find( el => {
+                    return (el.tagName.toUpperCase() === 'TR')
+                })
+
+                console.log('tr ',tr)
+
+                let data = JSON.parse(tr.dataset.row)
+                console.log('data', data)
+
+                if(!confirm(eval('`'+this.options.deleteMsg+'`'))) return false
+
+                fetch(eval('`'+this.options.deleteUrl+'`'),{ method:'DELETE'} )
+                    .then(response => response.json())
+                    .then(json=>{
+                    console.log('json ', json)
+                    window.location.reload()
+                }).catch(error =>{
+                    console.log('error ', error)
+                })
+
+            })
+
+
+        })
+    }
+
+
+}
