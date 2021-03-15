4ef48dbaaeea58755a3478cff722c688d585463c
William Goulart Pacheco <gtaskwill@gmail.com>
2021-03-15 12:14:40 -0300

    banco_lixo

---
+++ nodejs/005_curso_completo_com_proj_reais/012_projeto_restaurante/saboroso/public/js/hcode-formsave.js
@@ -0,0 +1,4 @@
+// metodos de saber qual a quase tem determinado elemento do DOM
+//dir(document.createElement('form'))
+//console.dir(document.createElement('form'))
+//console.dir(document.querySelector('form'))
