58733f3db2fdd5a189a8c61f64db87bc57ffd151
William Goulart Pacheco <gtaskwill@gmail.com>
2021-03-15 13:03:11 -0300

    banco_lixo

--- nodejs/005_curso_completo_com_proj_reais/012_projeto_restaurante/saboroso/inc/menus.js
+++ nodejs/005_curso_completo_com_proj_reais/012_projeto_restaurante/saboroso/inc/menus.js
@@ -24,24 +24,49 @@ module.exports ={
 
             fields.photo = `images/${path.parse(files.photo.path).base}`
 
-            conn.query(
+
+            let query, params
+
+            if(parseInt(fields.id) < 0){
+                query = `
+                    update tb_menus
+                    set title = ?,
+                        description = ?,
+                        price = ?,
+                        photo = ?
+                    where id = ?
                 `
+                params = [
+                    fields.title,
+                    fields.description,
+                    fields.price,
+                    fields.photo,
+                    fields.id
+                ]
+            }else{
+                query = `
                     insert into tb_menus (title, description, price, photo)
                     values (?,?,?,?)
-                `,
-                [
+                `
+                params = [
                     fields.title,
                     fields.description,
                     fields.price,
                     fields.photo
-                ],
+                ]
+            }
+
+            conn.query(
+                query,
+                params,
                 (error, results)=>{
                     if(error){
                         reject(error)
                     }else{
                         resolve(results)
                     }
-                })
+                }
+            )
         })
     }
 }
