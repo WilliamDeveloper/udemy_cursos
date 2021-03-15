cf39c22be9717ea934ae03771e0f8b66d9ba98f3
William Goulart Pacheco <gtaskwill@gmail.com>
2021-03-15 18:51:56 -0300

    banco_lixo

--- nodejs/005_curso_completo_com_proj_reais/012_projeto_restaurante/saboroso/inc/reservations.js
+++ nodejs/005_curso_completo_com_proj_reais/012_projeto_restaurante/saboroso/inc/reservations.js
@@ -91,4 +91,30 @@ module.exports ={
         })
     },
 
+    delete(id){
+        return new Promise((resolve, reject)=>{
+            let query =`
+                delete 
+                from tb_reservations
+                where id = ?
+            `
+            let params = [
+                id
+            ]
+
+
+            conn.query(
+                query,
+                params,
+                (error, results)=>{
+                    if(error){
+                        reject(error)
+                    }else{
+                        resolve(results)
+                    }
+                }
+            )
+        })
+    },
+
 }
