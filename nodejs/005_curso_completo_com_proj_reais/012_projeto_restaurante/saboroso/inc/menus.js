da0eb1d18529cfdad76f6f9e35267291c4c30536
William Goulart Pacheco <gtaskwill@gmail.com>
2021-03-15 13:20:43 -0300

    banco_lixo

--- nodejs/005_curso_completo_com_proj_reais/012_projeto_restaurante/saboroso/inc/menus.js
+++ nodejs/005_curso_completo_com_proj_reais/012_projeto_restaurante/saboroso/inc/menus.js
@@ -25,36 +25,46 @@ module.exports ={
             fields.photo = `images/${path.parse(files.photo.path).base}`
 
 
-            let query, params
+            let query
+            let queryPhoto = ''
+            let params
+
+            params = [
+                fields.title,
+                fields.description,
+                fields.price,
+            ]
+
+            if(files.photo.name){
+                queryPhoto = ", photo = ?"
+                params.push(fields.photo)
+            }
 
             if(parseInt(fields.id) > 0){
+                params.push(fields.id)
+
                 query = `
                     update tb_menus
                     set title = ?,
                         description = ?,
-                        price = ?,
-                        photo = ?
+                        price = ?
+                        ${queryPhoto}
                     where id = ?
                 `
-                params = [
-                    fields.title,
-                    fields.description,
-                    fields.price,
-                    fields.photo,
-                    fields.id
-                ]
+
             }else{
+                if(!files.photo.name){
+                    reject('Envie a foto do prato')
+                }
+
                 query = `
                     insert into tb_menus (title, description, price, photo)
                     values (?,?,?,?)
                 `
-                params = [
-                    fields.title,
-                    fields.description,
-                    fields.price,
-                    fields.photo
-                ]
+
             }
+
+            console.log('query ', query, params)
 
             conn.query(
                 query,
