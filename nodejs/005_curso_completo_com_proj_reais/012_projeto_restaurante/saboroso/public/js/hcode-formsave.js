aeb26e89bdd6a4179ddb87599ea2abdf8e7750ee
William Goulart Pacheco <gtaskwill@gmail.com>
2021-03-16 20:28:08 -0300

    banco_lixo

--- nodejs/005_curso_completo_com_proj_reais/012_projeto_restaurante/saboroso/public/js/hcode-formsave.js
+++ nodejs/005_curso_completo_com_proj_reais/012_projeto_restaurante/saboroso/public/js/hcode-formsave.js
@@ -11,16 +11,19 @@ HTMLFormElement.prototype.save = function(config){
         e.preventDefault()
 
         let formData = new FormData(form)
+        console.log(form, formData)
         fetch(form.action,{
             method: form.method,
             body: formData
         }).then( response => response.json()).then(json=>{
+            console.log('then- ', json)
             if(json.error){
                 if(typeof config.failure === 'function') config.failure(json.error)
             }else{
                 if(typeof config.success === 'function') config.success(json)
             }
         }).catch((error)=>{
+            console.log('catch- ', error)
             if(typeof config.failure === 'function') config.failure(error)
         })
 
