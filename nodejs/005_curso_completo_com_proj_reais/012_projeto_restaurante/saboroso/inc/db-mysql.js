8eb6f4d048dea8fac407c6d21ed4669023386c6b
William Goulart Pacheco <gtaskwill@gmail.com>
2021-03-17 00:04:35 -0300

    #'

--- nodejs/005_curso_completo_com_proj_reais/012_projeto_restaurante/saboroso/inc/db-mysql.js
+++ nodejs/005_curso_completo_com_proj_reais/012_projeto_restaurante/saboroso/inc/db-mysql.js
@@ -4,7 +4,8 @@ const connection = mysql.createConnection({
     host: '192.168.8.14',
     user : 'root',
     password:'123456',
-    database:'saboroso'
+    database:'saboroso',
+    multipleStatements:true
 })
 
 module.exports = connection
