e0387c69207a1025f8186b9e6679b18c519b0f33
William Goulart Pacheco <gtaskwill@gmail.com>
2021-03-25 11:55:47 -0300

    banco_lixo

---
+++ nodejs/006_html5_css3/006_aula/assets/js/index.js
@@ -0,0 +1,17 @@
+let data =[
+    {
+        id:1,
+        title: 'Estudar HTML',
+    },
+    {
+        id:2,
+        title: 'Estudar CSS',
+    },
+    {
+        id:3,
+        title: 'Estudar JavaScript',
+    },
+]
+
+
+console.log(data)
