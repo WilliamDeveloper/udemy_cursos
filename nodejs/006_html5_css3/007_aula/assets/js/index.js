303b820b3d4aa759d9cc52bce6a3ff2b5cb685ef
William Goulart Pacheco <gtaskwill@gmail.com>
2021-03-25 15:52:21 -0300

    banco_lixo

--- nodejs/006_html5_css3/007_aula/assets/js/index.js
+++ nodejs/006_html5_css3/007_aula/assets/js/index.js
@@ -1,83 +1,9 @@
-let data =[
-    {
-        id:1,
-        title: 'Estudar HTML',
-    },
-    {
-        id:2,
-        title: 'Estudar CSS',
-    },
-    {
-        id:3,
-        title: 'Estudar JavaScript',
-    },
-    {
-        id:4,
-        title: 'PHP',
-    },
-]
-
-
-console.log(data)
-
-function renderTodo(){
-    let toduEl = document.querySelector('.todu')
-
-    toduEl.innerHTML =''
-
-    data.forEach(item=>{
-        let li = document.createElement('li')
-        li.innerHTML = `   
-        <input type="checkbox" id="task-${item.id}">
-        <label for="task-${item.id}">${item.title}</label>     
-        <button type="button">X</button>      
-    `
-        li.querySelector('input').addEventListener('change',e=>{
-            if(e.target.checked){
-                li.classList.add('complete')
-            }else{
-                li.classList.remove('complete')
-            }
-        })
-
-        li.querySelector('button').addEventListener('click', e=>{
-
-
-            let button = e.target
-            let li = button.parentNode
-            let input = li.querySelector('input')
-            let id = input.id
-            let idArray = id.split('-')
-
-            let todoId = idArray[1]
-            let title = li.querySelector('label').innerHTML
-
-            let msg= `posso deletar ? ${title}`
-            console.warn(msg)
-            if(confirm(msg)){
-                data = data.filter( item => item.id !== parseInt(todoId))
-
-                renderTodo()
-            }
-
-
-        })
-
-        document.querySelector('.todu').append(li)
-    })
-}
-
-renderTodo()
-
-document.querySelector('#new-task').addEventListener('keyup', e=>{
-    if(e.key === 'Enter'){
-        let valor = e.target.value
-        let obj = {
-            id : data.length+1,
-            title: valor
-        }
-        document.querySelector('#new-task').value = ''
-        data.push(obj)
-        renderTodo()
-    }
+console.log('carregado')
+document.querySelector('.open-menu').addEventListener('click', e=>{
+    console.log('blau')
+    document.querySelector('header .menu').classList.add('open')
+})
+document.querySelector('.close-menu button').addEventListener('click', e=>{
+    console.log('blau')
+    document.querySelector('header .menu').classList.remove('open')
 })
