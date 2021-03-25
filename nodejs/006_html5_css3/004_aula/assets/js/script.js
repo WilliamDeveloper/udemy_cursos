ab3e96ba2859c061e651b9392066c817fa912058
William Goulart Pacheco <gtaskwill@gmail.com>
2021-03-25 10:03:27 -0300

    banco_lixo

--- nodejs/006_html5_css3/004_aula/assets/js/script.js
+++ nodejs/006_html5_css3/004_aula/assets/js/script.js
@@ -1,1 +1,19 @@
-console.log('carregado')
+console.log('carregado2')
+
+let email = ''
+email = document.querySelector('[name=email]').value
+
+console.log('email: ', email)
+
+let btnSubmit = document.querySelector("#btn-submit")
+btnSubmit.addEventListener('click', e=>{
+    console.log('clicaram no btnSubmit')
+})
+
+let formLogin = document.querySelector("#form-login")
+formLogin.addEventListener('mouseenter', e=>{
+    console.log('mouse esta sobre o formLogin')
+})
+formLogin.addEventListener('mouseleave', e=>{
+    console.log('mouse esta fora do formLogin')
+})
