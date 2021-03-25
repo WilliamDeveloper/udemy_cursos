9dfbd1f9672ab88100693a12e3094813bb984b43
William Goulart Pacheco <gtaskwill@gmail.com>
2021-03-25 12:06:17 -0300

    banco_lixo

--- nodejs/006_html5_css3/006_aula/assets/js/index.js
+++ nodejs/006_html5_css3/006_aula/assets/js/index.js
@@ -11,7 +11,20 @@ let data =[
         id:3,
         title: 'Estudar JavaScript',
     },
+    {
+        id:4,
+        title: 'PHP',
+    },
 ]
 
 
 console.log(data)
+
+data.forEach(item=>{
+    let li = document.createElement('li')
+    li.innerHTML = `   
+        <input type="checkbox" id="task-${item.id}">
+        <label for="task-${item.id}">${item.title}</label>           
+    `
+    document.querySelector('.todu').append(li)
+})
