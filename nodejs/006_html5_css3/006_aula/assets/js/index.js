57a2f1cac87c5820f16305ed40f7f88afd77f24b
William Goulart Pacheco <gtaskwill@gmail.com>
2021-03-25 12:22:14 -0300

    banco_lixo

--- nodejs/006_html5_css3/006_aula/assets/js/index.js
+++ nodejs/006_html5_css3/006_aula/assets/js/index.js
@@ -20,19 +20,39 @@ let data =[
 
 console.log(data)
 
-data.forEach(item=>{
-    let li = document.createElement('li')
-    li.innerHTML = `   
+function renderTodo(){
+    let toduEl = document.querySelector('.todu')
+
+    toduEl.innerHTML =''
+
+    data.forEach(item=>{
+        let li = document.createElement('li')
+        li.innerHTML = `   
         <input type="checkbox" id="task-${item.id}">
         <label for="task-${item.id}">${item.title}</label>           
     `
-    li.querySelector('input').addEventListener('change',e=>{
-        if(e.target.checked){
-            li.classList.add('complete')
-        }else{
-            li.classList.remove('complete')
-        }
-    })
+        li.querySelector('input').addEventListener('change',e=>{
+            if(e.target.checked){
+                li.classList.add('complete')
+            }else{
+                li.classList.remove('complete')
+            }
+        })
 
-    document.querySelector('.todu').append(li)
+        document.querySelector('.todu').append(li)
+    })
+}
+
+renderTodo()
+
+document.querySelector('#new-task').addEventListener('keyup', e=>{
+    if(e.key === 'Enter'){
+        let valor = e.target.value
+        let obj = {
+            id : data.length+1,
+            title: valor
+        }
+        data.push(obj)
+        renderTodo()
+    }
 })
