55851ec97db64055f53d46f8dd138f5cfc1d7ee5
William Goulart Pacheco <gtaskwill@gmail.com>
2021-03-29 12:18:20 -0300

    banco_lixo

--- nodejs/008_node_example/v3/modularnode/src/routes/hello.js
+++ nodejs/008_node_example/v3/modularnode/src/routes/hello.js
@@ -1,5 +1,4 @@
 const router = require('express').Router()
-
 const HelloController = require('../controllers/hello')
 
 router.get('/', HelloController.helloWorld)
--- nodejs/008_node_example/v3/modularnode/src/routes/users.js
+++ nodejs/008_node_example/v3/modularnode/src/routes/users.js
@@ -1,9 +1,6 @@
-var express = require('express');
-var router = express.Router();
+const router = require('express').Router()
+const UsersController = require('../controllers/users')
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
+router.get('/', UsersController.index)
 
-module.exports = router;
+module.exports =  router
