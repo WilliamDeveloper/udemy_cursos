677ad38c1cdf279a6053c75c0512d6639ab45c6d
William Goulart Pacheco <gtaskwill@gmail.com>
2021-03-29 12:07:40 -0300

    banco_lixo

--- nodejs/008_node_example/v3/modularnode/src/routes/hello.js
+++ nodejs/008_node_example/v3/modularnode/src/routes/hello.js
@@ -1,10 +1,5 @@
 const router = require('express').Router()
 
-// const TestController = require('./../src/controllers/index')
-// colocar no arquivo raiz
-// global.__base = __dirname + '/';
-// const TestController = require(`${__base}`+'/src/controllers/index')
-// const TestController = require('./../controllers/index')
 const HelloController = require('../controllers/hello')
 
 router.get('/', HelloController.helloWorld)
