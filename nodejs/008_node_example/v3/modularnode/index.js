9670f9672514a0855b197a8d5664bbdb49b89cc1
William Goulart Pacheco <gtaskwill@gmail.com>
2021-03-29 12:24:07 -0300

    banco_lixo

--- nodejs/008_node_example/v3/modularnode/index.js
+++ nodejs/008_node_example/v3/modularnode/index.js
@@ -2,9 +2,11 @@ require('dotenv').config()
 
 var app = require('./src/app');
 
-let PORT = process.env.port || 3000
+let HOST_PROTOCOL = process.env.HOST_PROTOCOL || 'http'
+let HOST_IP = process.env.HOST_IP || 'localhost'
+let PORT = process.env.PORT || 3000
 global.__projectBasePathRoot = __dirname + '/';
 
 app.listen(PORT,()=>{
-    console.log(`rodando app http://localhost:${PORT}`)
+    console.log(`rodando app ${HOST_PROTOCOL}://${HOST_IP}:${PORT}`)
 })
