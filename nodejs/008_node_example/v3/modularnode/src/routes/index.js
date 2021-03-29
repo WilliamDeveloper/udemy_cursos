44f0c6a57af1bc999794dfc1cbe7db21ba6487c8
William Goulart Pacheco <gtaskwill@gmail.com>
2021-03-29 12:13:59 -0300

    banco_lixo

--- nodejs/008_node_example/v3/modularnode/src/routes/index.js
+++ nodejs/008_node_example/v3/modularnode/src/routes/index.js
@@ -1,9 +1,6 @@
-var express = require('express');
-var router = express.Router();
+const router = require('express').Router()
+const IndexController = require('../controllers/index')
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+router.get('/', IndexController.index)
 
-module.exports = router;
+module.exports =  router
