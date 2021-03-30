c1b2885289fcfa7d8091a605fccd94188faf4757
William Goulart Pacheco <gtaskwill@gmail.com>
2021-03-30 14:02:11 -0300

    banco_lixo

--- nodejs/008_node_example/v5/modularnode/src/routes/sql.js
+++ nodejs/008_node_example/v5/modularnode/src/routes/sql.js
@@ -2,6 +2,7 @@ const router = require('express').Router()
 const SqlController = require('../controllers/sql')
 
 router.get('/select', SqlController.select)
+router.post('/select', SqlController.select)
 router.get('/insert', SqlController.insert)
 router.get('/update', SqlController.update)
 router.get('/delete', SqlController.delete)
