9508f347230214ddb1bdbc7df8cbed04bd8feca7
William Goulart Pacheco <gtaskwill@gmail.com>
2021-03-30 18:41:55 -0300

    banco_lixo

--- nodejs/008_node_example/v6/modularnode/src/config/sql/dba/sql.js
+++ nodejs/008_node_example/v6/modularnode/src/config/sql/dba/sql.js
@@ -16,9 +16,28 @@ const sql = {
         return lista
     },
 
-    'T411PASI_FULL' : ` 
-        
+    'LISTAR_OBJETOS_INVALIDOS' : ` 
+        SELECT  owner,  object_type,  object_name,  status FROM   dba_objects  WHERE  status = 'INVALID'
     `,
+
+    'LISTAR_GRANT_OBJ' : "SELECT * FROM  DBA_TAB_PRIVS where table_name = UPPER('${p_tabela}') AND GRANTEE = UPPER('${p_owner_granted}')  ",
+
+    'LISTAR_DADOS_TABELA':"select col.column_id,\n" +
+        "            col.owner as schema_name,\n" +
+        "            col.table_name,\n" +
+        "            col.column_name,\n" +
+        "            col.data_type,\n" +
+        "            col.data_length,\n" +
+        "            col.data_precision,\n" +
+        "            col.data_scale,\n" +
+        "            col.nullable\n" +
+        "            from sys.all_tab_columns col\n" +
+        "            inner join sys.all_tables t\n" +
+        "            on col.owner = t.owner\n" +
+        "            and col.table_name = t.table_name\n" +
+        "            where lower(col.owner) = '${p_owner}'\n" +
+        "            and lower(col.table_name) = '${p_nome_tabela}'\n" +
+        "            order by col.column_id",
 
 
 
