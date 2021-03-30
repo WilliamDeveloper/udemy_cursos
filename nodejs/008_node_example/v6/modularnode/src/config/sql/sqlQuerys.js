c90090eda3a08ae739059d34608024a35117bdb9
William Goulart Pacheco <gtaskwill@gmail.com>
2021-03-30 18:34:47 -0300

    banco_lixo

--- nodejs/008_node_example/v6/modularnode/src/config/sql/sqlQuerys.js
+++ nodejs/008_node_example/v6/modularnode/src/config/sql/sqlQuerys.js
@@ -3,12 +3,18 @@ const insert_ = require('./insert/sql')
 const update_ = require('./update/sql')
 const delete_ = require('./delete/sql')
 
+const job_ = require('./job/sql')
+const dba_ = require('./dba/sql')
+
 
 const sqlQuerys ={
     select: select_,
     insert:insert_,
     update:update_,
-    delete: delete_
+    delete: delete_,
+    job:job_,
+    dba: dba_
+
 }
 
 module.exports = sqlQuerys
