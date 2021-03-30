e92f7da8b421fcc429195bab9e9bd053fed2aa97
William Goulart Pacheco <gtaskwill@gmail.com>
2021-03-30 17:25:23 -0300

    banco_lixo

--- nodejs/008_node_example/v6/modularnode/src/config/sql/update/sql.js
+++ nodejs/008_node_example/v6/modularnode/src/config/sql/update/sql.js
@@ -16,8 +16,58 @@ const sql = {
         return lista
     },
 
-    'T411PASI_FULL' : ` 
-        
+    'DATA_PROC_PRA_HOJE' : ` 
+        begin
+            UPDATE t416proc proc
+            SET proc.dt_ult_pro=trunc(sysdate-1), proc.dt_pro_atu=trunc(sysdate);
+            commit;
+        end;
+    `,
+
+    'BIOMETRIA_WSDL':`
+        begin
+        update autorizador.t411wsdl
+        set end_wsdl = 'https://demo.creddefense.com/index.php/api/v2',
+            cd_usu = 'VERDECARD_API',
+            nr_ip= utl_inaddr.get_host_address(NULL),
+            de_snh_crp = '721FCFFEA6E38F785EEF39AB503B1219'
+        where (nr_ip= utl_inaddr.get_host_address(NULL) or nr_ip = '10.0.0.196' or nr_ip= '10.0.0.30')
+        and id_tp = 'CREDDEFENSE';
+        commit;
+        end;
+    `,
+
+    'BIOMETRIA_WSDL':`
+        begin
+            UPDATE autorizador.tbl_config_url_barramento_h SET api_url='http://10.0.0.85:5787/PasswordManager_REST/passwordControl/' where api_number >= 1;
+            commit;
+        end;
+    `,
+
+    'ACESSO_EXTERNO':`
+        begin
+            UPDATE autorizador.t411defg SET id_ace_ext='N';
+            commit;
+        end;
+    `,
+
+    'SENHA_RESET_01VSYS':`
+        begin   UPDATE t400usua usua    SET id_st = 'A', DE_SNH_USU = '518', nro_ten_ace = 0, dt_vld_snh =  (sysdate+(5*365))  WHERE 1=1; COMMIT;  end;
+    `,
+
+    'SENHA_RESET_02DIGI':`
+        begin   UPDATE digitaliza.usuario SET SENHA='518' , criptografia = 'ESPECIAL_1', situacao = 'ATIVO'   WHERE 1=1; COMMIT;  end;
+    `,
+
+    'SENHA_RESET_03VCOB':`
+        begin   UPDATE digitaliza.usuario SET SENHA='518' , criptografia = 'ESPECIAL_1', situacao = 'ATIVO'   WHERE 1=1; COMMIT;  end;
+    `,
+
+    'DESATIVAR_ACESSO_EXT':`
+        begin
+            UPDATE  autorizador.t411defg SET id_ace_ext='N';
+            commit;
+        end;
     `,
 
 
