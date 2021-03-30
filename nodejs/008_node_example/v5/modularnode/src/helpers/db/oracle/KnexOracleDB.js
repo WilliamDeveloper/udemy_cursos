fc003eae035468100f38ac7bce417f708453851c
William Goulart Pacheco <gtaskwill@gmail.com>
2021-03-30 16:58:27 -0300

    banco_lixo

--- nodejs/008_node_example/v5/modularnode/src/helpers/db/oracle/KnexOracleDB.js
+++ nodejs/008_node_example/v5/modularnode/src/helpers/db/oracle/KnexOracleDB.js
@@ -1,7 +1,13 @@
-process.env['TNS_ADMIN'] = 'D:/_dev_/_ide_/plsql/plsql11_64bits/instantclient-x64-19.5/network//admin'
-process.env['ORACLE_HOME'] = 'D:/_dev_/_ide_/plsql/plsql11_64bits/instantclient-x64-19.5'
-process.env['LD_LIBRARY_PATH'] = 'D:/_dev_/_ide_/plsql/plsql11_64bits/instantclient-x64-19.5'
+// process.env['TNS_ADMIN'] = 'D:/_dev_/_ide_/plsql/plsql11_64bits/instantclient-x64-19.5/network//admin'
+// process.env['ORACLE_HOME'] = 'D:/_dev_/_ide_/plsql/plsql11_64bits/instantclient-x64-19.5'
+// process.env['LD_LIBRARY_PATH'] = 'D:/_dev_/_ide_/plsql/plsql11_64bits/instantclient-x64-19.5'
+// process.env['PATH'] =`${process.env['ORACLE_HOME']};${process.env['PATH']}`
+
+process.env['TNS_ADMIN'] = `${process.env['ORACLE_HOME']}/network/admin`
+process.env['ORACLE_HOME'] = `${process.env['ORACLE_HOME']}`
+process.env['LD_LIBRARY_PATH'] = `${process.env['ORACLE_HOME']}`
 process.env['PATH'] =`${process.env['ORACLE_HOME']};${process.env['PATH']}`
+
 
 const tnsnamesOracle = require('../../../config/tnsnamesOracle')
 const knex = require('knex')
