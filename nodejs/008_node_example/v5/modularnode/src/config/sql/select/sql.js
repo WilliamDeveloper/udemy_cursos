4dfaef825a41405864691dee94903390018547a7
William Goulart Pacheco <gtaskwill@gmail.com>
2021-03-30 14:58:28 -0300

    banco_lixo

--- nodejs/008_node_example/v5/modularnode/src/config/sql/select/sql.js
+++ nodejs/008_node_example/v5/modularnode/src/config/sql/select/sql.js
@@ -1,5 +1,13 @@
 const sql = {
-    t411pasiFull : ` 
+
+    getValueFromKey(key){
+        return this[key]
+    },
+
+    'T411PASI_FULL' : ` 
         select cd_key, cd_value, id_sit from autorizador.t411pasi where rownum  <= 5
     `,
+
+
+
 }
