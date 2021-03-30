9b96b3a646bc7fc8c3974c34ece75ae757eec260
William Goulart Pacheco <gtaskwill@gmail.com>
2021-03-30 13:36:55 -0300

    banco_lixo

--- nodejs/008_node_example/v5/modularnode/src/controllers/sql.js
+++ nodejs/008_node_example/v5/modularnode/src/controllers/sql.js
@@ -1,13 +1,28 @@
 const KnexOracleDB = require("../helpers/db/oracle/KnexOracleDB")
+const tnsnamesOracle = require('../config/tnsnamesOracle')
 
 const SqlController ={
 
     async select(req, res, next){
 
         // let sql = " select * from autorizador.t411pasi  "
-        let nomeTabela = 'T411Pasi'
-        let sql = " select cd_key, cd_value, id_sit from autorizador.t411pasi  "
+        // let listaNomesBase = tnsnamesOracle['0']
+        // console.log(listaNomesBase)
+
         let nomeBase = 'homologa_autorizador'
+
+        let configBase = tnsnamesOracle.getConfigByNameBase(nomeBase)
+        console.log(configBase)
+
+        let listaNomesBase = tnsnamesOracle.getListaBasesConfig()
+        console.log('listaNomesBase',listaNomesBase)
+
+
+
+
+
+        let nomeTabela = 'T411Pasi'
+        let sql = " select cd_key, cd_value, id_sit from autorizador.t411pasi where rownum <= 2 "
         let conexao = await KnexOracleDB.getConexao(nomeBase)
         let resultSql = conexao.raw(sql)
 
@@ -24,6 +39,7 @@ const SqlController ={
                 rows: data
             }
             let dadosPagina={
+                listaNomesBase,
                 nomeTabela,
                 sqlResult
             }
