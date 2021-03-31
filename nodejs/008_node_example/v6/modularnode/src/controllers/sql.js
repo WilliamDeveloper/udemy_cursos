a184c5563f7dc145494334dc224cc2b61f938c34
William Goulart Pacheco <gtaskwill@gmail.com>
2021-03-30 22:20:08 -0300

    banco_lixo

--- nodejs/008_node_example/v6/modularnode/src/controllers/sql.js
+++ nodejs/008_node_example/v6/modularnode/src/controllers/sql.js
@@ -149,31 +149,7 @@ const SqlController = {
                 }
                 else
                 {
-
-
-                    let p_object_type = ''
-                    let p_owner = ''
-                    let p_object_name = ''
-                    let p_compile = ''
-
-
-                    for (let i = 0; i < lista.length ; i++) {
-                        console.log('lista ', lista[i])
-                         p_object_type = lista[i]['OBJECT_TYPE']
-                         p_owner = lista[i]['OWNER']
-                         p_object_name = lista[i]['OBJECT_NAME']
-                         p_compile = lista[i]['OBJECT_TYPE']
-
-                         if(p_object_type == 'PACKAGE BODY'){
-                             p_object_type='PACKAGE'
-                             p_compile= 'COMPILE BODY'
-                         }
-
-                        sql = sqlQuerys.update.COMPILAR_OBJ_OWNER
-                        sql = eval("`"+sql+"`")
-                        console.log('p_object_type ', p_object_type,p_owner, p_object_name, p_compile)
-                        await KnexOracleDB.runSQL(nomeBaseSelecionado,sql)
-                    }
+                    DbaService.recompileAllInvalid(nomeBaseSelecionado)
 
                     res.json({lista, sql})
                 }
--- nodejs/008_node_example/v6/modularnode/src/services/DbaService.js
+++ nodejs/008_node_example/v6/modularnode/src/services/DbaService.js
@@ -12,7 +12,40 @@ const DbaService = {
             console.log('error-getListObjectInvalid: ',e)
         }
         return lista
+    },
+
+    async recompileAllInvalid(nomeBaseSelecionado){
+        let lista = await DbaService.getListObjectInvalid(nomeBaseSelecionado)
+        let sql = ''
+
+        if(lista !== undefined && lista.length){
+            let p_object_type = ''
+            let p_owner = ''
+            let p_object_name = ''
+            let p_compile = ''
+
+            for (let i = 0; i < lista.length ; i++) {
+                console.log('lista ', lista[i])
+                p_object_type = lista[i]['OBJECT_TYPE']
+                p_owner = lista[i]['OWNER']
+                p_object_name = lista[i]['OBJECT_NAME']
+                p_compile = lista[i]['OBJECT_TYPE']
+
+                if(p_object_type == 'PACKAGE BODY'){
+                    p_object_type='PACKAGE'
+                    p_compile= 'COMPILE BODY'
+                }
+
+                sql = sqlQuerys.update.COMPILAR_OBJ_OWNER
+                sql = eval("`"+sql+"`")
+
+                await KnexOracleDB.runSQL(nomeBaseSelecionado,sql)
+            }
+
+        }
     }
+
+
 }
 
 module.exports = DbaService
