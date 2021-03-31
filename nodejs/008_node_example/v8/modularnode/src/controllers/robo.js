70a973d1658b7dc79ee458cef4e74554d8fb8757
William Goulart Pacheco <gtaskwill@gmail.com>
2021-03-31 14:57:44 -0300

    banco_lixo

--- nodejs/008_node_example/v8/modularnode/src/controllers/robo.js
+++ nodejs/008_node_example/v8/modularnode/src/controllers/robo.js
@@ -6,6 +6,20 @@ const RoboController = {
 
         }
         res.render('robo-acoes', obj);
+    },
+
+    abrir_browser(req, res, next){
+        let obj = {
+
+        }
+        res.render('robo-acoes', obj);
+    },
+
+    fechar_browser(req, res, next){
+        let obj = {
+
+        }
+        res.render('robo-acoes', obj);
     }
 
 }
--- nodejs/008_node_example/v8/modularnode/src/routes/robo.js
+++ nodejs/008_node_example/v8/modularnode/src/routes/robo.js
@@ -2,6 +2,8 @@ const router = require('express').Router()
 const RoboController = require('../controllers/robo')
 
 router.get('/', RoboController.robo_acoes)
+router.get('/abrir-browser', RoboController.abrir_browser)
+router.get('/fechar-browser', RoboController.fechar_browser)
 
 
 module.exports =  router
