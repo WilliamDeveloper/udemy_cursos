958ae4ed20f928283d2f76509af54b8959853809
William Goulart Pacheco <gtaskwill@gmail.com>
2021-03-31 21:43:57 -0300

    banco_lixo

--- nodejs/008_node_example/v8/modularnode/src/controllers/bat.js
+++ nodejs/008_node_example/v8/modularnode/src/controllers/bat.js
@@ -1,9 +1,28 @@
 // const Browser = require('./../helpers/selenium/browser/Browser')
-
+const batScritpt = require('./../config/bat/batScritpt')
+const fs = require('./../helpers/fs/fs')
 
 const BatController = {
 
     async clear_ff_profile_temp(req, res, next){
+
+        let comando_1 = batScritpt.DEL_ALL_FIREFOX_PROFILE_TMP_1
+        let comando_2 = batScritpt.DEL_ALL_FIREFOX_PROFILE_TMP_2
+        let comando_3 = batScritpt.DEL_ALL_FIREFOX_PROFILE_TMP_3
+
+
+
+
+        await fs.ls();
+        console.log('#')
+        // await fs.cmdWithDir('ls', '%temp%')
+        // process.chdir('%temp%')
+        let dirAtual = await fs.dirAtual()
+        console.log(dirAtual)
+        console.log('#')
+        await fs.ls();
+
+
         let obj = {
 
         }
