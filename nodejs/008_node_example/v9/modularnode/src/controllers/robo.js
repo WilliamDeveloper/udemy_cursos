1ddc7b1e4ffe6b2debb56fcabdf9b030fc0f5d71
William Goulart Pacheco <gtaskwill@gmail.com>
2021-04-01 07:58:16 -0300

    banco_lixo

--- nodejs/008_node_example/v9/modularnode/src/controllers/robo.js
+++ nodejs/008_node_example/v9/modularnode/src/controllers/robo.js
@@ -1,4 +1,7 @@
 const Browser = require('./../helpers/selenium/browser/Browser')
+const  RoboBrowserController = require('./../controllers/robo/browser')
+
+
 
 
 const RoboController = {
@@ -10,70 +13,12 @@ const RoboController = {
         res.render('robo-acoes', obj);
     },
 
-    async abrir_browser_ff(req, res, next){
-
-        try{
-            let termo = 'Hellow world'
-            const url ='http://www.google.com.br'
-            let browser = new Browser()
-            await browser.init.setDriverFirefox()
-            await browser.opcao.doAcessarPagina(`${url}/search?q=${termo}`)
+    abrir_browser_chrome: RoboBrowserController.abrir_browser_chrome,
+    abrir_browser_ff: RoboBrowserController.abrir_browser_ff,
+    abrir_browser_ff: RoboBrowserController.fechar_browser,
 
 
-            // await browser.elemento.clickByName('btnK')
 
-
-// driver.findElement(By.tagName('button')).click()
-
-            // browser
-        }catch (e) {
-            console.log(e)
-        }
-
-        let obj = {
-
-        }
-        res.render('robo-acoes', obj);
-    },
-
-    async abrir_browser_chrome(req, res, next){
-
-        try{
-            let termo = 'Hellow world'
-            const url ='http://www.google.com.br'
-            let browser = new Browser()
-            await browser.init.setDriverChrome()
-            await browser.opcao.doAcessarPagina(`${url}/search?q=${termo}`)
-
-            // browser
-        }catch (e) {
-            console.log(e)
-        }
-
-        let obj = {
-
-        }
-        res.render('robo-acoes', obj);
-    },
-
-    async fechar_browser(req, res, next){
-
-
-        try{
-            const url ='http://www.google.com.br'
-            let browser = new Browser()
-            await browser.opcao.doFecharNavegador()
-
-            // browser
-        }catch (e) {
-            console.log(e)
-        }
-
-        let obj = {
-
-        }
-        res.render('robo-acoes', obj);
-    }
 
 }
 
