fc9ac05b4e35c05e8740bec022fde52782c11292
William Goulart Pacheco <gtaskwill@gmail.com>
2021-04-01 07:09:07 -0300

    banco_lixo

--- nodejs/008_node_example/v9/modularnode/src/helpers/selenium/browser/BrowserEvento.js
+++ nodejs/008_node_example/v9/modularnode/src/helpers/selenium/browser/BrowserEvento.js
@@ -3,57 +3,73 @@ class  BrowserEvento {
         this.browser = pInstanciaBrowser
     }
 
-//     def doPressionarEnterToElementoId(self,pElementoId):
-//         self.browser.elemento.getById(pElementoId).send_keys(Keys.ENTER)
-//
-//
-// # -----
-//     def doClickBy(self,pLocatorTipo,pLocatorSentenca):
-//         v_locator = (pLocatorTipo, pLocatorSentenca)
-//
-//     encontrou_elemento =  False
-//     while not encontrou_elemento:
-//     try:
-//         encontrou_elemento = self.browser.wait.until(ec.element_to_be_clickable(v_locator))
-//     self.browser.log.print('- ',encontrou_elemento)
-//     except:
-//         self.browser.log.print(f'erro-doClickBy: ( {pLocatorTipo}, {pLocatorSentenca})')
-//     pass
-//
-//     # //para o click ganhar ter focus apos um reload de pagina
-//     self.browser.elemento.getByTagName('body').click()
-//     encontrou_elemento.click()
-//
-//
-//
-//     def doClickById(self, pElementoId):
-//         self.doClickBy(By.ID,pElementoId)
-//
-//     def doRecuperarfocusDepoisDeUmLoadingRefresh(self):
-//         self.browser.elemento.getByTagName('body').click()
-//
-//     def doClickOnButtonClassQueVemAposId(self,pElementoId,pElementoClass):
-//         v_termo = f"//*[@id='{pElementoId}']/following::button[1][contains(@class,'{pElementoClass}')]"
-//     self.browser.log.print('doClickOnButtonClassQueVemAposId - ',v_termo)
-//     v_elemento = self.browser.elemento.getByXPath(v_termo)
-//     v_elemento.click()
-//
-//     def doClickOnButtonQueVemAposId(self,pElementoId):
-//         v_termo = f"//*[@id='{pElementoId}']/following::button[contains(@class,'btnX')]"
-//     self.browser.log.print('doClickOnButtonClassQueVemAposId - ',v_termo)
-//     v_elemento = self.browser.elemento.getByXPath(v_termo)
-//     v_elemento.click()
-//
-//     def doEsperarLoadingByClass(self,pElementoClass):
-//         v_termo = f"//*[@class='{pElementoClass}']"
-//     self.browser.log.print(v_termo)
-//     v_elemento = self.browser.wait.until(ec.invisibility_of_element_located((By.XPATH, v_termo)))
-//     return v_elemento
-//
-//     def doEsperarLoadingByClassAndBackToIframeById(self,pElementoClass,pIframeId):
-//         self.browser.iframe.doGoToIframeDefault()
-//     self.browser.evento.doEsperarLoadingByClass(pElementoClass)
-//     self.browser.iframe.doGoToIframeById(pIframeId)
+
+     doPressionarEnterToElementoId(pElementoId){
+         // self.browser.elemento.getById(pElementoId).send_keys(Keys.ENTER)
+     }
+
+
+
+    doClickBy(pLocatorTipo,pLocatorSentenca){
+        // v_locator = (pLocatorTipo, pLocatorSentenca)
+        //
+        // encontrou_elemento =  False
+        // while not encontrou_elemento:
+        //     try:
+        // encontrou_elemento = self.browser.wait.until(ec.element_to_be_clickable(v_locator))
+        // self.browser.log.print('- ',encontrou_elemento)
+        // except:
+        //     self.browser.log.print(f'erro-doClickBy: ( {pLocatorTipo}, {pLocatorSentenca})')
+        // pass
+        //
+        // # //para o click ganhar ter focus apos um reload de pagina
+        //     self.browser.elemento.getByTagName('body').click()
+        // encontrou_elemento.click()
+    }
+
+
+
+
+    doClickById( pElementoId){
+        // self.doClickBy(By.ID,pElementoId)
+
+    }
+
+    doRecuperarfocusDepoisDeUmLoadingRefresh(){
+        // self.browser.elemento.getByTagName('body').click()
+    }
+
+
+    doClickOnButtonClassQueVemAposId(pElementoId,pElementoClass){
+        // v_termo = f"//*[@id='{pElementoId}']/following::button[1][contains(@class,'{pElementoClass}')]"
+        // self.browser.log.print('doClickOnButtonClassQueVemAposId - ',v_termo)
+        // v_elemento = self.browser.elemento.getByXPath(v_termo)
+        // v_elemento.click()
+    }
+
+
+    doClickOnButtonQueVemAposId(pElementoId){
+        // v_termo = f"//*[@id='{pElementoId}']/following::button[contains(@class,'btnX')]"
+        // self.browser.log.print('doClickOnButtonClassQueVemAposId - ',v_termo)
+        // v_elemento = self.browser.elemento.getByXPath(v_termo)
+        // v_elemento.click()
+    }
+
+
+    doEsperarLoadingByClass(pElementoClass){
+        // v_termo = f"//*[@class='{pElementoClass}']"
+        // self.browser.log.print(v_termo)
+        // v_elemento = self.browser.wait.until(ec.invisibility_of_element_located((By.XPATH, v_termo)))
+        // return v_elemento
+    }
+
+
+    doEsperarLoadingByClassAndBackToIframeById(pElementoClass,pIframeId){
+        // self.browser.iframe.doGoToIframeDefault()
+        // self.browser.evento.doEsperarLoadingByClass(pElementoClass)
+        // self.browser.iframe.doGoToIframeById(pIframeId)
+    }
+
 }
 
 module.exports = BrowserEvento
