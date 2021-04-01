1e5516f954f30599e299443c2a5698cfc1b03d48
William Goulart Pacheco <gtaskwill@gmail.com>
2021-04-01 07:30:11 -0300

    banco_lixo

--- nodejs/008_node_example/v9/modularnode/src/helpers/selenium/browser/BrowserKeys.js
+++ nodejs/008_node_example/v9/modularnode/src/helpers/selenium/browser/BrowserKeys.js
@@ -1,21 +1,28 @@
 class  BrowserKeys {
+
     constructor(pInstanciaBrowser){
         this.browser = pInstanciaBrowser
     }
 
-//     def doPressionarESC(self):
-//     # self.browser.action_chains.send_keys(Keys.ESCAPE).perform()
-//     ActionChains(self.browser.driver).send_keys(Keys.ESCAPE).perform()
-//     # pElemento.send_keys(Keys.ESCAPE)
-//
-//
-//     # pElemento.send_keys(Keys.ESCAPE)
-//     # ActionChains(self.browser.driver).key_down(Keys.ESCAPE).key_up(Keys.ESCAPE).perform()
-//     # Keys.ESCAPE
-//
-//     def doPressionarTAB(self):
-//         ActionChains(self.browser.driver).send_keys(Keys.TAB).perform()
-// # .send_keys(Keys.TAB)
+    doPressionarESC(){
+        // # self.browser.action_chains.send_keys(Keys.ESCAPE).perform()
+        // ActionChains(self.browser.driver).send_keys(Keys.ESCAPE).perform()
+        // # pElemento.send_keys(Keys.ESCAPE)
+        //
+        //
+        // # pElemento.send_keys(Keys.ESCAPE)
+        // # ActionChains(self.browser.driver).key_down(Keys.ESCAPE).key_up(Keys.ESCAPE).perform()
+        // # Keys.ESCAPE
+    }
+
+
+    doPressionarTAB(){
+        // ActionChains(self.browser.driver).send_keys(Keys.TAB).perform()
+        // # .send_keys(Keys.TAB)
+    }
+
+
+
 }
 
 module.exports = BrowserKeys
