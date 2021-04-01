f22de54b2b2d7d6af5d5854e0d2675124c274ab4
William Goulart Pacheco <gtaskwill@gmail.com>
2021-04-01 07:27:42 -0300

    banco_lixo

--- nodejs/008_node_example/v9/modularnode/src/helpers/selenium/browser/BrowserIframe.js
+++ nodejs/008_node_example/v9/modularnode/src/helpers/selenium/browser/BrowserIframe.js
@@ -2,31 +2,38 @@ class  BrowserIframe {
     constructor(pInstanciaBrowser){
         this.browser = pInstanciaBrowser
     }
+
+    doGoToIframeByElemento(pElemento){
+        // self.browser.driver.switch_to.frame(pElemento)
+    }
+
+    doGoToIframeById(pElementoId){
+        // self.browser.iframe.doGoToIframeDefault()
+        // v_elemento = self.browser.elemento.getById(pElementoId)
+        //
+        // self.doGoToIframeByElemento(v_elemento)
+    }
+
+
+
+    doGoToIframeByName(pElementoName){
+        // self.doGoToIframeByElemento(pElementoName)
+
+    }
+
+
+    doGoToIframeDefault(){
+        // try:
+        // self.browser.driver.switch_to.default_content()
+        // except Exception as e:
+        // print('erro: ',e)
+    }
+
 }
 
 
 
-// def __init__(self,pInstanciaBrowser):
-// self.browser = pInstanciaBrowser
-//
-// def doGoToIframeByElemento(self,pElemento):
-// self.browser.driver.switch_to.frame(pElemento)
-//
-// def doGoToIframeById(self,pElementoId):
-// self.browser.iframe.doGoToIframeDefault()
-// v_elemento = self.browser.elemento.getById(pElementoId)
-//
-// self.doGoToIframeByElemento(v_elemento)
-//
-//
-// def doGoToIframeByName(self,pElementoName):
-// self.doGoToIframeByElemento(pElementoName)
-//
-//
-// def doGoToIframeDefault(self):
-// try:
-// self.browser.driver.switch_to.default_content()
-// except Exception as e:
-// print('erro: ',e)
+
+
 
 module.exports = BrowserIframe
