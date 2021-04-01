2eedc21effced0553b748fbc998cf0581265d592
William Goulart Pacheco <gtaskwill@gmail.com>
2021-04-01 07:33:12 -0300

    banco_lixo

--- nodejs/008_node_example/v9/modularnode/src/helpers/selenium/browser/BrowserScreenshot.js
+++ nodejs/008_node_example/v9/modularnode/src/helpers/selenium/browser/BrowserScreenshot.js
@@ -2,20 +2,26 @@ class  BrowserScreenshot {
     constructor(pInstanciaBrowser){
         this.browser = pInstanciaBrowser
     }
-    //
-    // def doTirarScreenshotDoBodyEstandoEmIframeById(self,pIframeId):
-    //     self.browser.iframe.doGoToIframeDefault()
-    // self.doTirarScreenshotDoBody()
-    // self.browser.iframe.doGoToIframeById(pIframeId)
-    //
-    // def doTirarScreenshotDoBody(self, pPastaPadraoScreenShoot=_const.selenium.pasta_padrao_screenshot):
-    // if(self.is_habilitado):
-    // v_elemento = self.browser.elemento.getByTagName('body')
-    // v_elemento.screenshot( pPastaPadraoScreenShoot + f'print{time.time()}.png')
-    //
-    // def doTirarScreenshotFromElemento(self, pElemento, pPastaPadraoScreenShoot=_const.selenium.pasta_padrao_screenshot):
-    // if(self.is_habilitado):
-    // pElemento.screenshot( pPastaPadraoScreenShoot + f'print{time.time()}.png')
+
+    doTirarScreenshotDoBodyEstandoEmIframeById(pIframeId){
+        // self.browser.iframe.doGoToIframeDefault()
+        // self.doTirarScreenshotDoBody()
+        // self.browser.iframe.doGoToIframeById(pIframeId)
+    }
+
+
+    doTirarScreenshotDoBody(pPastaPadraoScreenShoot=_const.selenium.pasta_padrao_screenshot){
+        // if(self.is_habilitado):
+        // v_elemento = self.browser.elemento.getByTagName('body')
+        // v_elemento.screenshot( pPastaPadraoScreenShoot + f'print{time.time()}.png')
+    }
+
+
+    doTirarScreenshotFromElemento(pElemento, pPastaPadraoScreenShoot=_const.selenium.pasta_padrao_screenshot){
+        // if(self.is_habilitado):
+        // pElemento.screenshot( pPastaPadraoScreenShoot + f'print{time.time()}.png')
+    }
+
 
 
 
