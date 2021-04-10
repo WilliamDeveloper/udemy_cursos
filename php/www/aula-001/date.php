1165fdae676f557ca7aaa896ff8d7c9a4447a828
William Goulart Pacheco <gtaskwill@gmail.com>
2021-04-10 18:23:38 -0300

    banco_lixo

---
+++ php/www/aula-001/date.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by IntelliJ IDEA.
+ * User: willi
+ * Date: 10/04/2021
+ * Time: 18:21
+ */
+
+echo "<br>";
+echo date('d/m/Y H:i:s');
+echo "<br>";
+echo time();
+echo "<br>";
+echo date('d/m/Y H:i:s',1618089768);
+echo "<br>";
+echo "<br>";
+echo "<br>";
