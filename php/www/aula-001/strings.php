a7b7bbad1ebfd5ba19a4c66733461358f2b8ad9a
William Goulart Pacheco <gtaskwill@gmail.com>
2021-04-10 13:59:34 -0300

    banco_lixo

---
+++ php/www/aula-001/strings.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Created by IntelliJ IDEA.
+ * User: willi
+ * Date: 10/04/2021
+ * Time: 12:56
+ */
+
+$nome="william";
+$sobreNome="pacheco";
+$nomeCompleto= "$nome $sobreNome";
+echo "$nomeCompleto";
+echo "<br>";
+
+$nome="william";
+$sobreNome="pacheco";
+$nomeCompleto= "$nome $sobreNome";
+echo '$nomeCompleto';
+echo "<br>";
