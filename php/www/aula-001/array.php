57e71e2aeaf12eba1aa0a0de775dc08d5e50d12c
William Goulart Pacheco <gtaskwill@gmail.com>
2021-04-10 15:53:33 -0300

    banco_lixo

--- php/www/aula-001/array.php
+++ php/www/aula-001/array.php
@@ -26,3 +26,25 @@ echo "<br>";
 
 echo $lista[1][1];
 echo "<br>";
+
+
+######
+$pessoas = array();
+
+array_push($pessoas, array(
+    "nome" => "william",
+    "idade" => 18,
+));
+array_push($pessoas, array(
+    "nome" => "william2 com ÂcÊnTÃO",
+    "idade" => 19,
+));
+
+$obj_json = json_encode($pessoas);
+echo $obj_json;
+echo "<br>";
+
+$json='[{"nome":"william","idade":18},{"nome":"william2 com \u00c2c\u00canT\u00c3O","idade":19}]';
+$arrayJson = json_decode($json,true);
+var_dump($arrayJson);
+echo "<br>";
