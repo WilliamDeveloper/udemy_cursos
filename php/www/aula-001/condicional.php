9a5441b7596c4ec97429f12290fb1ed6163f7ad9
William Goulart Pacheco <gtaskwill@gmail.com>
2021-04-10 14:51:49 -0300

    banco_lixo

---
+++ php/www/aula-001/condicional.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by IntelliJ IDEA.
+ * User: willi
+ * Date: 10/04/2021
+ * Time: 14:48
+ */
+
+$valor=41;
+
+if($valor < 40){
+    echo "valor abaixo de 40";
+}else if($valor == 40){
+    echo "valor igual";
+}else{
+    echo "valor acima de 40";
+}
