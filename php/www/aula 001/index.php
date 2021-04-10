5d3ba7a4b677b79ea9704e2278e7d6046e26ad68
William Goulart Pacheco <gtaskwill@gmail.com>
2021-04-10 01:41:49 -0300

    #'

--- php/www/aula 001/index.php
+++ php/www/aula 001/index.php
@@ -6,6 +6,13 @@
     $valor=45.33;
 
     echo "meu  primeiro codigo php <strong> hello<strong> world";
+    echo "";
+    //comentario de 1 linha
+
+    /*
+     *
+     *
+     *  */
     echo "<br>    $nome $idade $verdade $valor";
 
 
