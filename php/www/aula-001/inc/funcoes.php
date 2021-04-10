290fa0842d121e2a5fc8093632c5b014f785430c
William Goulart Pacheco <gtaskwill@gmail.com>
2021-04-10 14:36:58 -0300

    banco_lixo

---
+++ php/www/aula-001/inc/funcoes.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Created by IntelliJ IDEA.
+ * User: willi
+ * Date: 10/04/2021
+ * Time: 14:25
+ */
+
+function somar($a,$b){
+    return $a+$b;
+}
---
+++ php/www/aula-001/include_require.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by IntelliJ IDEA.
+ * User: willi
+ * Date: 10/04/2021
+ * Time: 14:24
+ */
+
+// eh como se ele pegasse o conteudo do arquivo e botasse no lugar no include
+//include "inc/funcoes.php";
+
+//require "inc/funcoes.php";
+require_once "inc/funcoes.php";
+
+$resultado=somar(10,20);
+echo "$resultado";
+echo "<br>";
