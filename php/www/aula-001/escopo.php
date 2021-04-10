26b25a0d58d0cdca1a3d1c336810cfdd4f6f9bd2
William Goulart Pacheco <gtaskwill@gmail.com>
2021-04-10 12:20:56 -0300

    banco_lixo

--- php/www/aula-001/escopo.php
+++ php/www/aula-001/escopo.php
@@ -14,3 +14,5 @@ function teste(){
     global $nome;
     echo "daaijsuhdsaudhasuh $nome";
 }
+
+teste();
---
+++ php/www/aula-001/operadores.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Created by IntelliJ IDEA.
+ * User: willi
+ * Date: 10/04/2021
+ * Time: 12:18
+ */
+#atribuicao
+$nome = "william";
+echo "$nome";
+echo "<br>";
+
+###############################
+
+#contenacao
+$nome="william";
+$sobrenome="pacheco";
+# concatenando
+$nomeCompleto=$nome . ' ' . $sobrenome;
+echo "$nomeCompleto";
+echo "<br>";
