6ff44bd6efc00259e557c25af5c1467da0dd4ecf
William Goulart Pacheco <gtaskwill@gmail.com>
2021-04-10 02:12:43 -0300

    #'

--- php/www/aula 001/index.php
+++ php/www/aula 001/index.php
@@ -1,12 +1,15 @@
 <?php
 
     $nome="william";
+    $sobrenome="pacheco";
+    $nomeCompleto=$nome . ' ' . $sobrenome;
     $idade=18;
     $verdade=true;
     $valor=45.33;
 
     echo "meu  primeiro codigo php <strong> hello<strong> world";
     echo "";
+    echo $nomeCompleto;
     //comentario de 1 linha
 
     /*
@@ -14,6 +17,12 @@
      *
      *  */
     echo "<br>    $nome $idade $verdade $valor";
+    unset($valor);
+
+    if ( isset($valor) ){
+        echo $valor;
+    }
+
 
 
 ?>
