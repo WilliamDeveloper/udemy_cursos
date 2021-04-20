b2bf77ce5827c473f0890938d205d09a261d6537
William Goulart Pacheco <gtaskwill@gmail.com>
2021-04-19 22:04:48 -0300

    #'

--- nodejs/013_babel/src/index.js
+++ nodejs/013_babel/src/index.js
@@ -2,3 +2,12 @@ const meuNome="William";
 
 console.log('hello')
 
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+ReactDOM.render(
+    <div>
+        Nome: {meuNome}
+    </div>
+    , document.querySelector("#app")
+)
