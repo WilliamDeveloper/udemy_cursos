777aad25617f1638e1c58cd72d44cf946c501217
William Goulart Pacheco <gtaskwill@gmail.com>
2021-05-09 23:48:57 -0300

    #

--- angularjs/aula09/js/services/contatosAPIService3.js
+++ angularjs/aula09/js/services/contatosAPIService3.js
@@ -1,6 +1,16 @@
 //recuperando um modulo
 app = angular.module("listaTelefonica").provider("serialGenerator", function () {
 
+    var _length = 10;
+
+    this.getLength = function () {
+        return _length
+    }
+
+    this.setLength = function (length) {
+        _length = length;
+    }
+
     this.$get = function () {
         return{
             generate: function () {
