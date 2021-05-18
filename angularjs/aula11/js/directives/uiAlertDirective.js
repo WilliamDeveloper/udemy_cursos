cd0c9aac85afa0b6e8b8b6c2870d46f729ed976f
William Goulart Pacheco <gtaskwill@gmail.com>
2021-05-17 23:20:46 -0300

    #'

--- angularjs/aula11/js/directives/uiAlertDirective.js
+++ angularjs/aula11/js/directives/uiAlertDirective.js
@@ -4,7 +4,13 @@ app = angular.module("listaTelefonica").directive("uiAlert", function () {
     return {
         // template: "<div>uiAlert</div>"
         templateUrl: "view/alert.html",
-        replace:true, //sobrescrever o html externo
+        replace:true, //sobrescrever o html externo,
+
+        // tipos de restricao
+        // a-diretiva restrita ao atributo do elemento
+        // e-diretiva restrita ao elemento
+        // c-diretiva restrita a classe do elemento
+        // m-diretiva restrita ao comentario do elemento
     }
 
 })
