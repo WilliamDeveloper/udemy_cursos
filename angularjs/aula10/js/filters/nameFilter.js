2d09aaecc52cde680785debfd0dc76e8c4ce7941
William Goulart Pacheco <gtaskwill@gmail.com>
2021-05-17 21:59:20 -0300

    #'

--- angularjs/aula10/js/filters/nameFilter.js
+++ angularjs/aula10/js/filters/nameFilter.js
@@ -3,6 +3,9 @@ app = angular.module("listaTelefonica").filter("name", function () {
 
     return function(input){
         var listaDeNomes = input.split("L")
+
+        if(/(da|de)/.test("de")) console.log("blau")
+
         input = listaDeNomes.join("-")
         return input
     }
