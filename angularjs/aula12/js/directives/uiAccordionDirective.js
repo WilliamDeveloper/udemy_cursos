988628732192adb631fae18b8ccc63c94fa09a49
William Goulart Pacheco <gtaskwill@gmail.com>
2021-05-19 13:12:37 -0300

    #

--- angularjs/aula12/js/directives/uiAccordionDirective.js
+++ angularjs/aula12/js/directives/uiAccordionDirective.js
@@ -1,4 +1,15 @@
 //recuperando um modulo
+
+app = angular.module("listaTelefonica").directive("uiAccordions", function ($filter) {
+    return{
+        controller: function ($scope, $element, $attrs) {
+            this.helloWorld = function () {
+                console.log("hello world")
+            }
+        },
+    }
+});
+
 app = angular.module("listaTelefonica").directive("uiAccordion", function ($filter) {
 
     return {
@@ -6,7 +17,14 @@ app = angular.module("listaTelefonica").directive("uiAccordion", function ($filt
         transclude: true,
         scope:{
             title: "@"
-        }
+        },
+        require:"^uiAccordions",
+        link: function (scope, element, attrs, ctrl) {
+            ctrl.helloWorld()
+            scope.open = function () {
+                scope.isOpened = ! scope.isOpened
+            }
+        },
         // require:"ngModel",
         // link: function(scope, element, attrs, ctrl){
         //
