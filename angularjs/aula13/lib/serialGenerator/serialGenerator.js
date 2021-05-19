152b53f594f5f5e850c450d03f44c8fcb4fbf853
William Goulart Pacheco <gtaskwill@gmail.com>
2021-05-19 13:36:51 -0300

    #

---
+++ angularjs/aula13/lib/serialGenerator/serialGenerator.js
@@ -0,0 +1,30 @@
+//recuperando um modulo
+angular.module("serialGenerator", [])
+app = angular.module("serialGenerator").provider("serialGenerator", function () {
+
+    var _length = 10;
+
+    this.getLength = function () {
+        return _length
+    }
+
+    this.setLength = function (length) {
+        _length = length;
+    }
+
+    this.$get = function () {
+        return{
+            generate: function () {
+                var serial = "";
+                while(serial.length < _length){
+                    serial += String.fromCharCode(Math.floor(Math.random() * 64 ) +32);
+                }
+                return serial;
+            }
+        }
+    }
+
+
+
+
+})
