4f4f9a2a089f164b8eb1fb9ab5353a659431f504
William Goulart Pacheco <gtaskwill@gmail.com>
2021-05-20 00:00:38 -0300

    #'

--- angularjs/aula14/js/interceptors/timestampInterceptor.js
+++ angularjs/aula14/js/interceptors/timestampInterceptor.js
@@ -4,6 +4,10 @@ app = angular.module("listaTelefonica").factory("timestampInterceptor", function
    return{
         request:  function (config) {
             console.log(config)
+            var url = config.url
+            if(url.indexOf('view') > -1) return config
+            var timestamp = new Date().getTime()
+            config.url= url + "?timestamp="+timestamp
             return config
         }
 
