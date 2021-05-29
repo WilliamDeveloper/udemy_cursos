1c7f7664d8ae66f8361c1dc75d0e729b870d9ec8
William Goulart Pacheco <gtaskwill@gmail.com>
2021-05-29 14:37:23 -0300

    #

---
+++ typescript/aula01/PessoaClass.ts
@@ -0,0 +1,14 @@
+class Pessoa{
+    nome:string;
+
+    constructor(pessoa:string){
+        this.nome = pessoa
+    }
+
+    exibirNome(){
+        return `ola meu nome é ${this.nome}`;
+    }
+}
+
+let objPessoa = new Pessoa("William");
+console.log(objPessoa.exibirNome)
---
+++ typescript/aula01/readme.md
@@ -0,0 +1,19 @@
+/* tipo basicos de representacao de dados */
+
+/*
+- Number
+- Boolean
+- String
+- Array
+- Tuple
+- Enum
+- Any
+- Void
+- Null
+- Undefined
+- Never
+- Object
+
+
+
+*/
