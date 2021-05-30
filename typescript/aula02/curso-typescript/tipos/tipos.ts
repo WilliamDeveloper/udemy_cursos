146b462deb0ef3e6176da3829649de332845a3cb
William Goulart Pacheco <gtaskwill@gmail.com>
2021-05-29 21:13:26 -0300

    #

--- typescript/aula02/curso-typescript/tipos/tipos.ts
+++ typescript/aula02/curso-typescript/tipos/tipos.ts
@@ -1,12 +1,29 @@
 //string
-let nome='joao'
+let nome : string ='joao'
 console.log(nome)
 
 //numbers
-let idade=27
+let idade : number =27
 console.log(idade)
 
 //boolean
 let isOpen = true
 console.log(isOpen)
 
+//tipos explicitos
+let minhaIdade: any
+minhaIdade = 27
+console.log(typeof minhaIdade)
+
+minhaIdade = '27'
+console.log(typeof minhaIdade)
+
+
+// array
+let hobbies: any[] = ['asas', 12]
+console.log(typeof hobbies)
+
+
+// tuplas
+let endereco : [string, number] = ['santa rosa',555]
+console.log(endereco)
