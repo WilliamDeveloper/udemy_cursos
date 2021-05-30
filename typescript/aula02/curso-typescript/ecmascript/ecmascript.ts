0c3e308a222ef9156ec38babf2f45beae8b19e64
William Goulart Pacheco <gtaskwill@gmail.com>
2021-05-30 00:32:20 -0300

    #'

--- typescript/aula02/curso-typescript/ecmascript/ecmascript.ts
+++ typescript/aula02/curso-typescript/ecmascript/ecmascript.ts
@@ -1,3 +1,18 @@
 //let const
 var seraqpode = 5
 console.log(seraqpode)
+
+//this
+function normalComThis() {
+    console.log(this)
+}
+
+normalComThis()
+
+//mudando o contexto do this da funcao
+const normalComThisEspecial = normalComThis.bind('abc')
+normalComThisEspecial()
+
+// o this definido em uma funcao arrow nunca muda
+const arrowComThis = () => console.log(this)
+arrowComThis()
