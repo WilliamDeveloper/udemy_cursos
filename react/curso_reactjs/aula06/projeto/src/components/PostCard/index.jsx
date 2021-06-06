6b4b7d6e972f9a7d8c6ea8d19d9d4457762d5a3f
William Goulart Pacheco <gtaskwill@gmail.com>
2021-06-06 15:43:45 -0300

    #

--- react/curso_reactjs/aula06/projeto/src/components/PostCard/index.jsx
+++ react/curso_reactjs/aula06/projeto/src/components/PostCard/index.jsx
@@ -4,7 +4,7 @@ export const PostCard = ({title, body, id, cover}) =>{
         <div className="post">
             <img src={cover} title={title}></img>
             <div  className="post-content">
-                <h2>{title}</h2>
+                <h2>{title}-{id}</h2>
                 <p>{body}</p>
             </div>
         </div>
