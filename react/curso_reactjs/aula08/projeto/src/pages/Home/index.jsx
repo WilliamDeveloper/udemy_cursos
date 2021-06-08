fd42d0bdadcf3e02e16b4003b73662571ed59718
William Goulart Pacheco <gtaskwill@gmail.com>
2021-06-07 23:11:23 -0300

    #''

--- react/curso_reactjs/aula08/projeto/src/pages/Home/index.jsx
+++ react/curso_reactjs/aula08/projeto/src/pages/Home/index.jsx
@@ -1,12 +1,108 @@
-import  React from 'react'
+import  {Component, useState, useEffect, useCallback} from 'react'
 import './styles.css';
-import {loadPosts} from "./../../utils/load-posts";
 import {Posts} from "./../../components/Posts";
+import {loadPosts} from "./../../utils/load-posts";
 import {Button} from "../../components/Button";
 import {TextInput} from "../../components/TextInput/TextInput";
 
+export const Home = ()=>{
 
-export class Home extends React.Component{
+  const [posts, setPosts] = useState([])
+  const [allPosts, setAllPosts] = useState([])
+  const [page, setPage] = useState(0)
+  const [postsPerPage, setPostsPerPage] = useState(10)
+  const [searchValue, setSearchValue] = useState('')
+
+  const noMorePosts = page + postsPerPage >= allPosts.length ;
+
+  const filteredPosts = !!searchValue ?
+      posts.filter((post)=>{
+        return post.title.toLowerCase().includes(searchValue.toLowerCase())
+      })
+      : posts;
+
+
+
+  const handleLoadPosts= useCallback(async (page, postsPerPage)=>{
+    console.log('oi')
+
+    const postsAndPhotos = await loadPosts()
+
+    setPosts( postsAndPhotos.slice(page,postsPerPage) )
+    setAllPosts( postsAndPhotos )
+  },[])
+
+  useEffect(()=>{
+    handleLoadPosts(page, postsPerPage)
+  },[handleLoadPosts,page, postsPerPage])
+
+  const loadMorePosts =  ()=>{
+    console.log('load more posts')
+
+    const nextPage = page + postsPerPage
+    const nextPosts = allPosts.slice(nextPage, nextPage + postsPerPage)
+    posts.push( ... nextPosts)
+
+
+    setPosts(posts)
+    setPage(nextPage)
+
+  }
+
+  const handleChange = (event)=>{
+    const {value} = event.target;
+
+    setSearchValue( value )
+
+  }
+
+
+
+
+
+
+
+  return (
+      <section className="container">
+
+        <div className="search-container">
+
+          {!!searchValue &&(
+              <>
+                <h1>Search value: {searchValue} </h1>
+              </>
+          )}
+
+          <TextInput searchValue={searchValue} handleChange={handleChange}/>
+
+        </div>
+
+        {filteredPosts.length > 0 && (
+            <Posts posts={filteredPosts}/>
+        )}
+
+        {filteredPosts.length === 0 && (
+            <p>Nao existem post =(</p>
+        )}
+
+
+
+        <div className="button-container">
+          {!searchValue &&(
+              <Button
+                  text={"Load More Posts"}
+                  onClick={loadMorePosts}
+                  disabled={noMorePosts}
+              />
+          )}
+
+        </div>
+
+      </section>
+  )
+}
+
+export class Home2 extends Component{
 
   state = {
     name: 'William',
