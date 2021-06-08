907f8aadcf11d3cf23c427815aab3d739b3a3146
William Goulart Pacheco <gtaskwill@gmail.com>
2021-06-08 13:22:14 -0300

    #

---
+++ react/curso_reactjs/aula08/projeto/src/pages/Home/Home.test.jsx
@@ -0,0 +1,13 @@
+describe('<home />',()=>{
+
+  test('dummy one',()=>{
+    expect(1).toBe(1)
+  })
+  it('dummy two',()=>{
+    expect(1).toBe(1)
+  })
+  it('dummy three',()=>{
+    expect(1).toBe(1)
+  })
+
+})
