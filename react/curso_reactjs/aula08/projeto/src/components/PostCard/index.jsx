b9d90acde55c84079b8b2dffc66e582697c8bccf
William Goulart Pacheco <gtaskwill@gmail.com>
2021-06-09 14:16:32 -0300

    Merge branch 'main' of https://github.com/WilliamDeveloper/udemy_cursos into main
    # Conflicts:
    #	react/curso_reactjs/readme.md

--- react/curso_reactjs/aula08/projeto/src/components/PostCard/index.jsx
+++ react/curso_reactjs/aula08/projeto/src/components/PostCard/index.jsx
@@ -1,3 +1,4 @@
+import P from 'prop-types';
 import './styles.css'
 
 export const PostCard = ({title, body, id, cover}) =>{
@@ -11,4 +12,11 @@ export const PostCard = ({title, body, id, cover}) =>{
             </div>
         </div>
     )
+}
+
+PostCard.propTypes={
+    title:P.string.isRequired,
+    cover:P.string.isRequired,
+    body: P.string.isRequired,
+    id: P.number.isRequired
 }
