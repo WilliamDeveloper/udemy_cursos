bb6b1c816722c383f4cfb21705d92e2181e12e9c
William Goulart Pacheco <gtaskwill@gmail.com>
2021-06-08 23:17:40 -0300

    #''

--- react/curso_reactjs/aula08/projeto/src/components/Posts/Posts.spec.tsx
+++ react/curso_reactjs/aula08/projeto/src/components/Posts/Posts.spec.tsx
@@ -10,7 +10,8 @@ describe('<Posts />',()=>{
     it('should render Posts',()=>{
 
         // @ts-ignore
-        const {debug} = render(<Posts {...props} />)
+        render(<Posts {...props} />)
+        expect(screen.getAllByRole('heading',{name: /title/i})).toHaveLength(3)
         // debug()
         //
         // expect(screen.getByRole('img', {name: props.title})).toHaveAttribute("src",props.cover)
