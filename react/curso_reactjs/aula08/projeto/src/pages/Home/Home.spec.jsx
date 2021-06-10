e2bf1a610813253d117a0de2c90e922307cccf5c
William Goulart Pacheco <gtaskwill@gmail.com>
2021-06-09 23:42:08 -0300

    #''

--- react/curso_reactjs/aula08/projeto/src/pages/Home/Home.spec.jsx
+++ react/curso_reactjs/aula08/projeto/src/pages/Home/Home.spec.jsx
@@ -1,7 +1,13 @@
-describe('<home />',()=>{
+import {render, screen, waitForElementToBeRemoved} from "@testing-library/react";
+import {Home} from ".";
 
-  test('dummy oness',()=>{
-    expect(1).toBe(1)
+describe('<Home />',()=>{
+
+  test('should be render search',async ()=>{
+    render(<Home />)
+    const noMorePosts = screen.getByText('Nao existem post =(')
+    await waitForElementToBeRemoved(noMorePosts)
+    screen.debug()
   })
   it('dummy two',()=>{
     expect(1).toBe(1)
