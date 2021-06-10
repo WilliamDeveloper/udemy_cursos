4f30ba232a20c6ffa41b6ef1b61fdd878b4d33ef
William Goulart Pacheco <gtaskwill@gmail.com>
2021-06-10 00:10:46 -0300

    #''

--- react/curso_reactjs/aula08/projeto/src/pages/Home/Home.spec.jsx
+++ react/curso_reactjs/aula08/projeto/src/pages/Home/Home.spec.jsx
@@ -1,7 +1,53 @@
+import {rest} from 'msw'
+import {setupServer} from 'msw/node'
 import {render, screen, waitForElementToBeRemoved} from "@testing-library/react";
 import {Home} from ".";
 
+const handlers=[
+  rest.get("https://jsonplaceholder.typicode.com/photos", async (req,res,ctx)=> {
+    res(ctx.json(
+        [
+          {
+            userId: 1,
+            id: 1,
+            title: 'title1',
+            body: 'body1',
+            url: 'img1.jpg',
+          },
+          {
+            userId: 2,
+            id: 2,
+            title: 'title2',
+            body: 'body2',
+            url: 'img1.jpg',
+          },
+          {
+            userId: 3,
+            id: 3,
+            title: 'title3',
+            body: 'body3',
+            url: 'img3.jpg',
+          },
+        ]
+    ))
+  })
+]
+
+const server = setupServer(...handlers)
+
 describe('<Home />',()=>{
+
+  beforeAll(()=>{
+    server.listen()
+  })
+
+  afterEach(()=>{
+    server.resetHandlers()
+  })
+
+  afterAll(()=>{
+    server.close()
+  })
 
   test('should be render search',async ()=>{
     render(<Home />)
