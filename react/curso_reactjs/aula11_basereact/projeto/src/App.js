8f21527f51c70a0d0cd455e162b1345ba0834c21
William Goulart Pacheco <gtaskwill@gmail.com>
2021-06-10 20:52:24 -0300

    #''

--- react/curso_reactjs/aula11_basereact/projeto/src/App.js
+++ react/curso_reactjs/aula11_basereact/projeto/src/App.js
@@ -1,6 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
-import {Component} from 'react'
+import { Component } from 'react';
 
 // function App() {
 //   return (
@@ -18,34 +18,32 @@ import {Component} from 'react'
 //   );
 // }
 
-class App extends Component{
+class App extends Component {
+  constructor(props) {
+    super(props);
 
-  constructor(props){
-    super(props)
-
-    this.state={
-      reverse: false,
-    }
+    this.state = {
+      reverse: true,
+    };
   }
 
   render() {
+    const { reverse } = this.state;
+    const reverseClass = reverse ? 'App-logo-reverse' : '';
 
-      const {reverse} = this.state
-      const reverseClass = reverse ? 'App-logo-spin-reverse':''
-
-      return (
-        <div className="App">
-          <header className="App-header">
-            <img src={logo} className={`App-logo ${reverseClass}`} alt="logo" />
-            <p>
-              Edit <code>src/App.js</code> and save to reload.
-            </p>
-            <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-              Learn React
-            </a>
-          </header>
-        </div>
-      );
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className={`App-logo ${reverseClass}`} alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
+            Learn React
+          </a>
+        </header>
+      </div>
+    );
   }
 }
 
