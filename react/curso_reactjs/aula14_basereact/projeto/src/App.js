095428116ec2dc4431b3bd06e753da5e8d9a3cc6
William Goulart Pacheco <gtaskwill@gmail.com>
2021-06-12 00:07:55 -0300

    #''

--- react/curso_reactjs/aula14_basereact/projeto/src/App.js
+++ react/curso_reactjs/aula14_basereact/projeto/src/App.js
@@ -1,83 +1,23 @@
-import P from 'prop-types';
-import logo from './logo.svg';
 import './App.css';
-import React, { useState, useEffect, useCallback, useMemo } from 'react';
-
-const eventFn = () => {
-  console.log('h1 clicado');
-};
-
-const Button = ({ handleIncrement }) => {
-  return (
-    <button type="button" onClick={handleIncrement}>
-      +
-    </button>
-  );
-};
-
-Button.propTypes = {
-  handleIncrement: P.func,
-};
+import React, { useState, useEffect } from 'react';
 
 function App() {
-  const [reverse, setReverse] = useState(false);
-  const [counter, setCounter] = useState(0);
-  const reverseClass = reverse ? 'App-logo-reverse' : '';
+  const [posts, setPosts] = useState([]);
+  console.log('pai renderizou!');
 
-  //componentDidUpdate - excecuta toda vez q o componente atualiza
+  //component did mount
   useEffect(() => {
-    console.log('componentDidUpdate');
-  });
-
-  //componentDidmount - excecuta 1x
-  useEffect(() => {
-    console.log('componentDidmount');
+    fetch('https://jsonplaceholder.typicode.com/posts')
+      .then((r) => r.json())
+      .then((r) => setPosts(r));
   }, []);
-
-  //com dependencia - excecuta toda vez q a depedencia mudar
-  useEffect(() => {
-    console.log('contador mudou para ', counter);
-  }, [counter]);
-
-  //componentDidmount
-  useEffect(() => {
-    document.querySelector('h1')?.addEventListener('click', eventFn);
-
-    //componentWillAmount
-    return () => {
-      console.log('componentWillAmount');
-      document.querySelector('h1')?.removeEventListener('click', eventFn);
-    };
-  }, [counter]);
-
-  const handleReverse = () => {
-    setReverse((reverseA) => !reverseA);
-  };
-  const handleIncrement = useCallback(() => {
-    setCounter((counterA) => counterA + 1);
-  }, []);
-  const btn = useMemo(() => {
-    return <Button handleIncrement={handleIncrement} />;
-  }, [handleIncrement]);
 
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className={`App-logo ${reverseClass}`} alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-          Learn React
-        </a>
-        <h1>Contador : {counter}</h1>
-        <p>
-          <button type="button" onClick={handleReverse}>
-            Reverse {reverseClass}
-          </button>
-        </p>
-        <p>{btn}</p>
-      </header>
+      <h1>Oi</h1>
+      {posts.map((post) => {
+        div.
+      })}
     </div>
   );
 }
