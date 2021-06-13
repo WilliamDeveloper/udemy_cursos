be215185c00bdce9f43c02893609f69002ae98fc
William Goulart Pacheco <gtaskwill@gmail.com>
2021-06-13 01:27:36 -0300

    #''

--- react/curso_reactjs/aula16_basereact/projeto/src/App.js
+++ react/curso_reactjs/aula16_basereact/projeto/src/App.js
@@ -1,52 +1,13 @@
-// import P from 'prop-types';
 import './App.css';
-import React, { useContext, useState } from 'react';
-// import { useState, useEffect, useMemo, useRef } from 'react';
-
-const globalState = {
-  title: 'o titulo do contexto',
-  body: 'o body do contexto',
-  counter: 0,
-};
-
-const GlobalContext = React.createContext();
-
-// eslint-disable-next-line
-const Div = ({childen}) => {
-  return (
-    <>
-      <H1 />
-      <P />
-    </>
-  );
-};
-
-// eslint-disable-next-line
-const H1 = () => {
-  const theContext = useContext(GlobalContext);
-  console.log(theContext);
-  const { contextState } = theContext;
-  return (
-    <h1>
-      {contextState.title} {contextState.counter}
-    </h1>
-  );
-};
-// eslint-disable-next-line
-const P = () => {
-  const theContext = useContext(GlobalContext);
-  console.log(theContext);
-  const { contextState, setContextState } = theContext;
-  return <p onClick={() => setContextState((s) => ({ ...s, counter: s.counter + 1 }))}>{contextState.body}</p>;
-};
+import React from 'react';
+import { Div } from './components/Div';
+import { AppContext } from './contexts/App';
 
 function App() {
-  const [contextState, setContextState] = useState(globalState);
-
   return (
-    <GlobalContext.Provider value={{ contextState, setContextState }}>
+    <AppContext>
       <Div />
-    </GlobalContext.Provider>
+    </AppContext>
   );
 }
 
