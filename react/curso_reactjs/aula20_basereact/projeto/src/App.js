d30bda5f04370d27ef5d9318450fff8e219c8209
William Goulart Pacheco <gtaskwill@gmail.com>
2021-06-14 19:24:44 -0300

    #

--- react/curso_reactjs/aula20_basereact/projeto/src/App.js
+++ react/curso_reactjs/aula20_basereact/projeto/src/App.js
@@ -13,19 +13,44 @@ const useMyHook = (cb, delay = 1000) => {
     }, delay);
     // console.log('interval ', interval);
     return () => clearInterval(interval);
-  }, []);
+  }, [delay]);
 };
 
 function App() {
   const [counter, setCounter] = useState(0);
+  const [delay, setDelay] = useState(1000);
+  const [incrementor, setIncrementor] = useState(100);
+  console.log('setdelay ', setDelay, incrementor, setIncrementor);
 
   useMyHook(() => {
     setCounter((c) => c + 1);
-  });
+  }, delay);
 
   return (
     <div>
       <h1>oi {counter}</h1>
+      <button>delay {delay} </button>
+      <button
+        onClick={() => {
+          setDelay((d) => d + incrementor);
+        }}
+      >
+        + {incrementor}{' '}
+      </button>
+      <button
+        onClick={() => {
+          setDelay((d) => d - incrementor);
+        }}
+      >
+        - {incrementor}{' '}
+      </button>
+      <input
+        type="number"
+        value={incrementor}
+        onChange={(e) => {
+          setIncrementor(Number(e.target.value));
+        }}
+      />
     </div>
   );
 }
