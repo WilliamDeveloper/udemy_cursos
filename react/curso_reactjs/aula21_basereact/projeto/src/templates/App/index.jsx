bf07d103324b7e63fbc866684b9558496b5fd7ee
William Goulart Pacheco <gtaskwill@gmail.com>
2021-06-14 19:52:33 -0300

    #

---
+++ react/curso_reactjs/aula21_basereact/projeto/src/templates/App/index.jsx
@@ -0,0 +1,15 @@
+// import P from 'prop-types';
+import './styles.css';
+// import React, { useEffect, useState, useRef } from 'react';
+
+function Index() {
+  // console.log('setdelay ', setDelay, incrementor, setIncrementor);
+
+  return (
+    <div>
+      <h1>oi</h1>
+    </div>
+  );
+}
+
+export default Index;
