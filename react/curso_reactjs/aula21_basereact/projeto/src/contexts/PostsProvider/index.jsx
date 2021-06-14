b2980c3f996b28638e6a1fd75a8d0feb0f97c89a
William Goulart Pacheco <gtaskwill@gmail.com>
2021-06-14 20:37:06 -0300

    #

--- react/curso_reactjs/aula21_basereact/projeto/src/contexts/PostsProvider/index.jsx
+++ react/curso_reactjs/aula21_basereact/projeto/src/contexts/PostsProvider/index.jsx
@@ -1,7 +1,11 @@
 import P from 'prop-types';
 import { PostsContext } from './context';
+import { useReducer } from 'react';
+import { reducer } from './reducer';
+import { data } from './data';
 export const PostsProvider = ({ children }) => {
-  return <PostsContext.Provider>{children}</PostsContext.Provider>;
+  const [postsState, postsDispatch] = useReducer(reducer, data);
+  return <PostsContext.Provider value={{ postsState, postsDispatch }}>{children}</PostsContext.Provider>;
 };
 
 PostsProvider.propTypes = {
