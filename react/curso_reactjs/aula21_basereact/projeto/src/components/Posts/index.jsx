47ece910051aba6d8bcf3370ed15150abfd11406
William Goulart Pacheco <gtaskwill@gmail.com>
2021-06-14 20:53:50 -0300

    #

--- react/curso_reactjs/aula21_basereact/projeto/src/components/Posts/index.jsx
+++ react/curso_reactjs/aula21_basereact/projeto/src/components/Posts/index.jsx
@@ -1,4 +1,8 @@
+import { useContext } from 'react';
+import { PostsContext } from '../../contexts/PostsProvider/context';
 export const Posts = () => {
+  const postsContext = useContext(PostsContext);
+  console.log(postsContext);
   return (
     <div>
       <h1>hola</h1>
