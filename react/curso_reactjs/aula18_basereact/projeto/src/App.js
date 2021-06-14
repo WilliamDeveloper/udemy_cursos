b4cb6d482bdd7a39ef45232c7992bfb819182636
William Goulart Pacheco <gtaskwill@gmail.com>
2021-06-13 23:17:39 -0300

    #''

--- react/curso_reactjs/aula18_basereact/projeto/src/App.js
+++ react/curso_reactjs/aula18_basereact/projeto/src/App.js
@@ -1,13 +1,34 @@
 import './App.css';
-import React from 'react';
-import { Div } from './components/Div';
-import { AppContext } from './contexts/App';
+import React, { useReducer } from 'react';
+
+const globalState = {
+  title: 'o titulo do contexto',
+  body: 'o body do contexto',
+  counter: 0,
+};
+
+const reducer = (state, action) => {
+  console.log(action);
+  switch (action.type) {
+    case 'muda':
+      return { ...state, title: 'mudou' };
+  }
+  return { ...state };
+};
 
 function App() {
+  const [state, dispatch] = useReducer(reducer, globalState);
+  console.log(dispatch);
+  const { counter, title, body } = state;
+
   return (
-    <AppContext>
-      <Div />
-    </AppContext>
+    <div>
+      <h1>
+        {title} {counter}
+      </h1>
+      <p>{body}</p>
+      <button onClick={() => dispatch({ type: 'muda' })}>click</button>
+    </div>
   );
 }
 
