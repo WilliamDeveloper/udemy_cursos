885ee92beb8cafae0b0e25d6cd8f93c574d661f4
William Goulart Pacheco <gtaskwill@gmail.com>
2021-06-15 11:49:28 -0300

    #

--- react/curso_reactjs/aula22_basereact/projeto/src/templates/App/index.jsx
+++ react/curso_reactjs/aula22_basereact/projeto/src/templates/App/index.jsx
@@ -1,23 +1,9 @@
 // import P from 'prop-types';
 import './styles.css';
-import { PostsProvider } from '../../contexts/PostsProvider';
-import { CounterProvider } from '../../contexts/CounterProvider';
-import { Posts } from './../../components/Posts';
 // import React, { useEffect, useState, useRef } from 'react';
 
-function Index() {
+export const App = () => {
   // console.log('setdelay ', setDelay, incrementor, setIncrementor);
 
-  return (
-    <CounterProvider>
-      <PostsProvider>
-        <div>
-          <h1>oi</h1>
-          <Posts />
-        </div>
-      </PostsProvider>
-    </CounterProvider>
-  );
-}
-
-export default Index;
+  return <h1>oi</h1>;
+};
