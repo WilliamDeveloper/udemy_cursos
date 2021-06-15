aa3df887b10079bbc0650998a7cca81356ed9c24
William Goulart Pacheco <gtaskwill@gmail.com>
2021-06-14 21:03:05 -0300

    #

--- react/curso_reactjs/aula21_basereact/projeto/src/components/Posts/index.jsx
+++ react/curso_reactjs/aula21_basereact/projeto/src/components/Posts/index.jsx
@@ -1,11 +1,20 @@
-import { useContext } from 'react';
+import { useContext, useEffect } from 'react';
 import { PostsContext } from '../../contexts/PostsProvider/context';
+import { loadPosts } from '../../contexts/PostsProvider/actions';
 export const Posts = () => {
   const postsContext = useContext(PostsContext);
-  console.log(postsContext);
+  const { postsState, postsDispatch } = postsContext;
+  console.log(postsContext, postsState, postsDispatch);
+  useEffect(() => {
+    console.log('carregue os posts');
+    loadPosts(postsDispatch);
+  }, [postsDispatch]);
   return (
     <div>
       <h1>hola</h1>
+      {postsState.posts.map((p) => (
+        <p key={p.id}>{p.title}</p>
+      ))}
     </div>
   );
 };
