14efdd1427c1dd989aea25d04f6557bb1ac2147b
William Goulart Pacheco <gtaskwill@gmail.com>
2021-06-15 23:34:42 -0300

    #''

--- react/curso_reactjs/aula24_basereact/projeto/src/templates/Home2/index.jsx
+++ react/curso_reactjs/aula24_basereact/projeto/src/templates/Home2/index.jsx
@@ -1,13 +1,18 @@
-import { useState } from 'react';
+import { useState, useRef, useEffect } from 'react';
 export const Home2 = () => {
   const [counted, setCounted] = useState([0, 1, 2, 3, 4]);
+  const divRef = useRef();
+  useEffect(() => {
+    console.log(divRef.current);
+    divRef.current.scrollTop = divRef.current.scrollHeight;
+  });
   const handleClick = () => {
     setCounted((c) => [...c, +c.slice(-1) + 1]);
   };
   return (
     <p>
       <button onClick={handleClick}>{counted.slice(-1)}</button>
-      <div style={{ height: '100px', width: '100px', overflowY: 'scroll' }}>
+      <div ref={divRef} style={{ height: '100px', width: '100px', overflowY: 'scroll' }}>
         {counted.map((c) => {
           return <p key={`c-${c}`}>{c}</p>;
         })}
