ec4c6ce4ccce55689197ad3d3e87c9e8b2dea602
William Goulart Pacheco <gtaskwill@gmail.com>
2021-06-17 14:48:22 -0300

    #

--- react/curso_reactjs/aula28_proj2_roteamento/projeto/src/templates/Abc/index.jsx
+++ react/curso_reactjs/aula28_proj2_roteamento/projeto/src/templates/Abc/index.jsx
@@ -4,6 +4,17 @@ import { useHistory, useLocation, useParams } from 'react-router-dom';
 export const Abc = () => {
   const { slug, id } = useParams();
 
+  const location = useLocation();
+  console.log(' location ', location);
+
+  const history = useHistory();
+  console.log(' history ', history);
+  useEffect(() => {
+    setTimeout(() => {
+      history.push('/');
+    }, 5 * 1000);
+  });
+
   return (
     <div>
       <h1>
