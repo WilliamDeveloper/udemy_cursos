4ed9a5add8e48e44fe281e34c6068aaa4ebd366b
William Goulart Pacheco <gtaskwill@gmail.com>
2021-06-17 21:51:03 -0300

    #''

---
+++ react/curso_reactjs/aula30_strapi/projeto/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '84c6af4f7c3ef496543ce4bc442024d0'),
+    },
+  },
+});
---
+++ react/curso_reactjs/aula30_strapi/projeto/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '7f53a9d3-2caf-4d1a-a870-d2508aad55d7'
+};
