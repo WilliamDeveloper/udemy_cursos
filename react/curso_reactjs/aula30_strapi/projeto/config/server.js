61f77f1c7132bce8bb195899d28610c7150bfd21
William Goulart Pacheco <gtaskwill@gmail.com>
2021-06-17 22:02:38 -0300

    #''

--- react/curso_reactjs/aula30_strapi/projeto/config/server.js
+++ react/curso_reactjs/aula30_strapi/projeto/config/server.js
@@ -1,6 +1,6 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
+  port: env.int('PORT', 8082),
   admin: {
     auth: {
       secret: env('ADMIN_JWT_SECRET', '84c6af4f7c3ef496543ce4bc442024d0'),
