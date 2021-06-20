c43672b36929e90e05b5e43d6433cd9dc1bb6c71
William Goulart Pacheco <gtaskwill@gmail.com>
2021-06-19 22:50:34 -0300

    #''

--- react/curso_reactjs/aula33_nextjs/projeto/src/pages/index.jsx
+++ react/curso_reactjs/aula33_nextjs/projeto/src/pages/index.jsx
@@ -1,12 +1,10 @@
 // import Head from 'next/head'
-import styled from 'styled-components'
+import styled from 'styled-components';
 
 const Heading = styled.h1`
-  background: ${({theme}) => theme.colors.secondaryColor}
+  background: ${({ theme }) => theme.colors.secondaryColor};
 `;
 
 export default function Home() {
-  return (
-   <Heading>OI</Heading>
-  )
+  return <Heading>OI</Heading>;
 }
