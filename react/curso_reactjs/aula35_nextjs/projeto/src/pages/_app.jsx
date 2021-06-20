a67c762eb6bbf68608bf1964f6bbd7926834f7cf
William Goulart Pacheco <gtaskwill@gmail.com>
2021-06-20 13:54:39 -0300

    #

---
+++ react/curso_reactjs/aula35_nextjs/projeto/src/pages/_app.jsx
@@ -0,0 +1,20 @@
+// import P from 'prop-types';
+import { ThemeProvider } from 'styled-components';
+import { theme } from '../styles/theme';
+import { AppProps } from 'next/app';
+
+function MyApp({ Component, pageProps }) {
+  return (
+    <ThemeProvider theme={theme}>
+      <Component {...pageProps} />
+    </ThemeProvider>
+  );
+}
+
+export default MyApp;
+
+MyApp.propTypes = AppProps;
+// MyApp.propTypes = {
+//   Component: P.elementType.isRequired,
+//   pageProps: P.any,
+// };
