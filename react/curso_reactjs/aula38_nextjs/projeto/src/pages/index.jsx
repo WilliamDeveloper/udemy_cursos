e8bb8bd5a3d11a0aeb119ec986fe120534a0ec98
William Goulart Pacheco <gtaskwill@gmail.com>
2021-06-20 23:39:30 -0300

    #

--- react/curso_reactjs/aula38_nextjs/projeto/src/pages/index.jsx
+++ react/curso_reactjs/aula38_nextjs/projeto/src/pages/index.jsx
@@ -2,20 +2,25 @@ import P from 'prop-types';
 import Head from 'next/head';
 import styled from 'styled-components';
 import config from '../config';
+import pagesFakeData from './../api/dados.json';
 
 // const Heading = styled.h1`
 //   background: ${({ theme }) => theme.colors.secondaryColor};
 // `;
 
-export default function Index({ data }) {
+export default function Index({ data = null }) {
+  console.log(data);
   // return <Heading>OI</Heading>;
   return <h1>Luiz</h1>;
 };
 
 export const getStaticProps = async () => {
-  const raw = await fetch(config.url + config.defaultSlug);
-  const json = await raw.json();
-  const data = mapData(json);
+  // const raw = await fetch(config.url + config.defaultSlug);
+  // const json = await raw.json();
+  // const data = mapData(json)[0];
+
+  const json = pagesFakeData;
+  const data = mapData(json)[0];
 
   return {
     props:{
@@ -25,5 +30,5 @@ export const getStaticProps = async () => {
 };
 
 Index.propTypes = {
-  data: P.array,
+  data: P.object,
 };
