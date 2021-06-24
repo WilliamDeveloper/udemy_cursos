564e954094fe205b1564882bda81e0f6b1277ee3
William Goulart Pacheco <gtaskwill@gmail.com>
2021-06-23 23:30:48 -0300

    #''

--- react/curso_reactjs/aula43_nextjs/projeto/src/components/Heading/index.tsx
+++ react/curso_reactjs/aula43_nextjs/projeto/src/components/Heading/index.tsx
@@ -1,12 +1,11 @@
-import P from 'prop-types';
 import * as Styled from './styles';
 
 export type HeadingProps = {
   children: React.ReactNode,
-  colorDark: boolean,
-  as: 'h1'| 'h2'| 'h3'| 'h4'| 'h5'| 'h6',
-  size: 'small'| 'medium'| 'big'| 'huge',
-  uppercase: boolean,
+  colorDark?: boolean,
+  as?: 'h1'| 'h2'| 'h3'| 'h4'| 'h5'| 'h6',
+  size?: 'small'| 'medium'| 'big'| 'huge',
+  uppercase?: boolean,
 };
 
 export const Heading = ({
