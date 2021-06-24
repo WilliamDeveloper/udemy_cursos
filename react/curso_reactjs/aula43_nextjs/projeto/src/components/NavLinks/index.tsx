efe9a9090291f0cc28066b0d3c3d9d20f4161e02
William Goulart Pacheco <gtaskwill@gmail.com>
2021-06-24 00:18:06 -0300

    #''

--- react/curso_reactjs/aula43_nextjs/projeto/src/components/NavLinks/index.tsx
+++ react/curso_reactjs/aula43_nextjs/projeto/src/components/NavLinks/index.tsx
@@ -2,7 +2,7 @@ import * as Styled from './styles';
 import {MenuLink, MenuLinkProps} from '../MenuLink';
 
 export type NavLinksProps = {
-  links: MenuLinkProps[],
+  links?: MenuLinkProps[],
 };
 
 export const NavLinks = ({ links = [] } : NavLinksProps) => {
