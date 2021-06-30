5eabed6ccada0bfc1fd75a6305b669244d37f2a0
William Goulart Pacheco <gtaskwill@gmail.com>
2021-06-29 21:22:38 -0300

    #''

--- nest/aula02/nest-api/src/app.module.ts
+++ nest/aula02/nest-api/src/app.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ProductController } from './product/product.controller';
+import { ProductService } from './product/product.service';
 
 @Module({
   imports: [],
   controllers: [AppController, ProductController],
-  providers: [AppService],//os servicos ficam disponiveis para toda aplicacao
+  providers: [AppService, ProductService],//os servicos ficam disponiveis para toda aplicacao
 })
 export class AppModule {}
