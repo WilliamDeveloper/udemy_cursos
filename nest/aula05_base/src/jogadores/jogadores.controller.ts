cbe5c3baf23f68dcac05beb2fad6f38048c24e05
William Goulart Pacheco <gtaskwill@gmail.com>
2021-07-01 00:06:35 -0300

    #''

--- nest/aula05_base/src/jogadores/jogadores.controller.ts
+++ nest/aula05_base/src/jogadores/jogadores.controller.ts
@@ -1,10 +1,14 @@
-import { Controller, Post } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { CriarJogadorDto } from './dtos/criar-jogador.dto';
 
 @Controller('jogadores')
 export class JogadoresController {
 
   @Post()
-  async criarAtualizarJogador(){
-    return JSON.stringify({name:'william'})
+  async criarAtualizarJogador(
+    @Body() criarJogadorDto: CriarJogadorDto
+  ){
+    const { email, nome, telefoneCelular } = criarJogadorDto;
+    return JSON.stringify({email, nome, telefoneCelular})
   }
 }
