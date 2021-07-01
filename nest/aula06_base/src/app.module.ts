d0bd4a48f7a38ef15a99a0153f082fa5884e0510
William Goulart Pacheco <gtaskwill@gmail.com>
2021-07-01 15:03:13 -0300

    #

--- nest/aula06_base/src/app.module.ts
+++ nest/aula06_base/src/app.module.ts
@@ -1,11 +1,23 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { JogadoresController } from './jogadores/jogadores.controller';
 import { JogadoresModule } from './jogadores/jogadores.module';
 import { JogadoresService } from './jogadores/jogadores.service';
 import { AppController } from './app.controller';
 
+const mongoDB_url = 'mongodb+srv://admin_mongo:BNnz295Wk3gDLhU@cluster0.dwwu8.mongodb.net/test?retryWrites=true&w=majority';
+const mongoDB_params = {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useUnifiedTopology: true,
+  useFindAndModify: false,
+};
+
 @Module({
-  imports: [JogadoresModule],
+  imports: [
+    MongooseModule.forRoot(mongoDB_url, mongoDB_params),
+    JogadoresModule
+  ],
   controllers: [JogadoresController, AppController],
   providers: [JogadoresService],
 })
