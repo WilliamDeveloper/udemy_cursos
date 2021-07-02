4ab747f5fe387fed3f5b0deb5d0f0e44b0b80f60
William Goulart Pacheco <gtaskwill@gmail.com>
2021-07-02 15:18:45 -0300

    #

--- nest/aula08_base/src/categorias/categorias.service.ts
+++ nest/aula08_base/src/categorias/categorias.service.ts
@@ -1,4 +1,25 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { CriarCategoriaDto } from './dtos/criar-categoria.dto';
+import { Categoria } from './interfaces/categoria.interface';
+import { Model } from 'mongoose';
 
 @Injectable()
-export class CategoriasService {}
+export class CategoriasService {
+
+
+  constructor(
+    @InjectModel('Categoria') private readonly categoriaModel: Model<Categoria>
+  ){}
+
+  async criarCategoria( criarCategoriaDto : CriarCategoriaDto): Promise<Categoria>{
+      const { categoria } = criarCategoriaDto;
+      const categoriaEncontrada = await  this.categoriaModel.findOne({categoria}).exec()
+      if(categoriaEncontrada){
+        throw new BadRequestException(`categoria ${categoria} ja cadastrada`)
+      }
+      const categoriaCriada = new this.categoriaModel(criarCategoriaDto)
+      return await categoriaEncontrada.save();
+  }
+
+}
