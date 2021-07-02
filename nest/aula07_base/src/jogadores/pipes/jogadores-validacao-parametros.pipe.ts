c26a9f81b122fdca86940d45ddc45b26e20b3f81
William Goulart Pacheco <gtaskwill@gmail.com>
2021-07-01 21:01:11 -0300

    #''

--- nest/aula07_base/src/jogadores/pipes/jogadores-validacao-parametros.pipe.ts
+++ nest/aula07_base/src/jogadores/pipes/jogadores-validacao-parametros.pipe.ts
@@ -4,7 +4,7 @@ export class JogadoresValidacaoParametrosPipe implements PipeTransform {
   transform(value: any, metadata: ArgumentMetadata) {
     console.log(`value ${value} metadata ${metadata.type}`);
    if(!value){
-     throw new BadRequestException(`o valor do parametro ${metadata.data}`)
+     throw new BadRequestException(`o valor do parametro ${metadata.data} nao pode ser vazio`)
    }
     return value;
   }
