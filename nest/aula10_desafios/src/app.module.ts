5125963ca05f7666001d4678d2a9bf6b86de3217
William Goulart Pacheco <gtaskwill@gmail.com>
2021-07-03 19:48:55 -0300

    #''

---
+++ nest/aula10_desafios/src/app.module.ts
@@ -0,0 +1,31 @@
+import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { JogadoresModule } from './jogadores/jogadores.module';
+import { CategoriasModule } from './categorias/categorias.module';
+import { DesafiosModule } from './desafios/desafios.module';
+import { AppController } from './app.controller';
+
+
+// const mongoDB_url = 'mongodb+srv://admin_mongo:BNnz295Wk3gDLhU@cluster0.dwwu8.mongodb.net/test?retryWrites=true&w=majority';
+const db_user = 'admin_mongo';
+const db_pass = 'BNnz295Wk3gDLhU';
+const db_database = 'smartranking';
+const mongoDB_url = `mongodb+srv://${db_user}:${db_pass}@cluster0.dwwu8.mongodb.net/${db_database}?retryWrites=true&w=majority`;
+const mongoDB_params = {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useUnifiedTopology: true,
+  useFindAndModify: false,
+};
+
+
+@Module({
+  imports: [
+    MongooseModule.forRoot(mongoDB_url, mongoDB_params),
+    JogadoresModule,
+    CategoriasModule,
+    DesafiosModule],
+  controllers: [AppController],
+  providers: [],
+})
+export class AppModule {}
