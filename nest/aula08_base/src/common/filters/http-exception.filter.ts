cb0a2ff3f9fd68f36bd6879f3c7b0f18f7e49752
William Goulart Pacheco <gtaskwill@gmail.com>
2021-07-03 19:03:35 -0300

    #

--- nest/aula08_base/src/common/filters/http-exception.filter.ts
+++ nest/aula08_base/src/common/filters/http-exception.filter.ts
@@ -1,32 +1,32 @@
-import { ArgumentsHost, Catch, ExceptionFilter, HttpException, HttpStatus, Logger } from '@nestjs/common';
-
-@Catch()
-export class AllExceptionFilter implements ExceptionFilter {
-
-  private readonly logger = new Logger(AllExceptionFilter.name)
-
-  catch(exception: unknown, host: ArgumentsHost): any {
-    const ctx = host.switchToHttp();
-    const response = ctx.getResponse();
-    const request = ctx.getRequest();
-
-    const status = exception instanceof HttpException ?
-      exception.getStatus():
-      HttpStatus.INTERNAL_SERVER_ERROR;
-
-    const message = exception instanceof HttpException ?
-      exception.getResponse() :
-      '';
-
-    this.logger.error(`http status ${status} error message ${JSON.stringify(message)}`)
-
-    response.status(status).json({
-      timestamp: new Date().toISOString(),
-      path: request.url,
-      error: message
-    })
-
-  }
-
-}
-
+// import { ArgumentsHost, Catch, ExceptionFilter, HttpException, HttpStatus, Logger } from '@nestjs/common';
+//
+// @Catch()
+// export class AllExceptionFilter implements ExceptionFilter {
+//
+//   private readonly logger = new Logger(AllExceptionFilter.name)
+//
+//   catch(exception: unknown, host: ArgumentsHost): any {
+//     const ctx = host.switchToHttp();
+//     const response = ctx.getResponse();
+//     const request = ctx.getRequest();
+//
+//     const status = exception instanceof HttpException ?
+//       exception.getStatus():
+//       HttpStatus.INTERNAL_SERVER_ERROR;
+//
+//     const message = exception instanceof HttpException ?
+//       exception.getResponse() :
+//       '';
+//
+//     this.logger.error(`http status ${status} error message ${JSON.stringify(message)}`)
+//
+//     response.status(status).json({
+//       timestamp: new Date().toISOString(),
+//       path: request.url,
+//       error: message
+//     })
+//
+//   }
+//
+// }
+const a = 2
