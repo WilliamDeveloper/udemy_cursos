5fc22cef470fe3e48d2fc3c4ea55d041d950e9d5
William Goulart Pacheco <gtaskwill@gmail.com>
2021-07-03 23:56:55 -0300

    #''

---
+++ chatbot/telegran/aula01/index.js
@@ -0,0 +1,6 @@
+// let mode= process.env.NODE_ENV || 'development'
+//
+//
+// let caminhoEnv= `./src/config/.${mode}.env`
+// console.log('blau ', mode, caminhoEnv)
+// require('dotenv').config({ path:  caminhoEnv})
--- chatbot/telegran/readme.telegran.node.md
+++ chatbot/telegran/readme.telegran.node.md
@@ -1,2 +1,4 @@
 npm init -y
 npm install --save telegraf@3.17.3
+
+npm install --save dotenv
