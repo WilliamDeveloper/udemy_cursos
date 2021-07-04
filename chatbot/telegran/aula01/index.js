194e392e235907e5b6a3efd32fb019110d5cf2d3
William Goulart Pacheco <gtaskwill@gmail.com>
2021-07-04 00:05:52 -0300

    #''

--- chatbot/telegran/aula01/index.js
+++ chatbot/telegran/aula01/index.js
@@ -3,4 +3,8 @@ let caminhoEnv= `./src/config/.${mode}.env`
 console.log('blau ', mode, caminhoEnv)
 require('dotenv').config({ path:  caminhoEnv})
 
-const telegraf = require('telegraf')
+const token = process.env.TELEGRAN_TOKEN_BOT;
+console.log('token ',token)
+
+const Telegraf = require('telegraf')
+const bot = new Telegraf()
