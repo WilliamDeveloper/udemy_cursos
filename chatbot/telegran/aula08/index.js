3c588e2672f88b20eacd8ae3ab8492776b1618a1
William Goulart Pacheco <gtaskwill@gmail.com>
2021-07-04 18:39:43 -0300

    #

---
+++ chatbot/telegran/aula08/index.js
@@ -0,0 +1,48 @@
+let mode= process.env.NODE_ENV || 'development'
+let caminhoEnv= `./${mode}.env`
+console.log('blau ', mode, caminhoEnv)
+require('dotenv').config({ path:  caminhoEnv})
+
+const token = `${process.env.TELEGRAN_TOKEN_BOT}`;
+console.log('token ',token)
+
+const Extra = require('telegraf/extra')
+const Markup = require('telegraf/markup')
+let lista = []
+
+const botoes = () => Extra.markup(
+    Markup.inlineKeyboard(
+      lista.map( (item) => Markup.callbackButton(item, `delete ${item}`)),
+      {columns:3}
+    )
+)
+
+
+const Telegraf = require('telegraf')
+const bot = new Telegraf(token)
+
+
+bot.start(async(ctx, next) => {
+  await ctx.reply(`bem vindo`)
+  // await ctx.reply(`voce ta com fome?`,
+  //   Markup.keyboard(['Coca', 'Pepsi']).resize().oneTime().extra()
+  // )
+  await ctx.reply(`escreva os item q deseja adicionar..`)
+  next()
+})
+
+bot.on('text', async (ctx, next) => {
+  lista.push(ctx.update.message.text)
+  await ctx.reply(`item foi adicionado ${ctx.update.message.text}`, botoes())
+  next()
+});
+
+bot.action(/delete (.+)/gi, async (ctx, next)=>{
+  lista = lista.filter(item => item !== ctx.match[1])
+  await ctx.reply(`item foi deletado ${ctx.match[1]}`, botoes())
+  next()
+})
+
+
+
+bot.startPolling()
