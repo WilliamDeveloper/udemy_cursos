dc2be5bfca916e672b220421a4e536e4bec68e03
William Goulart Pacheco <gtaskwill@gmail.com>
2021-07-04 13:57:13 -0300

    #

--- chatbot/telegran/aula02/index.js
+++ chatbot/telegran/aula02/index.js
@@ -11,6 +11,9 @@ const bot = new Telegraf(token)
 
 bot.start((ctx) => {
   const from = ctx.update.message.from
+  console.log(' ctx.update.message ',ctx.update.message)
+  console.log(' ctx.update ',ctx.update)
+  console.log(' ctx ',ctx)
   console.log(from)
   if(from.id === 123){
     console.log('usuario permitido')
@@ -22,11 +25,37 @@ bot.start((ctx) => {
 
 bot.on('text',async (ctx,next) =>{
   await ctx.reply('luciane 1')
+
   next()
 })
 
-bot.on('text',async (ctx,next) =>{
-  await ctx.reply('luciane 2')
+bot.on('location',async (ctx,next) =>{
+  // console.log(' ctx ', ctx)
+  const message = ctx.update.message
+  const location = ctx.update.message.location
+  console.log(' message ', message)
+  console.log(' location ', location)
+  // const {location} = ctx.update.location
+
+  await ctx.reply(`voce esta em
+    lat: ${location.latitude},
+    lon: ${location.longitude} !,
+  `)
+
+})
+
+bot.on('contact',async (ctx,next) =>{
+  // console.log(' ctx ', ctx)
+  const message = ctx.update.message
+  const contact = ctx.update.message.contact
+  console.log(' message ', message)
+  console.log(' contact ', contact)
+  // const {location} = ctx.update.location
+
+  await ctx.reply(`ok vou lembrar
+    first_name: ${contact.first_name},
+    phone_number: ${contact.phone_number},    
+  `)
 
 })
 
