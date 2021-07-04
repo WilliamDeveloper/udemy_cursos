2cd2dc53386b9e03cf5e1a1d980975ae5ebdb2b4
William Goulart Pacheco <gtaskwill@gmail.com>
2021-07-04 00:00:54 -0300

    #''

--- chatbot/telegran/aula01/index.js
+++ chatbot/telegran/aula01/index.js
@@ -1,6 +1,4 @@
-// let mode= process.env.NODE_ENV || 'development'
-//
-//
-// let caminhoEnv= `./src/config/.${mode}.env`
-// console.log('blau ', mode, caminhoEnv)
-// require('dotenv').config({ path:  caminhoEnv})
+let mode= process.env.NODE_ENV || 'development'
+let caminhoEnv= `./src/config/.${mode}.env`
+console.log('blau ', mode, caminhoEnv)
+require('dotenv').config({ path:  caminhoEnv})
